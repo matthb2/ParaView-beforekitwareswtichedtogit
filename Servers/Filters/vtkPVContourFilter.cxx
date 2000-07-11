bf46de477a2f276cbf9c5bd4bb6acc4e4d0090be
Amy Squillacote <amy.squillacote@kitware.com>
2000-07-11 19:57:30 +0000

    This is to add in the elevation and contour filters for poly data.

---
+++ Servers/Filters/vtkPVContourFilter.cxx
@@ -0,0 +1,116 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1998-2000 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. No part of this software may be reproduced, distributed,
+or modified, in any form or by any means, without permission in writing from
+Kitware Inc.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+
+#include "vtkPVContourFilter.h"
+#include "vtkKWApplication.h"
+#include "vtkKWView.h"
+#include "vtkKWRenderView.h"
+#include "vtkPVComposite.h"
+
+int vtkPVContourFilterCommand(ClientData cd, Tcl_Interp *interp,
+			      int argc, char *argv[]);
+
+vtkPVContourFilter::vtkPVContourFilter()
+{
+  this->CommandFunction = vtkPVContourFilterCommand;
+  
+  this->Label = vtkKWLabel::New();
+  this->Label->SetParent(this);
+  this->Accept = vtkKWWidget::New();
+  this->Accept->SetParent(this);
+  this->ContourValueEntry = vtkKWEntry::New();
+  this->ContourValueEntry->SetParent(this);
+  this->ContourValueLabel = vtkKWLabel::New();
+  this->ContourValueLabel->SetParent(this);
+  
+  this->Contour = vtkContourFilter::New();
+}
+
+vtkPVContourFilter::~vtkPVContourFilter()
+{
+  this->Label->Delete();
+  this->Label = NULL;
+  
+  this->Accept->Delete();
+  this->Accept = NULL;
+  
+  this->ContourValueEntry->Delete();
+  this->ContourValueEntry = NULL;
+  this->ContourValueLabel->Delete();
+  this->ContourValueLabel = NULL;
+  
+  this->Contour->Delete();
+  this->Contour = NULL;
+}
+
+vtkPVContourFilter* vtkPVContourFilter::New()
+{
+  return new vtkPVContourFilter();
+}
+
+void vtkPVContourFilter::Create(vtkKWApplication *app, char *args)
+{
+  // must set the application
+  if (this->Application)
+    {
+    vtkErrorMacro("vtkPVContourFilter already created");
+    return;
+    }
+  this->SetApplication(app);
+  
+  // create the top level
+  this->Script("frame %s %s", this->GetWidgetName(), args);
+  
+  this->Label->Create(this->Application, "");
+  this->Label->SetLabel("vtkContourFilter label");
+  
+  this->Script("pack %s", this->Label->GetWidgetName());
+  
+  this->ContourValueLabel->Create(this->Application, "");
+  this->ContourValueLabel->SetLabel("Contour Value:");
+  this->ContourValueEntry->Create(this->Application, "");
+  this->ContourValueEntry->SetValue(this->GetContour()->GetValue(0), 2);
+  
+  this->Accept->Create(this->Application, "button", "-text Accept");
+  this->Accept->SetCommand(this, "ContourValueChanged");
+  this->Script("pack %s", this->Accept->GetWidgetName());
+  this->Script("pack %s %s -side left -anchor w",
+	       this->ContourValueLabel->GetWidgetName(),
+	       this->ContourValueEntry->GetWidgetName());
+}
+
+void vtkPVContourFilter::ContourValueChanged()
+{  
+  this->Contour->SetValue(0, this->ContourValueEntry->GetValueAsFloat());
+  this->Contour->Modified();
+  this->Contour->Update();
+  
+  this->Composite->GetView()->Render();
+}
+
+
---
+++ Servers/Filters/vtkPVContourFilter.h
@@ -0,0 +1,65 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1998-1999 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. No part of this software may be reproduced, distributed,
+or modified, in any form or by any means, without permission in writing from
+Kitware Inc.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+
+#ifndef __vtkPVContourFilter_h
+#define __vtkPVContourFilter_h
+
+#include "vtkKWLabel.h"
+#include "vtkContourFilter.h"
+#include "vtkKWEntry.h"
+#include "vtkKWLabel.h"
+#include "vtkPVSource.h"
+
+
+class VTK_EXPORT vtkPVContourFilter : public vtkPVSource
+{
+public:
+  static vtkPVContourFilter* New();
+  vtkTypeMacro(vtkPVContourFilter, vtkPVSource);
+
+  void Create(vtkKWApplication *app, char *args);
+  
+  void ContourValueChanged();
+
+  vtkGetObjectMacro(Contour, vtkContourFilter);
+  
+protected:
+  vtkPVContourFilter();
+  ~vtkPVContourFilter();
+  vtkPVContourFilter(const vtkPVContourFilter&) {};
+  void operator=(const vtkPVContourFilter&) {};
+    
+  vtkKWLabel *Label;
+  vtkKWWidget *Accept;
+  vtkKWEntry *ContourValueEntry;
+  vtkKWLabel *ContourValueLabel;
+
+  vtkContourFilter  *Contour;
+};
+
+#endif
