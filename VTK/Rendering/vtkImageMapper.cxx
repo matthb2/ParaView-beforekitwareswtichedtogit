453c4ef4adc4cea1b29a550ed3c04a98499a26a2
John Biddiscombe <jbiddiscombe@skippingmouse.co.uk>
2002-01-10 11:13:35 +0000

    ERR: When displaying with custom extents, use WholeExtent for UpdateExtent otherwise strange things can happen

--- VTK/Rendering/vtkImageMapper.cxx
+++ VTK/Rendering/vtkImageMapper.cxx
@@ -238,7 +238,7 @@ void vtkImageMapper::RenderStart(vtkViewport* viewport, vtkActor2D* actor)
     this->DisplayExtent[4] = this->ZSlice;
     this->DisplayExtent[5] = this->ZSlice;
     //
-    this->GetInput()->SetUpdateExtent(this->DisplayExtent);
+    this->GetInput()->SetUpdateExtentToWholeExtent();
     // clear the position adjustment
     this->PositionAdjustment[0] = 0;
     this->PositionAdjustment[1] = 0;
