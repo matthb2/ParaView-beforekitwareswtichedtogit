9f277a26fb724971837bc21dd93ca4a5694c67d6
Wes Turner <turner@crd.ge.com>
2002-01-29 21:58:27 +0000

    ERR: Could access a NULL pointer.

--- VTK/IO/vtkDataReader.cxx
+++ VTK/IO/vtkDataReader.cxx
@@ -1708,9 +1708,9 @@ vtkFieldData *vtkDataReader::ReadFieldData()
     this->Read(&numTuples);
     this->ReadString(type);
     data = this->ReadArray(type, numTuples, numComp);
-    data->SetName(name);
     if ( data != NULL )
       {
+      data->SetName(name);
       if ( ! skipField )
         {
         f->AddArray(data);
