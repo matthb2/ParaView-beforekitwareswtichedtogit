db60a079c27203048ce820486f8bd297700a1b21
Brad King <brad.king@kitware.com>
2002-01-29 16:34:38 +0000

    ERR: Removed unused operator new.  It causes many warnings on some platforms.

--- VTK/Common/vtkObject.cxx
+++ VTK/Common/vtkObject.cxx
@@ -25,12 +25,6 @@ static int vtkObjectGlobalWarningDisplay = 1;
 
 // avoid dll boundary problems
 #ifdef _WIN32
-void* vtkObject::operator new(size_t nSize, const char *, int)
-{
-  void* p=malloc(nSize);
-  return p;
-}
-
 void* vtkObject::operator new(size_t nSize)
 {
   void* p=malloc(nSize);
