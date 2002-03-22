5887612d307ff3b7b9f3e82f0de5b24a91cebab1
Lisa Avila <lisa.avila@kitware.com>
2002-03-22 20:51:45 +0000

    fixed printself problem

--- VTK/Rendering/vtkVolumeMapper.cxx
+++ VTK/Rendering/vtkVolumeMapper.cxx
@@ -123,6 +123,23 @@ void vtkVolumeMapper::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
 
+  os << indent << "Independent Components: " << 
+    (this->IndependentComponents ? "On\n" : "Off\n");
+  
+  os << indent << "Component Blend Mode: ";
+  switch ( this->ComponentBlendMode )
+    {
+    case vtkVolumeMapper::ComponentBlendAdd: 
+      os << "Add\n"; 
+      break;
+    case vtkVolumeMapper::ComponentBlendMaxOpacity:
+      os << "Maximum Opacity\n";
+      break;
+    default:
+      os << "Unknown\n";
+      break;
+    }
+  
   os << indent << "Cropping: " << (this->Cropping ? "On\n" : "Off\n");
 
   os << indent << "Cropping Region Planes: " << endl 
