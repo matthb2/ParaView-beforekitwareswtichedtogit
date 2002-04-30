5eec83166a5c00e6cce42c66ca4a18d8a9bf631b
Sebastien Barre <sebastien.barre@kitware.com>
2002-04-30 21:51:15 +0000

    ENH: get rid of FindPokedCamera (was only used for style's CurrentCamera)

--- VTK/Rendering/vtkRenderWindowInteractor.cxx
+++ VTK/Rendering/vtkRenderWindowInteractor.cxx
@@ -441,16 +441,6 @@ vtkRenderer* vtkRenderWindowInteractor::FindPokedRenderer(int x,int y)
 }
 
 //----------------------------------------------------------------------------
-vtkCamera* vtkRenderWindowInteractor::FindPokedCamera(int x,int y) 
-{
-  vtkRenderer *aren = this->FindPokedRenderer(x,y);
-  if (aren != NULL) 
-    {
-    return aren->GetActiveCamera();
-    }
-  return NULL;
-}
-
 void vtkRenderWindowInteractor::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
