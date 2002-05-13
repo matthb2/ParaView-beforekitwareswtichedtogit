ea13e040f713a86a2053cc1bec0e05e1ca0c302b
Charles Law <charles.law@kitware.com>
2002-05-13 14:23:58 +0000

    Record how long it takes to build display lists.  It can be a long time.

--- VTK/Rendering/vtkOpenGLPolyDataMapper.cxx
+++ VTK/Rendering/vtkOpenGLPolyDataMapper.cxx
@@ -228,6 +228,7 @@ void vtkOpenGLPolyDataMapper::RenderPiece(vtkRenderer *ren, vtkActor *act)
     if (!this->ImmediateModeRendering && 
         !this->GetGlobalImmediateModeRendering())
       {
+      vtkTimerLog::MarkStartEvent("Building display list");
       this->ReleaseGraphicsResources(ren->GetRenderWindow());
       this->LastWindow = ren->GetRenderWindow();
       
@@ -237,6 +238,7 @@ void vtkOpenGLPolyDataMapper::RenderPiece(vtkRenderer *ren, vtkActor *act)
 
       noAbort = this->Draw(ren,act);
       glEndList();
+      vtkTimerLog::MarkEndEvent("Building display list");
 
       // Time the actual drawing
       this->Timer->StartTimer();
