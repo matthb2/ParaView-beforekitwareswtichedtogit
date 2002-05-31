68b4b4f95f917247badeed65ba6f1b7ba4948e01
Amy Squillacote <amy.squillacote@kitware.com>
2002-05-31 15:59:46 +0000

    read time range data in UpdateInformation pass

--- VTK/IO/vtkEnSightReader.cxx
+++ VTK/IO/vtkEnSightReader.cxx
@@ -236,7 +236,7 @@ void vtkEnSightReader::Execute()
   char* fileName;
   int filenameNum;
   
-  if (!this->ReadCaseFile())
+  if ( ! this->CaseFileRead)
     {
     vtkErrorMacro("error reading case file");
     return;
@@ -424,6 +424,7 @@ void vtkEnSightReader::Update()
 {
   int i;
   
+  this->UpdateInformation();
   this->Execute();
   
   for (i = 0; i < this->GetNumberOfOutputs(); i++)
@@ -433,6 +434,11 @@ void vtkEnSightReader::Update()
       this->GetOutput(i)->DataHasBeenGenerated();
       }
     }
+}
+//----------------------------------------------------------------------------
+void vtkEnSightReader::UpdateInformation()
+{
+  this->CaseFileRead = this->ReadCaseFile();
 }
 
 //----------------------------------------------------------------------------
