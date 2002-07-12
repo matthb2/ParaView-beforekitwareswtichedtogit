13d896067ac0df299a01d3dc1ebf966b15a3c278
Sebastien Barre <sebastien.barre@kitware.com>
2002-07-12 15:31:20 +0000

    ENH: increase cache size

--- VTK/Rendering/vtkOpenGLFreeTypeTextMapper.cxx
+++ VTK/Rendering/vtkOpenGLFreeTypeTextMapper.cxx
@@ -133,7 +133,7 @@ int IsAntiAliasingRequestedByThisProperty(vtkTextProperty *tprop)
 //----------------------------------------------------------------------------
 // A cache
 
-#define FONT_CACHE_CAPACITY 60
+#define FONT_CACHE_CAPACITY 150
 
 class vtkFontCache
 {
