2d852f8c9e66536e1482e83d1ed50d76fb30ae0e
Charles Law <charles.law@kitware.com>
2002-07-30 12:43:41 +0000

    Stupid mistake: Initializing NumberOfProcesses.

--- VTK/Parallel/vtkCompositeManager.cxx
+++ VTK/Parallel/vtkCompositeManager.cxx
@@ -95,12 +95,13 @@ vtkCompositeManager::vtkCompositeManager()
   this->RenderWindow = NULL;
   this->RenderWindowInteractor = NULL;
   this->Controller = vtkMultiProcessController::GetGlobalController();
+
+  Sleep(15000);
   if (this->Controller)
     {
     this->Controller->Register(this);
     this->NumberOfProcesses = this->Controller->GetNumberOfProcesses();
     }
-  this->NumberOfProcesses = 1;
 
   this->RendererSize[0] = this->RendererSize[1] = 0;
 
