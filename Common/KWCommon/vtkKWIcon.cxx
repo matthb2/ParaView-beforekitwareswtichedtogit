a75511f4e663fed8dde97e7638ef5c7600a65805
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-08-07 12:36:42 +0000

    Add test of headers, add missing PrintSelf and change to vtkTypeRevisionMacro

--- Common/KWCommon/vtkKWIcon.cxx
+++ Common/KWCommon/vtkKWIcon.cxx
@@ -50,6 +50,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //------------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWIcon );
+vtkCxxRevisionMacro(vtkKWIcon, "$Revision$");
 
 vtkKWIcon::vtkKWIcon()
 {
