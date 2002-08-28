a09ecbddc2507a5097b720ee70da5130cca1e762
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-08-28 16:26:55 +0000

    Make PProbeFilter work without controller too

--- VTK/Parallel/vtkPProbeFilter.cxx
+++ VTK/Parallel/vtkPProbeFilter.cxx
@@ -48,18 +48,19 @@ void vtkPProbeFilter::ExecuteInformation()
 //----------------------------------------------------------------------------
 void vtkPProbeFilter::Execute()
 {
-  if ( !this->Controller )
-    {
-    vtkErrorMacro("Controller not set");
-    return;
-    }
   vtkDataSet *output = this->GetOutput();
   //vtkDataObject *input = this->GetInput();
   //vtkDataObject *source = this->GetSource();
   
   this->vtkProbeFilter::Execute();
-  int procid = this->Controller->GetLocalProcessId();
-  int numProcs = this->Controller->GetNumberOfProcesses();
+  int procid = 0;
+  int numProcs = 1;
+  if ( this->Controller )
+    {
+    procid = this->Controller->GetLocalProcessId();
+    numProcs = this->Controller->GetNumberOfProcesses();
+    }
+
   vtkIdType numPoints = this->GetValidPoints()->GetMaxId() + 1;
   if ( procid )
     {
