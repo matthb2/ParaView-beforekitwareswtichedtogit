fb7f3ee42d7b1bb98d8042b8e8e53b0e89fcd101
Charles Law <charles.law@kitware.com>
2002-09-20 11:14:21 +0000

    Small change to maximum resolution

--- VTK/Graphics/vtkQuadricClustering.cxx
+++ VTK/Graphics/vtkQuadricClustering.cxx
@@ -128,11 +128,11 @@ void vtkQuadricClustering::Execute()
     {
     double factor = pow(((double)numDiv/(double)target),0.33333);
     this->NumberOfDivisions[0] = 
-      (int)((float)(this->NumberOfXDivisions)/factor);  
+      (int)(0.5+(float)(this->NumberOfXDivisions)/factor);  
     this->NumberOfDivisions[1] = 
-      (int)((float)(this->NumberOfYDivisions)/factor);  
+      (int)(0.5+(float)(this->NumberOfYDivisions)/factor);  
     this->NumberOfDivisions[2] = 
-      (int)((float)(this->NumberOfZDivisions)/factor);  
+      (int)(0.5+(float)(this->NumberOfZDivisions)/factor);  
     }
   else
     {
