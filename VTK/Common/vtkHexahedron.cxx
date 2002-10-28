bbb373691413bec68b61a4bf51bf9477d00cff3d
Sebastien Barre <sebastien.barre@kitware.com>
2002-10-28 21:35:00 +0000

    ENH: cleanup, move some utility classes/structs outside global space.

--- VTK/Common/vtkHexahedron.cxx
+++ VTK/Common/vtkHexahedron.cxx
@@ -372,7 +372,7 @@ void vtkHexahedron::Contour(float value, vtkDataArray *cellScalars,
                             vtkCellData *outCd)
 {
   static int CASE_MASK[8] = {1,2,4,8,16,32,64,128};
-  VTK_TRIANGLE_CASES *triCase;
+  vtkMarchingCubesTriangleCases *triCase;
   EDGE_LIST  *edge;
   int i, j, index, *vert;
   int v1, v2, newCellId;
@@ -388,7 +388,7 @@ void vtkHexahedron::Contour(float value, vtkDataArray *cellScalars,
       }
     }
 
-  triCase = VTK_TRIANGLE_CASES::GetCases() + index;
+  triCase = vtkMarchingCubesTriangleCases::GetCases() + index;
   edge = triCase->edges;
 
   for ( ; edge[0] > -1; edge += 3 )
--- VTK/Common/vtkVoxel.cxx
+++ VTK/Common/vtkVoxel.cxx
@@ -301,7 +301,7 @@ void vtkVoxel::Contour(float value, vtkDataArray *cellScalars,
                        vtkCellData *inCd, vtkIdType cellId, vtkCellData *outCd)
 {
   static int CASE_MASK[8] = {1,2,4,8,16,32,64,128};
-  VTK_TRIANGLE_CASES *triCase;
+  vtkMarchingCubesTriangleCases *triCase;
   EDGE_LIST  *edge;
   int i, j, index, *vert;
   static int vertMap[8] = { 0, 1, 3, 2, 4, 5, 7, 6 };
@@ -318,7 +318,7 @@ void vtkVoxel::Contour(float value, vtkDataArray *cellScalars,
       }
     }
   
-  triCase = VTK_TRIANGLE_CASES::GetCases() + index;
+  triCase = vtkMarchingCubesTriangleCases::GetCases() + index;
   edge = triCase->edges;
 
   for ( ; edge[0] > -1; edge += 3 )
