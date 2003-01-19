bc19f76a5122f71ed70b7a63d230d0b528c76c2d
Bill Lorensen <lorensen@crd.ge.com>
2003-01-19 22:40:00 +0000

    ERR: trun off z-buffering to overlay text.

--- VTK/Rendering/vtkOpenGLFreeTypeTextMapper.cxx
+++ VTK/Rendering/vtkOpenGLFreeTypeTextMapper.cxx
@@ -379,6 +379,7 @@ void vtkOpenGLFreeTypeTextMapper::RenderOverlay(vtkViewport* viewport,
       }
 #endif
     
+    glDepthFunc(GL_ALWAYS);
     // Set the color here since load/render glyphs is done
     // on demand and this color has to be consistent for a given font entry.
     
@@ -433,6 +434,7 @@ void vtkOpenGLFreeTypeTextMapper::RenderOverlay(vtkViewport* viewport,
   glMatrixMode(GL_MODELVIEW);
   glPopMatrix();
   glEnable(GL_LIGHTING);
+  glDepthFunc(GL_LEQUAL);
 }
 
 void vtkOpenGLFreeTypeTextMapper::PrintSelf(ostream& os, vtkIndent indent)
