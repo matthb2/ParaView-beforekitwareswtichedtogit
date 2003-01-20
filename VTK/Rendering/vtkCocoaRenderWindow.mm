51d8c53f49e9aa461fab0486bc7a6ca6dc1dd6bf
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-01-20 15:17:52 +0000

    Index: vtkCocoaRenderWindow.h - Made Start() virtual - Made Frame() virtual - Changed parameter for SetWindowName(const char *); - Made SetWindowInfo() virtual - Made SetParentInfo() virtual - Made MakeCurrent() virtual - Made SetForceMakeCurrent() virtual - Made HideCursor() and ShowCursor() virtual
    Index: vtkCocoaRenderWindow.mm
    - Changed parameter for SetWindowName() to const char* and made the function virtual.
    - Implemented SetWindowName()
    - Implemented ShowCursor() and HideCursor()

--- VTK/Rendering/vtkCocoaRenderWindow.mm
+++ VTK/Rendering/vtkCocoaRenderWindow.mm
@@ -140,12 +140,12 @@ void vtkCocoaRenderWindow::Clean()
     }
 }
 
-void vtkCocoaRenderWindow::SetWindowName( char * _arg )
+void vtkCocoaRenderWindow::SetWindowName( const char * _arg )
 {
   vtkWindow::SetWindowName(_arg);
   if (this->WindowId)
     {
-    //DOCocoaSetWindowText(this->WindowId,this->WindowName);
+    [(vtkCocoaWindow  *)this->WindowId setTitle:[NSString stringWithCString:_arg] ];
     }
 }
 
@@ -397,6 +397,8 @@ void vtkCocoaRenderWindow::WindowInitialize (void)
         glView = [[[vtkCocoaGLView alloc] initWithFrame:glRect] retain];
         [(vtkCocoaWindow *)this->WindowId setvtkCocoaGLView:glView];
 	[(vtkCocoaWindow *)this->WindowId setAcceptsMouseMovedEvents:YES];
+	[(vtkCocoaWindow  *)this->WindowId setTitle:[NSString
+stringWithCString: this->WindowName]];
 	[(vtkCocoaWindow *)this->WindowId setVTKRenderWindow:this];
 	[(vtkCocoaWindow *)this->WindowId setVTKRenderWindowInteractor:0];
 	[glView setVTKRenderWindow:this];
@@ -624,7 +626,7 @@ void vtkCocoaRenderWindow::HideCursor()
     }
   this->CursorHidden = 1;
 
-  //DOCocoa::ShowCursor(!this->CursorHidden);
+  [NSCursor hide];
 }
 
 //----------------------------------------------------------------------------
@@ -636,6 +638,6 @@ void vtkCocoaRenderWindow::ShowCursor()
     }
   this->CursorHidden = 0;
 
-  //DOCocoa::ShowCursor(!this->CursorHidden);
+  [NSCursor show];
 }				   
 
