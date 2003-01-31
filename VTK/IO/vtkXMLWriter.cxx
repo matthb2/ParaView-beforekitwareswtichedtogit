ab27c63398c7dbf8f14e4a20d0c911521e65fc45
Bill Hoffman <bill.hoffman@kitware.com>
2003-01-31 22:06:44 +0000

    BUG: open binary on windows

--- VTK/IO/vtkXMLWriter.cxx
+++ VTK/IO/vtkXMLWriter.cxx
@@ -188,7 +188,11 @@ int vtkXMLWriter::Write()
     }
   
   // Try to open the output file for writing.
-  ofstream outFile(this->FileName);
+#ifdef _WIN32
+  ofstream outFile(this->FileName, ios::out | ios::binary);
+#else
+  ofstream outFile(this->FileName, ios::out);
+#endif
   if(!outFile)
     {
     vtkErrorMacro("Error opening output file \"" << this->FileName << "\"");
