da3389a8c4faa99de5722d8bb58f076fa71e10d7
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-02-06 20:44:02 +0000

    Try to make oScone test to at least run

--- VTK/Rendering/vtkCarbonRenderWindow.cxx
+++ VTK/Rendering/vtkCarbonRenderWindow.cxx
@@ -789,15 +789,7 @@ void vtkCarbonRenderWindow::Initialize (void)
     return;
     }
 
-  // now initialize the window
-  if (this->OffScreenRendering) // always fails since OffScreen not done yet
-    {
-    this->InitializeApplication();
-    }
-  else
-    {
-    this->WindowInitialize();
-    }
+  this->WindowInitialize();
 }
 
 //--------------------------------------------------------------------------
