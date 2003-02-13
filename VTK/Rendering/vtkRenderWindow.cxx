5e869e82b1409abe66fea837fec9a2ca11a5c7db
Ken Martin <ken.martin@kitware.com>
2003-02-13 18:25:56 +0000

    fixed print self defect

--- VTK/Rendering/vtkRenderWindow.cxx
+++ VTK/Rendering/vtkRenderWindow.cxx
@@ -672,6 +672,7 @@ void vtkRenderWindow::PrintSelf(ostream& os, vtkIndent indent)
   this->Superclass::PrintSelf(os,indent);
 
   os << indent << "Borders: " << (this->Borders ? "On\n":"Off\n");
+  os << indent << "IsPicking: " << (this->IsPicking ? "On\n":"Off\n");
   os << indent << "Double Buffer: " << (this->DoubleBuffer ? "On\n":"Off\n");
   os << indent << "Full Screen: " << (this->FullScreen ? "On\n":"Off\n");
   os << indent << "Renderers:\n";
