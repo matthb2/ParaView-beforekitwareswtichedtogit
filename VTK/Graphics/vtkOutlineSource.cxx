ccf1de137df28e129c28d75700f0e7fbd2739edb
John Biddiscombe <jbiddiscombe@skippingmouse.co.uk>
2003-04-01 08:24:23 +0000

    ERR: Printself defects. Also make sure Corners ivar is initialized to something.

--- VTK/Graphics/vtkOutlineSource.cxx
+++ VTK/Graphics/vtkOutlineSource.cxx
@@ -28,10 +28,14 @@ vtkStandardNewMacro(vtkOutlineSource);
 //----------------------------------------------------------------------------
 vtkOutlineSource::vtkOutlineSource()
 {
-  for (int i=0; i<3; i++) 
+  for (int i=0; i<3; i++)
     {
     this->Bounds[2*i] = -1.0;
     this->Bounds[2*i+1] = 1.0;
+    }
+  for (int i=0; i<24; i++)
+    {
+    this->Corners[i] = 0.0;
     }
   this->BoxType = 0;
 }
@@ -130,8 +134,16 @@ void vtkOutlineSource::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
 
-  os << indent << "Bounds: (" << this->Bounds[0] << ", " 
+  os << indent << "BoxType: " << this->BoxType << "\n";
+  os << indent << "Bounds: (" << this->Bounds[0] << ", "
      << this->Bounds[1] << ") (" << this->Bounds[2] << ") ("
      << this->Bounds[3] << ") (" << this->Bounds[4] << ") ("
      << this->Bounds[5] << ")\n";
+  os << indent << "Corners: (";
+  for (int i=0; i<24; i++)
+    {
+    os << this->Corners[i];
+    }
+  os << ")\n";
+
 }
