4c7e17f54daa332321b47b288b19f6960e14fe4c
Jerry Clarke <clarke@arl.army.mil>
2003-04-14 17:16:42 +0000

    change "IF" statement for CTH-like data

--- Utilities/Xdmf/vtk/vtkMyXdmfReader.cxx
+++ Utilities/Xdmf/vtk/vtkMyXdmfReader.cxx
@@ -565,8 +565,10 @@ void vtkMyXdmfReader::Execute()
       XdmfArray *values = this->FormatMulti->ElementToArray( 
         dataNode, this->DataDescription );
       */
-    if( !XDMF_WORD_CMP(NodeType, "DataTransform") &&
-        ( this->Grid->GetTopologyType() == XDMF_3DRECTMESH ) ){
+    // if( !XDMF_WORD_CMP(NodeType, "DataTransform") &&
+    //   ( this->Grid->GetTopologyType() == XDMF_3DRECTMESH ) ){
+    if( XDMF_WORD_CMP(NodeType, "DataStructure") &&
+	( this->Grid->GetTopologyType() == XDMF_3DRECTMESH )){
 	// Only works for the CTH-like special case
         values = this->FormatMulti->ElementToArray(dataNode, this->DataDescription);
     } else {
