704897b2ec4e20cdc7a19ad4f19ad3e7783cfa4b
Brad King <brad.king@kitware.com>
2003-05-05 13:45:23 +0000

    ENH: Added progress implementation to rest of XML serial readers.

--- VTK/IO/vtkXMLReader.cxx
+++ VTK/IO/vtkXMLReader.cxx
@@ -717,11 +717,12 @@ void vtkXMLReader::SetProgressRange(float* range, int curStep, int numSteps)
 }
 
 //----------------------------------------------------------------------------
-void vtkXMLReader::SetProgressRange(float* range, float* fractions, int step)
+void vtkXMLReader::SetProgressRange(float* range, int curStep,
+                                    float* fractions)
 {
   float width = range[1] - range[0];
-  this->ProgressRange[0] = range[0] + fractions[step]*width;
-  this->ProgressRange[1] = range[0] + fractions[step+1]*width;
+  this->ProgressRange[0] = range[0] + fractions[curStep]*width;
+  this->ProgressRange[1] = range[0] + fractions[curStep+1]*width;
   this->UpdateProgressDiscrete(this->ProgressRange[0]);
 }
 
