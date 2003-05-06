11d5c03881628cb41eb6ab9c47ec4f24fb25eb55
Sebastien Barre <sebastien.barre@kitware.com>
2003-05-06 15:39:41 +0000

    FIX: fixing one case of RGB to HSV conversion (if R = G = B, then sat would be 0, and dividing by sat in all the other case lead to a NAN hue)

--- VTK/Common/vtkMath.cxx
+++ VTK/Common/vtkMath.cxx
@@ -2077,7 +2077,12 @@ void vtkMath::RGBToHSV(float r, float g, float b, float *h, float *s, float *v)
     *v = 0.0;
     return;
     }
-  if (r >= g && g >= b)
+  if (r == g && g == b)
+    {
+    hue = sat = 0.0;
+    val = r;
+    }
+  else if (r >= g && g >= b)
     { // case 0
     val = r;
     lz = g;
