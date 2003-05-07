9501fb77e8f291f94f577040be3fe1c1e02bde14
Bill Lorensen <lorensen@crd.ge.com>
2003-05-07 23:12:19 +0000

    ERR: was not passing through data other than scalars.

--- VTK/Imaging/vtkImageDataStreamer.cxx
+++ VTK/Imaging/vtkImageDataStreamer.cxx
@@ -96,7 +96,8 @@ void vtkImageDataStreamer::UpdateData(vtkDataObject *vtkNotUsed(out))
   this->Progress = 0.0;
   this->InvokeEvent(vtkCommand::StartEvent,NULL);
   output->SetExtent(output->GetUpdateExtent());
-  output->AllocateScalars();
+  //output->AllocateScalars();
+  AllocateOutputData(output);
   
   // now start the loop over the number of pieces
   translator->SetWholeExtent(output->GetUpdateExtent());
