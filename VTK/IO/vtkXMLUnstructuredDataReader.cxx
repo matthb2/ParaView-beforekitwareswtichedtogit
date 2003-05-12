55f624f9292db7b8530408fa5e87214b82d43179
Brad King <brad.king@kitware.com>
2003-05-12 13:39:54 +0000

    BUG: Fixed divide by zero when there are no data.

--- VTK/IO/vtkXMLUnstructuredDataReader.cxx
+++ VTK/IO/vtkXMLUnstructuredDataReader.cxx
@@ -259,6 +259,10 @@ void vtkXMLUnstructuredDataReader::ReadXMLData()
                           this->GetNumberOfPointsInPiece(i) + 
                           this->GetNumberOfCellsInPiece(i));
     }
+  if(fractions[this->EndPiece-this->StartPiece] == 0)
+    {
+    fractions[this->EndPiece-this->StartPiece] = 1;
+    }
   for(i=this->StartPiece; i < this->EndPiece; ++i)
     {
     int index = i-this->StartPiece;
