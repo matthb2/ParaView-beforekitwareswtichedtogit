368cf8a51d0209bcdd6bcf2cc80c38aaa93931f4
Prabhu Ramachandran <prabhu_r@users.sourceforge.net>
2003-06-30 05:26:23 +0000

    ENH: Line width and point size for GL2PS output are multiplied by the appropriate global factors in vtkGL2PSExporter.

--- VTK/Rendering/vtkOpenGLPolyDataMapper2D.cxx
+++ VTK/Rendering/vtkOpenGLPolyDataMapper2D.cxx
@@ -35,6 +35,7 @@
 
 #ifdef VTK_USE_GL2PS
 #include "gl2ps.h"
+#include "vtkGL2PSExporter.h"
 #endif // VTK_USE_GL2PS
 
 #include <math.h>
@@ -268,8 +269,10 @@ void vtkOpenGLPolyDataMapper2D::RenderOverlay(vtkViewport* viewport,
 
   // Set pointsize and linewidth for GL2PS output.
 #ifdef VTK_USE_GL2PS
-  gl2psPointSize(actor->GetProperty()->GetPointSize());
-  gl2psLineWidth(actor->GetProperty()->GetLineWidth());
+  gl2psPointSize(actor->GetProperty()->GetPointSize()*
+                 vtkGL2PSExporter::GetGlobalPointSizeFactor());
+  gl2psLineWidth(actor->GetProperty()->GetLineWidth()*
+                 vtkGL2PSExporter::GetGlobalLineWidthFactor());
 #endif // VTK_USE_GL2PS
 
   // Set the LineWidth
