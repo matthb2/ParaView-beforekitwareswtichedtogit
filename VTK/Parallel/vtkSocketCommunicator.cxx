d09e119a047ee7236ee5da337f45665a5d056e90
Berk Geveci <berk.geveci@kitware.com>
2003-07-08 19:14:53 +0000

    Moved header into unix block

--- VTK/Parallel/vtkSocketCommunicator.cxx
+++ VTK/Parallel/vtkSocketCommunicator.cxx
@@ -24,12 +24,12 @@ PURPOSE.  See the above copyright notice for more information.
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
 #endif
 
-#include <netinet/tcp.h>
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
 #define WSA_VERSION MAKEWORD(1,1)
