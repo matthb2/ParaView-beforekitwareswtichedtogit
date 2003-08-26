111d873f6790fac162e03374e3b4c5a0a03b8494
Chris Volpe <cvolpe@ara.com>
2003-08-26 20:31:25 +0000

    Fixed ambiguous pow() invocation under VS .NET 2003

--- VTK/Rendering/vtkTextActor.cxx
+++ VTK/Rendering/vtkTextActor.cxx
@@ -311,7 +311,7 @@ int vtkTextActor::RenderOpaqueGeometry(vtkViewport *viewport)
           size[0], 
           (size[1] < max_height ? size[1] : max_height));
         // apply non-linear scaling
-        fsize = static_cast<int>(pow(fsize,this->FontScaleExponent)*
+        fsize = static_cast<int>(pow(static_cast<double>(fsize),this->FontScaleExponent)*
           pow(this->FontScaleTarget, 1.0 - this->FontScaleExponent));
         tpropmapper->SetFontSize(fsize);
         }
