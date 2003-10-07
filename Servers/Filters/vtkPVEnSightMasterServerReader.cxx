099e9bf03fac57010c72dd71722b3db71185c19c
Charles Law <charles.law@kitware.com>
2003-10-07 18:30:49 +0000

    Changes to allow paraview to read ensight sos files.

--- Servers/Filters/vtkPVEnSightMasterServerReader.cxx
+++ Servers/Filters/vtkPVEnSightMasterServerReader.cxx
@@ -629,6 +629,15 @@ int vtkPVEnSightMasterServerReader::ParseMasterServerFile()
   // specified in the file and the number of processes to read the
   // data.
   int numProcs = this->Controller->GetNumberOfProcesses();
+
+  // Make sure we have enoght processes to read all the pieces.
+  if (numProcs < numServers)
+    {
+    vtkErrorMacro("Not enough processes (" << numProcs
+                << ") to read all Ensight server files (" 
+                << numServers << ")");
+    }
+  
   this->NumberOfPieces = (numServers < numProcs)? numServers:numProcs;
 
   for (int i = 0; i < this->GetNumberOfOutputs(); i++)
