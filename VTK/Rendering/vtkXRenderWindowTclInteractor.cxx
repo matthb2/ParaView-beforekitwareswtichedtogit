4286e288a4962dfbc1d55fd15fc37dac65e9bbbb
Ken Martin <ken.martin@kitware.com>
2003-11-21 20:19:15 +0000

    fix for unix hopefully (done on win32)

--- VTK/Rendering/vtkXRenderWindowTclInteractor.cxx
+++ VTK/Rendering/vtkXRenderWindowTclInteractor.cxx
@@ -150,15 +150,10 @@ void  vtkXRenderWindowTclInteractor::Start()
     return;
     }
 
-  if ( this->ExitTag )
-    {
-    this->RemoveObserver(this->ExitTag);
-    }
-  
   vtkOldStyleCallbackCommand *cbc = vtkOldStyleCallbackCommand::New();
   cbc->Callback = vtkBreakTclLoop;
   cbc->ClientData = this;
-  this->ExitTag = this->AddObserver(vtkCommand::ExitEvent,cbc);
+  unsigned long ExitTag = this->AddObserver(vtkCommand::ExitEvent,cbc, 0.5);
   cbc->Delete();
   
   this->BreakLoopFlag = 0;
@@ -166,7 +161,7 @@ void  vtkXRenderWindowTclInteractor::Start()
     {
     Tk_DoOneEvent(0);
     }
-  this->RemoveObserver(this->ExitTag);
+  this->RemoveObserver(ExitTag);
 }
 
 // Initializes the event handlers
