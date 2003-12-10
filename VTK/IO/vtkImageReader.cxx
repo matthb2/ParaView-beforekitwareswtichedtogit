82f7b4225a3a665845187b5ddd97634e99aa9497
Amy Squillacote <amy.squillacote@kitware.com>
2003-12-10 18:23:12 +0000

    Set ScalarArrayName in constructor, not in ExecuteData

--- VTK/IO/vtkImageReader.cxx
+++ VTK/IO/vtkImageReader.cxx
@@ -48,6 +48,7 @@ vtkImageReader::vtkImageReader()
   this->Transform = NULL;
   
   this->ScalarArrayName = NULL;
+  this->SetScalarArrayName("ImageFile");
 }
 
 //----------------------------------------------------------------------------
@@ -424,11 +425,6 @@ void vtkImageReader::ExecuteData(vtkDataObject *output)
     }
 
   ext = data->GetExtent();
-
-  if (!this->ScalarArrayName)
-    {
-    this->SetScalarArrayName("ImageFile");
-    }
   data->GetPointData()->GetScalars()->SetName(this->ScalarArrayName);
 
   vtkDebugMacro("Reading extent: " << ext[0] << ", " << ext[1] << ", " 
