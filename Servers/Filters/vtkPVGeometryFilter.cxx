49c3cc53934929c96d0edce0287cb75d99c56b73
Amy Squillacote <amy.squillacote@kitware.com>
2004-01-07 14:07:51 +0000

    unused variable

--- Servers/Filters/vtkPVGeometryFilter.cxx
+++ Servers/Filters/vtkPVGeometryFilter.cxx
@@ -208,7 +208,6 @@ void vtkPVGeometryFilter::ExecuteCellNormals(vtkPolyData *output)
   vtkIdType* cellPtr;
   vtkIdType *pts = 0;
   vtkIdType npts = 0;
-  unsigned char count = 0;
   double polyNorm[3];
   vtkFloatArray* cellNormals = vtkFloatArray::New();
   cellNormals->SetNumberOfComponents(3);
