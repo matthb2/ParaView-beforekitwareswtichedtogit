a5b7fc21ff1b3fc0cb9a876b544fd06b995b6b56
Charles Law <charles.law@kitware.com>
2004-01-15 18:00:53 +0000

    Rebuild colors if alpha changes.

--- VTK/Rendering/vtkMapper.cxx
+++ VTK/Rendering/vtkMapper.cxx
@@ -215,16 +215,19 @@ vtkUnsignedCharArray *vtkMapper::MapScalars(double alpha)
   // Lets try to resuse the old colors.
   if (this->ScalarVisibility && this->Colors)
     {
-    vtkDataArray *scalars = vtkAbstractMapper::
-      GetScalars(this->GetInput(), this->ScalarMode, this->ArrayAccessMode,
-                 this->ArrayId, this->ArrayName, this->ArrayComponent);
-    if (this->GetMTime() < this->Colors->GetMTime() &&
-        scalars && scalars->GetMTime() < this->Colors->GetMTime())
+    if (this->LookupTable && this->LookupTable->GetAlpha() == alpha)
       {
-      return this->Colors;
+      vtkDataArray *scalars = vtkAbstractMapper::
+        GetScalars(this->GetInput(), this->ScalarMode, this->ArrayAccessMode,
+                   this->ArrayId, this->ArrayName, this->ArrayComponent);
+      if (this->GetMTime() < this->Colors->GetMTime() &&
+          scalars && scalars->GetMTime() < this->Colors->GetMTime())
+        {
+        return this->Colors;
+        }
       }
     }
-
+  
   // Get rid of old colors
   if ( this->Colors )
     {
