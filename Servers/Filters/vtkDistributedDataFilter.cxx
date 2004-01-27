011bcf762e38ba4b47f7e032da6a58ebbc9b59f7
Lee Ann Fisk <lafisk@sandia.gov>
2004-01-27 21:24:35 +0000

    1. bug fix - the case where the global node ID array name is a NULL string was causing an error. 2.  k-d tree wasn't dividing space into enough regions (at least one per processor) when the dataset was a very small test data set.  Now in D3 we set the minimum number of cells allowed per spatial region to 2. Normally D3 divides into enough regions so there is at least one spatial region per process.

--- Servers/Filters/vtkDistributedDataFilter.cxx
+++ Servers/Filters/vtkDistributedDataFilter.cxx
@@ -169,6 +169,12 @@ const char *vtkDistributedDataFilter::GetGlobalNodeIdArray(vtkDataSet *set)
      };
   //------------------------------------------------
 
+  if (this->GlobalIdArrayName && (this->GlobalIdArrayName[0] == NULL))
+    {
+    delete [] this->GlobalIdArrayName;
+    this->GlobalIdArrayName = NULL;
+    }
+
   const char *gidArrayName = NULL;
 
   if (this->GlobalIdArrayName)
@@ -431,7 +437,6 @@ void vtkDistributedDataFilter::ExecuteInformation()
     output->SetMaximumNumberOfPieces(-1);
     }
 }
-
 void vtkDistributedDataFilter::Execute()
 {
   vtkDataSet *input  = this->GetInput();
@@ -483,6 +488,7 @@ void vtkDistributedDataFilter::Execute()
     this->Kdtree->SetController(this->Controller);
     this->Kdtree->SetTiming(this->Timing);
     this->Kdtree->SetNumRegionsOrMore(this->NumProcesses);
+    this->Kdtree->SetMinCells(2);
     }
 
   // Stage (1) - use vtkPKdTree to...
@@ -628,7 +634,7 @@ void vtkDistributedDataFilter::Execute()
 
   if (!nodeIdArrayName &&            // we don't have global point IDs
       ((this->GhostLevel > 0) ||     // we need ids for ghost cell computation
-        this->GlobalIdArrayName))    // user requested that we compute ids
+        this->GlobalIdArrayName)) // user requested that we compute ids
     {
     // Create unique global point IDs across processes
 
