b9578d98b405e61eb660c58bb179d045493f0ac1
Andy Cedilnik <andy.cedilnik@kitware.com>
2004-01-28 19:15:37 +0000

    ERR: Remove warning on Visual Studio 7

--- Utilities/Xdmf/vtk/vtkXdmfWriter.cxx
+++ Utilities/Xdmf/vtk/vtkXdmfWriter.cxx
@@ -84,11 +84,11 @@ struct vtkXdmfWriterInternal
     CellType(const CellType& ct) : VTKType(ct.VTKType), NumPoints(ct.NumPoints) {}
     vtkIdType VTKType;
     vtkIdType NumPoints;
-    int operator<(const CellType& ct) const
+    vtkstd_bool operator<(const CellType& ct) const
       {
       return this->VTKType < ct.VTKType || this->VTKType == ct.VTKType && this->NumPoints < ct.NumPoints;
       }
-    int operator==(const CellType& ct) const
+    vtkstd_bool operator==(const CellType& ct) const
       {
       return this->VTKType == ct.VTKType && this->NumPoints == ct.NumPoints;
       }
