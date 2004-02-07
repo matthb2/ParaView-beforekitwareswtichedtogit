c29bc57b774bb177dad17446eb2661aae7e237ce
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-02-07 22:20:15 +0000

    Fix memory deletion

--- VTK/IO/vtkAVSucdReader.cxx
+++ VTK/IO/vtkAVSucdReader.cxx
@@ -801,6 +801,7 @@ void vtkAVSucdReader::ReadNodeData()
         }
       scalars[i]->Delete();
       }
+      delete[] scalars;
     } // end of ASCII read
   vtkDebugMacro( << "End of ReadNodeData()\n");
 }
@@ -908,6 +909,7 @@ void vtkAVSucdReader::ReadCellData()
         }
       scalars[i]->Delete();
       }
+      delete[] scalars;
     } // end of ASCII read
   vtkDebugMacro( << "End of ReadCellData()\n");
 }
