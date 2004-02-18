52daffab3970424a47e98758329d2df03a6463f0
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-02-18 20:30:10 +0000

    BUG: Code should compile now with all compilers

--- VTK/Common/vtkPentagonalPrism.cxx
+++ VTK/Common/vtkPentagonalPrism.cxx
@@ -407,6 +407,7 @@ void vtkPentagonalPrism::Contour(double value, vtkDataArray* cellScalars,
                                  vtkCellData *inCd, vtkIdType cellId,
                                  vtkCellData *outCd)
 {
+  int i;
   double s1, s2;
 
   // subdivide into 5 internal wedges with two points added in the middle
@@ -430,7 +431,7 @@ void vtkPentagonalPrism::Contour(double value, vtkDataArray* cellScalars,
   vtkDataArray *localScalars = this->PointData->GetScalars();
 
  //contour each internal wedge separately
-  for(int i=0; i<5; i++) //for each wedge
+  for(i=0; i<5; i++) //for each wedge
     {
     for(int j=0; j<6; j++)
       {
@@ -456,6 +457,7 @@ void vtkPentagonalPrism::Clip(double value, vtkDataArray *cellScalars,
                               vtkCellData *inCd, vtkIdType cellId, 
                               vtkCellData *outCd, int insideOut)
 {
+  int i;
   double s1, s2;
 
   // subdivide into 5 internal wedges with two points added in the middle
@@ -479,7 +481,7 @@ void vtkPentagonalPrism::Clip(double value, vtkDataArray *cellScalars,
   vtkDataArray *localScalars = this->PointData->GetScalars();
 
   // clip each linear wedge separately
-  for(int i=0; i<5; i++) //for each wedge
+  for(i=0; i<5; i++) //for each wedge
     {
     for(int j=0; j<6; j++)
       {
