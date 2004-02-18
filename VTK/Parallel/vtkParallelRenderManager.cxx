c8272074cc9175a858af6e09fe3dd8a253a99e48
Ken Moreland <kmorel@sandia.gov>
2004-02-18 19:57:11 +0000

    Fixed an ussue with bad timings fouling up automatic image reduction factor.

--- VTK/Parallel/vtkParallelRenderManager.cxx
+++ VTK/Parallel/vtkParallelRenderManager.cxx
@@ -1056,6 +1056,12 @@ void vtkParallelRenderManager::SetImageReductionFactorForUpdateRate(double desir
     }
 
   this->AverageTimePerPixel = (3*this->AverageTimePerPixel + timePerPixel)/4;
+  if (this->AverageTimePerPixel <= 0)
+    {
+    this->AverageTimePerPixel = 0;
+    this->SetImageReductionFactor(1);
+    return;
+    }
 
   double allottedPixelTime = 1.0/desiredUpdateRate - renderTime;
   // Give ourselves at least 15% of render time.
