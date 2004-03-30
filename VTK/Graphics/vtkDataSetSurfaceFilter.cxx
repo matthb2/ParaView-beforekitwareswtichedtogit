5df43fb7bbf859bf8b1813519151a5402460ec4a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-03-30 18:10:33 +0000

    BUG: The pt id where inserted in the reversed order. Thus BackfaceCullingOn was failing

--- VTK/Graphics/vtkDataSetSurfaceFilter.cxx
+++ VTK/Graphics/vtkDataSetSurfaceFilter.cxx
@@ -1032,7 +1032,7 @@ void vtkDataSetSurfaceFilter::UnstructuredGridExecute()
         {
         inPtId = ids[i];
         outPtId = this->GetOutputPointId(inPtId, input, newPts, outputPD); 
-        pts->InsertId(i, outPtId);
+        pts->InsertId(numCellPts/2-i-1, outPtId);
         }
       newPolys->InsertNextCell(pts);
       outputCD->CopyData(cd, cellId, this->NumberOfNewCells++);
