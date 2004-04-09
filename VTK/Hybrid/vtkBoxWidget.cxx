0cb71dcabc33e6fd0bddb3ac769d5074dcc67dce
Ken Martin <ken.martin@kitware.com>
2004-04-09 18:42:48 +0000

    merge from main tree

--- VTK/Hybrid/vtkBoxWidget.cxx
+++ VTK/Hybrid/vtkBoxWidget.cxx
@@ -1243,6 +1243,8 @@ void vtkBoxWidget::SetTransform(vtkTransform* t)
 {
   double *pts = ((vtkDoubleArray *)this->Points->GetData())->GetPointer(0);
   double xIn[3];
+  // make sure the transform is up-to-date before using it
+  t->Update();
 
   // Position the eight points of the box and then update the
   // position of the other handles.
