bef14f30b780ab35370793277b8534d86d8e1cc8
Berk Geveci <berk.geveci@kitware.com>
2004-07-02 19:28:42 +0000

    Improved doc

--- Servers/ServerManager/vtkSMDataTypeDomain.cxx
+++ Servers/ServerManager/vtkSMDataTypeDomain.cxx
@@ -117,6 +117,9 @@ int vtkSMDataTypeDomain::IsInDomain(vtkSMSourceProxy* proxy)
     return 0;
     }
 
+  // Get an actual instance of the same type as the data represented
+  // by the information object. This is later used to check match
+  // with IsA. See the vtkProcessModule for more information.
   vtkDataObject* dobj = pm->GetDataObjectOfType(info->GetDataClassName());
   if (!dobj)
     {
