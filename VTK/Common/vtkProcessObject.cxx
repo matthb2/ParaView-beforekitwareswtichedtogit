de41351193168ef045b5581a1d08cad9b87137f9
Ken Martin <ken.martin@kitware.com>
2004-07-17 16:00:11 +0000

    another fix

--- VTK/Common/vtkProcessObject.cxx
+++ VTK/Common/vtkProcessObject.cxx
@@ -44,6 +44,9 @@ public:
 #endif
     return new vtkProcessObjectDummyData;
     }
+protected:
+  vtkProcessObjectDummyData() {}; 
+  virtual ~vtkProcessObjectDummyData() {}; 
 private:
   vtkProcessObjectDummyData(const vtkProcessObjectDummyData&);
   void operator=(const vtkProcessObjectDummyData&);  // Not implemented.
