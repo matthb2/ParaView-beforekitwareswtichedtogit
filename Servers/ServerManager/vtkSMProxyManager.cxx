659ed2327e9999f0dc7ce09a52de7f7ea8ee9d7f
Ken Martin <ken.martin@kitware.com>
2004-07-24 19:58:02 +0000

    revert

--- Servers/ServerManager/vtkSMProxyManager.cxx
+++ Servers/ServerManager/vtkSMProxyManager.cxx
@@ -348,7 +348,6 @@ void vtkSMProxyManager::SaveState(const char* filename)
   this->SaveState(0, &os, indent);
 }
 
-//---------------------------------------------------------------------------
 void vtkSMProxyManager::SaveState(const char*, ostream* os, vtkIndent indent)
 {
 
