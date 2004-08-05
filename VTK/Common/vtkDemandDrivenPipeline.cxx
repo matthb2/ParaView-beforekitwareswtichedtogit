4b058d77886d97a44c05e7a340998aade875905b
Ken Martin <ken.martin@kitware.com>
2004-08-05 14:15:42 +0000

    added access to PipelineMTime

--- VTK/Common/vtkDemandDrivenPipeline.cxx
+++ VTK/Common/vtkDemandDrivenPipeline.cxx
@@ -74,6 +74,7 @@ vtkDemandDrivenPipeline::~vtkDemandDrivenPipeline()
 void vtkDemandDrivenPipeline::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
+  os << indent << "PipelineMTime: " << this->PipelineMTime << "\n";
 }
 
 //----------------------------------------------------------------------------
