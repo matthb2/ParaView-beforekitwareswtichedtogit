f64c3f050e1790edc4b79b5db96dab82ebecba65
Charles Law <charles.law@kitware.com>
2004-08-09 14:38:43 +0000

    Moving render module into the server manager.

---
+++ Servers/ServerManager/vtkPVDisplay.cxx
@@ -0,0 +1,36 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    $RCSfile$
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPVDisplay.h"
+
+
+//----------------------------------------------------------------------------
+vtkCxxRevisionMacro(vtkPVDisplay, "$Revision$");
+
+//----------------------------------------------------------------------------
+vtkPVDisplay::vtkPVDisplay()
+{
+}
+
+//----------------------------------------------------------------------------
+vtkPVDisplay::~vtkPVDisplay()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkPVDisplay::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
+
