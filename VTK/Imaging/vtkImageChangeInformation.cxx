5b32e585d4c45524d83a2d2412c3520944e6c02f
Ken Martin <ken.martin@kitware.com>
2004-08-17 12:45:23 +0000

    fix tests on old pipeline

--- VTK/Imaging/vtkImageChangeInformation.cxx
+++ VTK/Imaging/vtkImageChangeInformation.cxx
@@ -24,7 +24,6 @@ vtkStandardNewMacro(vtkImageChangeInformation);
 //----------------------------------------------------------------------------
 vtkImageChangeInformation::vtkImageChangeInformation()
 {
-  this->NumberOfRequiredInputs = 2;
   this->CenterImage = 0;
 
   for (int i = 0; i < 3; i++)
