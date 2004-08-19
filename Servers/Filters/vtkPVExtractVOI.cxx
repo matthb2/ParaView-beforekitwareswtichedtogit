45ac775b8b91a6cb1ceb9b73df78f33283f1949a
Brad King <brad.king@kitware.com>
2004-08-19 22:07:07 +0000

    BUG: Added ReportReferences/RemoveReferences method to include internal filters in reference loop checks.

--- Servers/Filters/vtkPVExtractVOI.cxx
+++ Servers/Filters/vtkPVExtractVOI.cxx
@@ -18,6 +18,7 @@
 #include "vtkExtractGrid.h"
 #include "vtkExtractVOI.h"
 #include "vtkExtractRectilinearGrid.h"
+#include "vtkGarbageCollector.h"
 #include "vtkImageData.h"
 #include "vtkInstantiator.h"
 #include "vtkObjectFactory.h"
@@ -46,9 +47,18 @@ vtkPVExtractVOI::vtkPVExtractVOI()
 //----------------------------------------------------------------------------
 vtkPVExtractVOI::~vtkPVExtractVOI()
 {
-  this->ExtractGrid->Delete();
-  this->ExtractVOI->Delete();
-  this->ExtractRG->Delete();
+  if(this->ExtractVOI)
+    {
+    this->ExtractVOI->Delete();
+    }
+  if(this->ExtractGrid)
+    {
+    this->ExtractGrid->Delete();
+    }
+  if(this->ExtractRG)
+    {
+    this->ExtractRG->Delete();
+    }
 }
 
 //----------------------------------------------------------------------------
@@ -273,6 +283,36 @@ void vtkPVExtractVOI::SetSampleRateK(int ratek)
   
   this->SampleRate[2] = ratek;
   this->Modified();
+}
+
+//-----------------------------------------------------------------------------
+void vtkPVExtractVOI::ReportReferences(vtkGarbageCollector* collector)
+{
+  this->Superclass::ReportReferences(collector);
+  collector->ReportReference(this->ExtractVOI, "ExtractVOI");
+  collector->ReportReference(this->ExtractGrid, "ExtractGrid");
+  collector->ReportReference(this->ExtractRG, "ExtractRG");
+}
+
+//-----------------------------------------------------------------------------
+void vtkPVExtractVOI::RemoveReferences()
+{
+  if(this->ExtractVOI)
+    {
+    this->ExtractVOI->Delete();
+    this->ExtractVOI = 0;
+    }
+  if(this->ExtractGrid)
+    {
+    this->ExtractGrid->Delete();
+    this->ExtractGrid = 0;
+    }
+  if(this->ExtractRG)
+    {
+    this->ExtractRG->Delete();
+    this->ExtractRG = 0;
+    }
+  this->Superclass::RemoveReferences();
 }
 
 //----------------------------------------------------------------------------
