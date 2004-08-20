4ffad14728a38e45c897f54f7ace0a5e123426a8
Francois Bertel <francois.bertel@kitware.com>
2004-08-20 13:39:36 +0000

    Added vtkGenericDataSetSource, vtkGenericDataSetToPolyDataFilter and vtkGenericDataSetToUnstructuredGridFilter

---
+++ VTK/Filtering/vtkGenericDataSetToUnstructuredGridFilter.cxx
@@ -0,0 +1,45 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile$
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkGenericDataSetToUnstructuredGridFilter.h"
+
+#include "vtkGenericDataSet.h"
+
+vtkCxxRevisionMacro(vtkGenericDataSetToUnstructuredGridFilter, "$Revision$");
+
+//----------------------------------------------------------------------------
+// Specify the input data or filter.
+void vtkGenericDataSetToUnstructuredGridFilter::SetInput(vtkGenericDataSet *input)
+{
+  this->vtkProcessObject::SetNthInput(0, input);
+}
+
+//----------------------------------------------------------------------------
+// Specify the input data or filter.
+vtkGenericDataSet *vtkGenericDataSetToUnstructuredGridFilter::GetInput()
+{
+  if (this->NumberOfInputs < 1)
+    {
+    return NULL;
+    }
+  
+  return static_cast<vtkGenericDataSet *>(this->Inputs[0]);
+}
+
+//----------------------------------------------------------------------------
+void vtkGenericDataSetToUnstructuredGridFilter::PrintSelf(ostream& os,
+                                                          vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
