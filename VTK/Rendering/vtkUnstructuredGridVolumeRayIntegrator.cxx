a9183058d4b1c6c6e3bc516957b46d4669a3d0de
Ken Moreland <kmorel@sandia.gov>
2004-08-23 14:49:25 +0000

    Removed the ray integration from the ray cast function and moved it into its own class as the two operations are independent.

---
+++ VTK/Rendering/vtkUnstructuredGridVolumeRayIntegrator.cxx
@@ -0,0 +1,34 @@
+// -*- c++ -*-
+
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $RCSfile$
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkUnstructuredGridVolumeRayIntegrator.h"
+
+vtkCxxRevisionMacro(vtkUnstructuredGridVolumeRayIntegrator, "$Revision$");
+
+//----------------------------------------------------------------------------
+
+vtkUnstructuredGridVolumeRayIntegrator::vtkUnstructuredGridVolumeRayIntegrator()
+{
+}
+
+vtkUnstructuredGridVolumeRayIntegrator::~vtkUnstructuredGridVolumeRayIntegrator()
+{
+}
+
+void vtkUnstructuredGridVolumeRayIntegrator::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
