04d82375c28d2147ba351a24bc67817a1f70c99c
Andy Cedilnik <andy.cedilnik@kitware.com>
2004-08-31 15:36:57 +0000

    ERR: Remove warning

--- VTK/Rendering/vtkRenderer.cxx
+++ VTK/Rendering/vtkRenderer.cxx
@@ -218,7 +218,7 @@ void vtkRenderer::Render(void)
   this->PropArrayCount = 0;
   vtkCollectionSimpleIterator pit;
   for ( this->Props->InitTraversal(pit); 
-        aProp = this->Props->GetNextProp(pit); )
+        (aProp = this->Props->GetNextProp(pit)); )
     {
     if ( aProp->GetVisibility() )
       {
