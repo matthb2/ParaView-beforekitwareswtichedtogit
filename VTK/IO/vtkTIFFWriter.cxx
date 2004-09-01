b25b8a87dcf675e961e107e77470f437c9df902f
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-09-01 17:04:39 +0000

    ENH: Fix boolean logic

--- VTK/IO/vtkTIFFWriter.cxx
+++ VTK/IO/vtkTIFFWriter.cxx
@@ -226,9 +226,9 @@ void vtkTIFFWriter::WriteFile(ofstream *, vtkImageData *data,
 
   // take into consideration the scalar type
   if( data->GetScalarType() != VTK_UNSIGNED_CHAR 
-   || data->GetScalarType() != VTK_UNSIGNED_SHORT)
+   && data->GetScalarType() != VTK_UNSIGNED_SHORT)
     {
-    vtkErrorMacro("TIFFWriter only accepts unsigned char scalars!");
+    vtkErrorMacro("TIFFWriter only accepts unsigned char/short scalars!");
     return; 
     }
 
