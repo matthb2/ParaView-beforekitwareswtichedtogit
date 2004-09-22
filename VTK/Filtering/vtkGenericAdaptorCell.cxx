f0612bd45f315e9b4e1fc2408607ae3c1e9c10c3
Francois Bertel <francois.bertel@kitware.com>
2004-09-22 16:34:59 +0000

    Removed useless line

--- VTK/Filtering/vtkGenericAdaptorCell.cxx
+++ VTK/Filtering/vtkGenericAdaptorCell.cxx
@@ -501,7 +501,6 @@ void vtkGenericAdaptorCell::Tessellate(vtkGenericAttributeCollection *attributes
       for(int i=0;i<npts;i++,point+=3,scalar++)
         {
         outpoints->InsertPoint(pts[i], point );
-        pd->InsertTuple(pts[i], scalar);
         tetsScalars->InsertTuple(pts[i], scalar); //FIXME
         }
       }
