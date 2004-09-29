fc5f0a1271d679f3a61b493a8a6aaff064ae49b8
Lee Ann Fisk <lafisk@sandia.gov>
2004-09-29 20:02:31 +0000

    STYLE: Remove compile warnings

--- VTK/Parallel/vtkModelMetadata.cxx
+++ VTK/Parallel/vtkModelMetadata.cxx
@@ -489,14 +489,14 @@ int vtkModelMetadata::GetBlockLocalIndex(int id)
 
   vtkstd::map<int,int>::iterator mapit = blockIdIndex.find(id);
 
-  if (mapit == blockIdIndex.end())
+  int retVal = -1;
+
+  if (mapit != blockIdIndex.end())
     {
-    return -1;
+    retVal = mapit->second;
     }
-  else
-    {
-    return mapit->second;
-    }
+
+  return retVal;
 }
 
 void vtkModelMetadata::SetBlockIds(int *b)
