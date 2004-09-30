28095ba676200802e631303bddb290f78db7722c
Berk Geveci <berk.geveci@kitware.com>
2004-09-30 15:06:04 +0000

    ENH: An empty input or an input without an attribute should not cause an error macro (neither should it cause a core dump)

--- VTK/Patented/vtkSynchronizedTemplates3D.cxx
+++ VTK/Patented/vtkSynchronizedTemplates3D.cxx
@@ -647,7 +647,8 @@ void vtkSynchronizedTemplates3D::ThreadedExecute(vtkImageData *data,
   inScalars = data->GetPointData()->GetScalars(this->InputScalarsSelection);
   if (inScalars == NULL)
     {
-    vtkErrorMacro("No scalars for contouring.");
+    vtkDebugMacro("No scalars for contouring.");
+    return;
     }
   int numComps = inScalars->GetNumberOfComponents();
   
