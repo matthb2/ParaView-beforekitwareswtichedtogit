1c881a4b50339cc984c3e65da258606bf5bd7ac7
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-10-06 20:31:04 +0000

    BUG: Fix Bug #1204 - JA: Isosurface passes scalars if generate scalar is off . The behavior of patented classes is now consistent with the other ones.

--- VTK/Patented/vtkSynchronizedTemplates3D.cxx
+++ VTK/Patented/vtkSynchronizedTemplates3D.cxx
@@ -155,6 +155,11 @@ void vtkSynchronizedTemplates3DInitializeOutput(
     scalars->SetName("Scalars");
     }
   
+  // if we did not ask for scalars to be computed, don't copy them
+  if (!self->GetComputeScalars())
+    {
+    o->GetPointData()->CopyScalarsOff();
+    }
   o->GetPointData()->InterpolateAllocate(input->GetPointData(),
                                          estimatedSize,estimatedSize/2);
   o->GetCellData()->CopyAllocate(input->GetCellData(),
