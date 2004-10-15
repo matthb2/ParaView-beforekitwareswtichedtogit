e885b1c71ddc1ad8ee92658ee963da504a0a5849
Brad King <brad.king@kitware.com>
2004-10-15 20:19:53 +0000

    COMP: Replaced c-style cast with reinterpret_cast to avoid HP warning.

--- VTK/Rendering/vtkFreeTypeUtilities.cxx
+++ VTK/Rendering/vtkFreeTypeUtilities.cxx
@@ -849,7 +849,7 @@ int vtkFreeTypeUtilities::GetBoundingBox(vtkTextProperty *tprop,
       continue;
       }
 
-    bitmap_glyph = (FT_BitmapGlyph)glyph;
+    bitmap_glyph = reinterpret_cast<FT_BitmapGlyph>(glyph);
     bitmap = &bitmap_glyph->bitmap;
 
     if (bitmap->width && bitmap->rows)
@@ -1034,7 +1034,7 @@ int vtkFreeTypeUtilitiesRenderString(
       continue;
       }
 
-    bitmap_glyph = (FT_BitmapGlyph)glyph;
+    bitmap_glyph = reinterpret_cast<FT_BitmapGlyph>(glyph);
     bitmap = &bitmap_glyph->bitmap;
 
     if (bitmap->pixel_mode != FT_PIXEL_MODE_GRAY)
