d1223332d3ac2a944c4ad62aafacac7fe8e07308
Berk Geveci <berk.geveci@kitware.com>
2004-12-01 15:36:02 +0000

    ENH: Added methods to access individual datasets in a multiblock

--- VTK/Filtering/vtkMultiBlockDataSet.cxx
+++ VTK/Filtering/vtkMultiBlockDataSet.cxx
@@ -36,13 +36,30 @@ vtkMultiBlockDataSet::~vtkMultiBlockDataSet()
 }
 
 //----------------------------------------------------------------------------
-void vtkMultiBlockDataSet::AddDataSet(vtkDataObject* data)
+unsigned int vtkMultiBlockDataSet::GetNumberOfDataSets()
+{
+  return this->Internal->DataSets.size();
+}
+
+//----------------------------------------------------------------------------
+vtkDataObject* vtkMultiBlockDataSet::GetDataSet(unsigned int idx)
+{
+  if ( idx >= this->Internal->DataSets.size() )
+    {
+    return 0;
+    }
+  return this->Internal->DataSets[idx];
+}
+
+//----------------------------------------------------------------------------
+unsigned int vtkMultiBlockDataSet::AddDataSet(vtkDataObject* data)
 {
   if (data)
     {
     this->Internal->DataSets.push_back(data);
     this->Modified();
     }
+  return this->Internal->DataSets.size() - 1;
 }
 
 //----------------------------------------------------------------------------
