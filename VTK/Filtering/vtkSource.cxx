ca4755ca38f5a00a0a194e1b66cf8c404e191fb6
Berk Geveci <berk.geveci@kitware.com>
2004-12-07 14:38:07 +0000

    BUG: vtkSource should send unknown requests to superclass

--- VTK/Filtering/vtkSource.cxx
+++ VTK/Filtering/vtkSource.cxx
@@ -519,7 +519,7 @@ void vtkSource::SetNumberOfOutputPorts(int n)
 
 //----------------------------------------------------------------------------
 int vtkSource::ProcessRequest(vtkInformation* request,
-                              vtkInformationVector**,
+                              vtkInformationVector** inputVector,
                               vtkInformationVector* outputVector)
 {
   if(request->Has(vtkDemandDrivenPipeline::REQUEST_DATA_OBJECT()))
@@ -714,7 +714,7 @@ int vtkSource::ProcessRequest(vtkInformation* request,
       }
     return 1;
     }
-  return 0;
+  return this->Superclass::ProcessRequest(request, inputVector, outputVector);
 }
 
 //----------------------------------------------------------------------------
