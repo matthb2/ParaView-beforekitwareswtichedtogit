316c1617dfa8877f4a087c1f180c82850d650df5
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-12-08 21:58:04 +0000

    STYLE: Reuse VTK macro when possible (Get, Clamp...). Fix some comments.

--- VTK/Filtering/vtkAttributesErrorMetric.cxx
+++ VTK/Filtering/vtkAttributesErrorMetric.cxx
@@ -36,18 +36,6 @@ vtkAttributesErrorMetric::~vtkAttributesErrorMetric()
 }
 
 //-----------------------------------------------------------------------------
-// Description:
-// Set the relative attribute accuracy to `value'. See
-// GetAttributeTolerance() for details.
-// \pre valid_range_value: value>0 && value<1
-void vtkAttributesErrorMetric::SetAttributeTolerance(double value)
-{
-  assert("pre: valid_range_value" && value>0 && value<1);
-  this->AttributeTolerance=value;
-  this->Modified();
-}
-
-//-----------------------------------------------------------------------------
 int vtkAttributesErrorMetric::NeedEdgeSubdivision(double *leftPoint,
                                                   double *midPoint,
                                                   double *rightPoint,
