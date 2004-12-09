a2df71dd66e606a85b5fb8b0084b035340cc10c4
David Thompson <dcthomp@sandia.gov>
2004-12-09 23:16:16 +0000

    BUG: Added a missing "break" statement to a switch

--- VTK/Graphics/vtkMeshQuality.cxx
+++ VTK/Graphics/vtkMeshQuality.cxx
@@ -158,6 +158,7 @@ void vtkMeshQuality::Execute()
       break;
     case VTK_QUALITY_EDGE_RATIO:
       TetQuality = TetEdgeRatio;
+      break;
     default:
       TetQuality = NULL;
       break;
