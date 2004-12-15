8e3d827149ffae536e018d74fd588c43df1bb377
Brad King <brad.king@kitware.com>
2004-12-15 15:33:20 +0000

    ENH: Renaming ReplaceText to TextReplace to avoid windows name mangling.

--- Common/KWCommon/vtkCornerAnnotation.cxx
+++ Common/KWCommon/vtkCornerAnnotation.cxx
@@ -97,7 +97,7 @@ void vtkCornerAnnotation::ReleaseGraphicsResources(vtkWindow *win)
 }
 
 //----------------------------------------------------------------------------
-void vtkCornerAnnotation::ReplaceText(vtkImageActor *ia,
+void vtkCornerAnnotation::TextReplace(vtkImageActor *ia,
                                       vtkImageMapToWindowLevelColors *wl)
 {
   int i;
@@ -277,7 +277,7 @@ int vtkCornerAnnotation::RenderOpaqueGeometry(vtkViewport *viewport)
     vtkDebugMacro(<<"Rebuilding text");
     
     // Replace text
-    this->ReplaceText(ia, wl);
+    this->TextReplace(ia, wl);
     
     // Get the viewport size in display coordinates
     this->LastSize[0] = vSize[0];
