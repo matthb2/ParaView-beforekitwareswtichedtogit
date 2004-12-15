4c222d7a0c13e575aaa8bc9ba5394edfe43297ee
Charles Law <charles.law@kitware.com>
2004-12-15 16:46:50 +0000

    BUG: Trying to quickly fix a compile problem.

--- Servers/Filters/vtkCTHAMRContour.cxx
+++ Servers/Filters/vtkCTHAMRContour.cxx
@@ -135,7 +135,7 @@ void vtkCTHAMRContour::Execute()
   this->UpdateProgress(.95);
   output = this->GetOutput();
  
-  if (tmp->GetNumberOfInputConnections() > 0)
+  if (tmp->GetNumberOfInputConnections(0) > 0)
     {
     vtkTimerLog::MarkStartEvent("BlockAppend");               
     tmp->Update();
