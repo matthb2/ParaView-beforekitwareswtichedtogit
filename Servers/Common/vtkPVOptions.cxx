a500a97ed286b03ba419210d4728823c8873e5b4
Bill Hoffman <bill.hoffman@kitware.com>
2005-01-10 14:04:59 +0000

    BUG: fix unitialized ivar

--- Servers/Common/vtkPVOptions.cxx
+++ Servers/Common/vtkPVOptions.cxx
@@ -61,7 +61,8 @@ vtkPVOptions::vtkPVOptions()
   this->UseRenderingGroup = 0;
   this->GroupFileName = 0;
   this->XMLConfigFile = 0;
-
+  this->ParaViewDataName = 0;
+  
   this->ClientRenderServer = 0;
   this->ConnectRenderToData = 0;
   this->ConnectDataToRender = 0;
