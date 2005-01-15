0d777159edb67f512fb9bfd2b2638c2ea1d86b49
Francois Bertel <francois.bertel@kitware.com>
2005-01-15 15:28:52 +0000

    ENH: 1. Tessellation is now skip if the geometry and all the attributes of a cell are linear (a generic hexahedron will generate a linear hexahedron); 2. Tessellation now works with complex cells, not only simplices.

--- VTK/GenericFiltering/vtkGenericGeometryFilter.cxx
+++ VTK/GenericFiltering/vtkGenericGeometryFilter.cxx
@@ -311,7 +311,7 @@ void vtkGenericGeometryFilter::Execute()
               {
               cell->Tessellate(input->GetAttributes(), input->GetTessellator(),
                                newPts, locator,cellArray, this->internalPD,
-                               outputPD, outputCD); //newScalars );
+                               outputPD, outputCD,0); //newScalars );
               }
           break;
         case 3:
