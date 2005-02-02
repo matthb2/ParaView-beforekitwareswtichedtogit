f6ea3f71c64b13eb2c0c22290fa106d6d1d44241
Brian Wylie <bnwylie@sandia.gov>
2005-02-02 15:20:16 +0000

    BUG:   Added ShowActualBounds to PrintSelf

--- VTK/Hybrid/vtkCubeAxesActor2D.cxx
+++ VTK/Hybrid/vtkCubeAxesActor2D.cxx
@@ -785,6 +785,7 @@ void vtkCubeAxesActor2D::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "Label Format: " << this->LabelFormat << "\n";
   os << indent << "Font Factor: " << this->FontFactor << "\n";
   os << indent << "Inertia: " << this->Inertia << "\n";
+  os << indent << "ShowActualBounds: " << this->ShowActualBounds << "\n";
   os << indent << "Corner Offset: " << this->CornerOffset << "\n";
   os << indent << "UseRanges: " << (this->UseRanges ? "On" : "Off") << "\n";
   os << indent << "Ranges: "
