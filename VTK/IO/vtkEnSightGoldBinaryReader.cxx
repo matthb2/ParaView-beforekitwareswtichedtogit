4f5dcee091aeb54ea49fdb7dfaf54192c035c39d
Ken Martin <ken.martin@kitware.com>
2005-02-16 19:42:56 +0000

    BUG: better Ensight file support and compatibility

--- VTK/IO/vtkEnSightGoldBinaryReader.cxx
+++ VTK/IO/vtkEnSightGoldBinaryReader.cxx
@@ -218,6 +218,10 @@ int vtkEnSightGoldBinaryReader::ReadGeometryFile(const char* fileName, int timeS
     
     this->ReadLine(line); // part description line
     char *name = strdup(line);
+    if (strncmp(line, "interface", 9) == 0)
+      {
+      return 1; // ignore it and move on
+      }
     this->ReadLine(line);
     
     if (strncmp(line, "block", 5) == 0)
@@ -2893,6 +2897,12 @@ int vtkEnSightGoldBinaryReader::CreateUnstructuredGridOutput(int partId,
       }
     else if (strncmp(line, "END TIME STEP", 13) == 0)
       {
+      return 1;
+      }
+    else if (this->IS->fail())
+      {
+      //May want consistency check here?
+      //vtkWarningMacro("EOF on geometry file");
       return 1;
       }
     else
