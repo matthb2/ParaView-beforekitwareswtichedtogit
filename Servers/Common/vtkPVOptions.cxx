c60536ebde4293bd10b65f41b61678ec3839e44a
Amy Squillacote <amy.squillacote@kitware.com>
2005-02-18 15:50:55 +0000

    ENH: adding an abbreviation (-sh) for the --server-host command-line option

--- Servers/Common/vtkPVOptions.cxx
+++ Servers/Common/vtkPVOptions.cxx
@@ -169,7 +169,7 @@ void vtkPVOptions::Initialize()
                            "Tell the application to enable stereo rendering"
                            " (only when running on a single process).",
                            vtkPVOptions::PVCLIENT | vtkPVOptions::PARAVIEW);
-  this->AddArgument("--server-host", "-dsh", &this->ServerHostName,
+  this->AddArgument("--server-host", "-sh", &this->ServerHostName,
                     "Tell the client the host name of the data server.",
                     vtkPVOptions::PVCLIENT);
   this->AddArgument("--data-server-host", "-dsh", &this->DataServerHostName,
