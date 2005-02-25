00e20f353f1f31c1a87c35faa00e0620bff80218
Ken Martin <ken.martin@kitware.com>
2005-02-25 17:57:12 +0000

    BUG: singular matrix should not force a warning

--- VTK/Common/vtkMatrix4x4.cxx
+++ VTK/Common/vtkMatrix4x4.cxx
@@ -157,7 +157,6 @@ void vtkMatrix4x4::Invert(const double inElements[16],
   det = vtkMatrix4x4::Determinant(inElements);
   if ( det == 0.0 ) 
     {
-    vtkGenericWarningMacro(<< "Singular matrix, no inverse!" );
     return;
     }
 
