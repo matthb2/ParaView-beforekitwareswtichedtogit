a3032e236cc6077aba72ebc0725b46b69ba9c24a
Berk Geveci <berk.geveci@kitware.com>
2005-03-10 19:37:08 +0000

    BUG: Should use the received length to receive the actual array.

--- Servers/Filters/vtkRedistributePolyData.cxx
+++ Servers/Filters/vtkRedistributePolyData.cxx
@@ -2730,7 +2730,7 @@ int vtkRedistributePolyData::DoubleCheckArrays(vtkPolyData* input)
     int* zeroSanity;
     this->Controller->Receive(&zeroLength, 1, 0, 77431);
     zeroSanity = new int[zeroLength];
-    this->Controller->Receive(zeroSanity, length, 0, 77432);
+    this->Controller->Receive(zeroSanity, zeroLength, 0, 77432);
     // Compare
     if (length != zeroLength)
       {
