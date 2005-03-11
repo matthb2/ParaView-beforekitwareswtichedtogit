7c7ec806168d7a384f27d8e0e1b59c93891e1ef8
Sebastien Barre <sebastien.barre@kitware.com>
2005-03-11 20:19:08 +0000

    ENH: remove deps to vtkKWMath. Move some funcs back to vtkMath, vtkPiecewiseFunction, vtkColorTransferFunction, etc.

--- VTK/Filtering/vtkPiecewiseFunction.cxx
+++ VTK/Filtering/vtkPiecewiseFunction.cxx
@@ -547,6 +547,53 @@ double *vtkPiecewiseFunction::GetRange()
   return this->FunctionRange;
 }
 
+int vtkPiecewiseFunction::AdjustRange(double range[2])
+{
+  if (!range)
+    {
+    return 0;
+    }
+
+  double *function_range = this->GetRange();
+  
+  // Make sure we have points at each end of the range
+
+  if (function_range[0] < range[0])
+    {
+    this->AddPoint(range[0], this->GetValue(range[0]));
+    }
+  else
+    {
+    this->AddPoint(range[0], this->GetValue(function_range[0]));
+    }
+
+  if (function_range[1] > range[1])
+    {
+    this->AddPoint(range[1], this->GetValue(range[1]));
+    }
+  else
+    {
+    this->AddPoint(range[1], this->GetValue(function_range[1]));
+    }
+
+  // Remove all points out-of-range
+
+  int func_size = this->GetSize();
+  double *func_ptr = this->GetDataPointer();
+  
+  int i;
+  for (i = func_size - 1; i >= 0; i--)
+    {
+    double x = func_ptr[i * 2];
+    if (x < range[0] || x > range[1])
+      {
+      this->RemovePoint(x);
+      }
+    }
+
+  return 1;
+}
+
 // Returns a table of function values evaluated at regular intervals
 void vtkPiecewiseFunction::GetTable( double x1, double x2, int size,
                                      double* table, int stride )
