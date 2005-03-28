2a18127a8c5b63faf905b4234d6f1624e88cd012
Ken Moreland <kmorel@sandia.gov>
2005-03-28 20:53:14 +0000

    STYLE: Print out ExtensionsString in PrintSelf.

--- VTK/Rendering/vtkOpenGLExtensionManager.cxx
+++ VTK/Rendering/vtkOpenGLExtensionManager.cxx
@@ -77,6 +77,8 @@ void vtkOpenGLExtensionManager::PrintSelf(ostream &os, vtkIndent indent)
 
   os << indent << "RenderWindow: (" << this->RenderWindow << ")" << endl;
   os << indent << "BuildTime: " << this->BuildTime << endl;
+  os << indent << "ExtensionsString: "
+     << (this->ExtensionsString ? this->ExtensionsString : "(NULL)") << endl;
 }
 
 void vtkOpenGLExtensionManager::SetRenderWindow(vtkRenderWindow *renwin)
