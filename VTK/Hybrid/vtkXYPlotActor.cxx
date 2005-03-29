d61c8fd244ef54d81ef88da15a96ed04d95940a9
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-03-29 15:48:09 +0000

    ENH: Fix PrintSelf

--- VTK/Hybrid/vtkXYPlotActor.cxx
+++ VTK/Hybrid/vtkXYPlotActor.cxx
@@ -943,6 +943,11 @@ void vtkXYPlotActor::PrintSelf(ostream& os, vtkIndent indent)
      << this->LegendPosition2[1] << ")\n";
 
   os << indent << "Glyph Size: " << this->GlyphSize << endl;
+
+  os << indent << "Legend Actor:";
+  this->LegendActor->PrintSelf( os << endl, i2);
+  os << indent << "Glyph Source:";
+  this->GlyphSource->PrintSelf( os << endl, i2);
 }
 
 //----------------------------------------------------------------------------
