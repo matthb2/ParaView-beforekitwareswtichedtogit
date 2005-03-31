00bd13270f40185cc654b4de6f8f6eb5f1778c78
Berk Geveci <berk.geveci@kitware.com>
2005-03-31 20:40:45 +0000

    BUG: Even if the input if not vtkHierarchicalBoxDataSet, remove the blanking on the last level

--- VTK/Graphics/vtkHierarchicalDataExtractLevel.cxx
+++ VTK/Graphics/vtkHierarchicalDataExtractLevel.cxx
@@ -22,6 +22,7 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkInformation.h"
 #include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
+#include "vtkUniformGrid.h"
 
 vtkCxxRevisionMacro(vtkHierarchicalDataExtractLevel, "$Revision$");
 vtkStandardNewMacro(vtkHierarchicalDataExtractLevel);
@@ -247,7 +248,18 @@ int vtkHierarchicalDataExtractLevel::RequestData(
       {
       hbds->SetRefinementRatio(level, ihbds->GetRefinementRatio(level));
       }
-    hbds->GenerateVisibilityArrays();
+    }
+
+  // Last level should not be blanked (uniform grid only)
+  unsigned int numDataSets = output->GetNumberOfDataSets(numLevels-1);
+  for (unsigned int dataSet=0; dataSet<numDataSets; dataSet++)
+    {
+    vtkUniformGrid* ug = vtkUniformGrid::SafeDownCast(
+      output->GetDataSet(numLevels-1, dataSet));
+    if (ug)
+      {
+      ug->SetCellVisibilityArray(0);
+      }
     }
 
   return 1;
