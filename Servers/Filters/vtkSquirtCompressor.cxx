92aa665a8c9f5348fde206a9f9f54850e2f7b70a
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2005-05-04 13:37:47 +0000

    BUG: fixed printself error

--- Servers/Filters/vtkSquirtCompressor.cxx
+++ Servers/Filters/vtkSquirtCompressor.cxx
@@ -145,6 +145,6 @@ int vtkSquirtCompressor::DecompressData()
 //-----------------------------------------------------------------------------
 void vtkSquirtCompressor::PrintSelf(ostream& os, vtkIndent indent)
 {
-  this->PrintSelf(os, indent);
+  this->Superclass::PrintSelf(os, indent);
   os << indent << "SquirtLevel: " << this->SquirtLevel << endl;
 }
