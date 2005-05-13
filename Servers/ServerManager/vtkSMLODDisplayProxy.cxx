c0f1c038b7bad5f9d1342ff9727ebcc9b8531ce3
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2005-05-13 18:22:15 +0000

    BUG: fixed crash when CacheUpdate is called before objects are created

--- Servers/ServerManager/vtkSMLODDisplayProxy.cxx
+++ Servers/ServerManager/vtkSMLODDisplayProxy.cxx
@@ -242,6 +242,11 @@ void vtkSMLODDisplayProxy::CreateVTKObjects(int numObjects)
 //-----------------------------------------------------------------------------
 void vtkSMLODDisplayProxy::CacheUpdate(int idx, int total)
 {
+  if (!this->LODMapperProxy)
+    {
+    vtkErrorMacro("Objects not created yet!");
+    return;
+    }
   this->Superclass::CacheUpdate(idx, total);
   vtkClientServerStream stream;
   stream
