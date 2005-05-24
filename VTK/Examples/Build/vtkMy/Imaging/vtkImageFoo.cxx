7ea5d647fd6a0e3912e3c02f16b948cc265cccf8
David Gobbi <david.gobbi@gmail.com>
2005-05-24 11:12:54 +0000

    COMP: change increments to vtkIdType

--- VTK/Examples/Build/vtkMy/Imaging/vtkImageFoo.cxx
+++ VTK/Examples/Build/vtkMy/Imaging/vtkImageFoo.cxx
@@ -71,8 +71,8 @@ void vtkImageFooExecute(vtkImageFoo *self,
 
   int idxR, idxY, idxZ;
   int maxY, maxZ;
-  int inIncX, inIncY, inIncZ;
-  int outIncX, outIncY, outIncZ;
+  vtkIdType inIncX, inIncY, inIncZ;
+  vtkIdType outIncX, outIncY, outIncZ;
   int rowLength;
 
   unsigned long count = 0;
