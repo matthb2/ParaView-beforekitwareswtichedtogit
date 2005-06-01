101c7e5d62e6cfc702433780618db2f26a77c2e4
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2005-06-01 18:20:01 +0000

    BUG: the IgnorePlaceWidget flag must be set to 1 while saving SM state for PVEE

--- Servers/ServerManager/vtkSM3DWidgetProxy.cxx
+++ Servers/ServerManager/vtkSM3DWidgetProxy.cxx
@@ -21,6 +21,7 @@
 #include "vtkRenderer.h"
 #include "vtkInteractorObserver.h"
 #include "vtkSMRenderModuleProxy.h"
+#include "vtkSMIntVectorProperty.h"
 //----------------------------------------------------------------------------
 vtkCxxRevisionMacro(vtkSM3DWidgetProxy, "$Revision$");
 
@@ -177,6 +178,18 @@ void vtkSM3DWidgetProxy::SaveInBatchScript(ofstream *file)
     *file << endl;
     }
 
+}
+
+//----------------------------------------------------------------------------
+void vtkSM3DWidgetProxy::SaveState(const char* name, ostream* file,
+  vtkIndent indent)
+{
+  vtkSMIntVectorProperty* ivp = vtkSMIntVectorProperty::SafeDownCast(
+    this->GetProperty("IgnorePlaceWidgetChanges"));
+  int old_ipc = ivp->GetElement(0);
+  ivp->SetElement(0, 1);
+  this->Superclass::SaveState(name, file, indent);
+  ivp->SetElement(0, old_ipc);
 }
 
 //----------------------------------------------------------------------------
