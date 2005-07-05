a51b680c0ff549ae1db19d7f25eae437796cbaf8
Berk Geveci <berk.geveci@kitware.com>
2005-07-05 13:23:01 +0000

    ENH: New key of vtkIdType

--- VTK/Filtering/vtkInformation.cxx
+++ VTK/Filtering/vtkInformation.cxx
@@ -20,6 +20,7 @@
 #include "vtkInformationDoubleVectorKey.h"
 #include "vtkInformationExecutivePortKey.h"
 #include "vtkInformationExecutivePortVectorKey.h"
+#include "vtkInformationIdTypeKey.h"
 #include "vtkInformationInformationKey.h"
 #include "vtkInformationInformationVectorKey.h"
 #include "vtkInformationIntegerKey.h"
@@ -323,6 +324,7 @@ int vtkInformation::Has(vtkInformationRequestKey* key)
     {                                                                       \
     return key->Has(this);                                                  \
     }
+VTK_INFORMATION_DEFINE_SCALAR_PROPERTY(IdType, vtkIdType);
 VTK_INFORMATION_DEFINE_SCALAR_PROPERTY(Integer, int);
 VTK_INFORMATION_DEFINE_SCALAR_PROPERTY(Double, double);
 VTK_INFORMATION_DEFINE_SCALAR_PROPERTY(UnsignedLong, unsigned long);
