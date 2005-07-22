ec7f05430623145fcb21c2bedd166ee990b5af85
Amy Squillacote <amy.squillacote@kitware.com>
2005-07-22 16:12:18 +0000

    BUG: fix for bug #1800 - For data sets with multiple time steps, ParaView was only showing the first time step when you tried to animate over the time steps.

--- VTK/Parallel/vtkTransmitPolyDataPiece.cxx
+++ VTK/Parallel/vtkTransmitPolyDataPiece.cxx
@@ -132,11 +132,15 @@ int vtkTransmitPolyDataPiece::RequestData(
     outInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_PIECE_NUMBER());
   int updateNumPieces =
     outInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_NUMBER_OF_PIECES());
-  unsigned long outMTime =
-    outInfo->Get(vtkDemandDrivenPipeline::PIPELINE_MODIFIED_TIME());
+  vtkDemandDrivenPipeline* ddp =
+    vtkDemandDrivenPipeline::SafeDownCast(this->GetExecutive());
 
-  // Just use the buffer if possible.
-  if (outMTime < this->Buffer->GetMTime()
+  // Check the pipline mtime to see if we are executing only because
+  // the update request changed.  If so and the only change was to
+  // decrease the number of requested ghost levels then do not bother
+  // retransmitting.
+  if (ddp
+      && ddp->GetPipelineMTime() < this->Buffer->GetMTime()
       && updatePiece == this->BufferPiece
       && updateNumPieces == this->BufferNumberOfPieces
       && updateGhostLevel <= this->BufferGhostLevel)
