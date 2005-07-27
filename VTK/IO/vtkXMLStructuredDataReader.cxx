41da91c6d4ecde27bf39455ccb9b1b7347b6f9d0
Brad King <brad.king@kitware.com>
2005-07-27 13:35:22 +0000

    BUG: Fixed computation of number of cells to support dimensions lower than 3.  This addresses bug#2081.

--- VTK/IO/vtkXMLStructuredDataReader.cxx
+++ VTK/IO/vtkXMLStructuredDataReader.cxx
@@ -148,9 +148,21 @@ vtkIdType vtkXMLStructuredDataReader::GetNumberOfPoints()
 //----------------------------------------------------------------------------
 vtkIdType vtkXMLStructuredDataReader::GetNumberOfCells()
 {
-  return (this->CellDimensions[0]*
-          this->CellDimensions[1]*
-          this->CellDimensions[2]);
+  // Special computation of cell count to support dimensions lower
+  // than 3.
+  vtkIdType nCells = 1;
+  for(int i=0; i < 3; ++i)
+    {
+    if(this->CellDimensions[i] == -1)
+      {
+      return 0;
+      }
+    if(this->CellDimensions[i] > 0)
+      {
+      nCells *= this->CellDimensions[i];
+      }
+    }
+  return nCells;
 }
 
 //----------------------------------------------------------------------------
