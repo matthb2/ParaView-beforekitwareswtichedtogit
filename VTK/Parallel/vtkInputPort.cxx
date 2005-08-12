fc23a087e6def93e787555a0403dbbb9582c83e4
Berk Geveci <berk.geveci@kitware.com>
2005-08-12 20:43:06 +0000

    ENH: Better error reporting

--- VTK/Parallel/vtkInputPort.cxx
+++ VTK/Parallel/vtkInputPort.cxx
@@ -335,11 +335,18 @@ int vtkInputPort::RequestDataObject(
       case VTK_IMAGE_DATA:
         output = vtkImageData::New();
         break;
+      case VTK_STRUCTURED_POINTS:
+        vtkErrorMacro("vtkStructuredPoints are being deprecated. Please use "
+                      "vtkImageData instead");
+        break;
       }
-    output->SetPipelineInformation(info);
-    output->Delete();
-    this->GetOutputPortInformation(0)->Set(
-      vtkDataObject::DATA_EXTENT_TYPE(), output->GetExtentType());
+    if (output)
+      {
+      output->SetPipelineInformation(info);
+      output->Delete();
+      this->GetOutputPortInformation(0)->Set(
+        vtkDataObject::DATA_EXTENT_TYPE(), output->GetExtentType());
+      }
     }
   return 1;
 }
