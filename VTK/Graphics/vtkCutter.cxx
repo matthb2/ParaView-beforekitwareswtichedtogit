813b11a9bc702a091c558a5c5a042bd8c9921d87
Dave Demarle <dave.demarle@kitware.com>
2005-08-12 15:49:05 +0000

    BUG: Cutting rectilinear grids not working in parallel.

--- VTK/Graphics/vtkCutter.cxx
+++ VTK/Graphics/vtkCutter.cxx
@@ -246,8 +246,7 @@ void vtkCutter::StructuredGridCutter(vtkDataSet *dataSetInput,
 }
 
 void vtkCutter::RectilinearGridCutter(vtkDataSet *dataSetInput,
-                                      vtkPolyData *thisOutput,
-                                      vtkInformation *outInfo)
+                                      vtkPolyData *thisOutput)
 {
   vtkRectilinearGrid *input = vtkRectilinearGrid::SafeDownCast(dataSetInput);
   vtkPolyData *output;
@@ -359,7 +358,7 @@ int vtkCutter::RequestData(
     int dim = ((vtkRectilinearGrid*)input)->GetDataDimension();
     if ( dim == 3 ) 
       {
-      this->RectilinearGridCutter(input, output, outInfo);
+      this->RectilinearGridCutter(input, output);
       return 1;
       }
     }
