5b13a4e891d51b31ce6ab0bd925939ec0f0793e9
Lee Ann Fisk <lafisk@sandia.gov>
2005-08-16 20:52:21 +0000

    COMP: fix for Visual Studio unreferenced parameter warning

--- VTK/Parallel/vtkPChacoReader.cxx
+++ VTK/Parallel/vtkPChacoReader.cxx
@@ -448,6 +448,10 @@ int vtkPChacoReader::DivideCells(vtkMultiProcessController *contr,
     mygrid->Delete();
     }
 
+#else
+  source = 0;
+  contr = NULL;
+  output = NULL;
 #endif
   return retVal;
 }
@@ -484,7 +488,10 @@ int vtkPChacoReader::SendGrid(vtkMultiProcessController *c,
     }
 
   delete [] buf;
-
+#else
+  c = NULL;
+  to = 0;
+  grid = NULL;
 #endif
   return retVal;
 }
@@ -518,7 +525,9 @@ vtkUnstructuredGrid *vtkPChacoReader::GetGrid(vtkMultiProcessController *c, int 
     ack = 0;
     contr->Send(&ack, 1, 0, 0x12);
     }
-
+#else
+  c = NULL;
+  from = 0;
 #endif
   return grid;
 }
