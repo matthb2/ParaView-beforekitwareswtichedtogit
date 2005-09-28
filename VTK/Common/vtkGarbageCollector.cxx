e488c865d454558c20ada042ec9525c9cadb93f4
Brad King <brad.king@kitware.com>
2005-09-28 20:22:08 +0000

    BUG: ClassFinalize should set vtkGarbageCollectorSingletonInstance to NULL so that destruction of other singletons that collect VTK objects do not try to defer collection in a deleted singleton.

--- VTK/Common/vtkGarbageCollector.cxx
+++ VTK/Common/vtkGarbageCollector.cxx
@@ -868,8 +868,14 @@ void vtkGarbageCollector::ClassInitialize()
 //----------------------------------------------------------------------------
 void vtkGarbageCollector::ClassFinalize()
 {
-  // We are done with the singleton.
+  // We are done with the singleton.  Delete it and reset the pointer.
+  // Other singletons may still cause garbage collection of VTK
+  // objects, they just will not have the option of deferred
+  // collection.  In order to get it they need only to include
+  // vtkGarbageCollectorManager.h so that this singleton stays around
+  // longer.
   delete vtkGarbageCollectorSingletonInstance;
+  vtkGarbageCollectorSingletonInstance = 0;
 }
 
 //----------------------------------------------------------------------------
