9b4513c967a38c53201b623fe6aa008b3d29e9ca
Berk Geveci <berk.geveci@kitware.com>
2005-09-29 15:13:31 +0000

    ENH: Added new data types. Restructure of composite datasets. vtkMultiGroupDataSet is the new superclass with two subclasses: vtkMultiBlockDataSet and vtkHierarchicalDataSet

--- VTK/Filtering/vtkDemandDrivenPipeline.cxx
+++ VTK/Filtering/vtkDemandDrivenPipeline.cxx
@@ -21,6 +21,8 @@
 #include "vtkDataObject.h"
 #include "vtkDataSet.h"
 #include "vtkGarbageCollector.h"
+#include "vtkMultiBlockDataSet.h"
+#include "vtkMultiGroupDataSet.h"
 #include "vtkHierarchicalBoxDataSet.h"
 #include "vtkHierarchicalDataSet.h"
 #include "vtkInformation.h"
@@ -975,6 +977,14 @@ vtkDataObject* vtkDemandDrivenPipeline::NewDataObject(const char* type)
   else if(strcmp(type, "vtkUnstructuredGrid") == 0)
     {
     return vtkUnstructuredGrid::New();
+    }
+  else if(strcmp(type, "vtkMultiGroupDataSet") == 0)
+    {
+    return vtkMultiGroupDataSet::New();
+    }
+  else if(strcmp(type, "vtkMultiBlockDataSet") == 0)
+    {
+    return vtkMultiBlockDataSet::New();
     }
   else if(strcmp(type, "vtkHierarchicalDataSet") == 0)
     {
