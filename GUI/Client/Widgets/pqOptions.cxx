ea3ddef2167a39fa0295a304d00c7041373a6aca
Clinton Stimpson <clinton@elemtech.com>
2005-11-03 20:27:03 +0000

    STYLE: Fix coding style.

--- GUI/Client/Widgets/pqOptions.cxx
+++ GUI/Client/Widgets/pqOptions.cxx
@@ -39,7 +39,7 @@ void pqOptions::PrintSelf(ostream &os, vtkIndent indent)
 
 void pqOptions::SetClientMode(int Mode)
 {
-  ClientMode = Mode;
+  this->ClientMode = Mode;
 }
 
 void pqOptions::SetServerHost(const char* const Host)
@@ -49,7 +49,7 @@ void pqOptions::SetServerHost(const char* const Host)
 
 void pqOptions::SetServerPort(int Port)
 {
-  ServerPort = Port;
+  this->ServerPort = Port;
 }
 
 
