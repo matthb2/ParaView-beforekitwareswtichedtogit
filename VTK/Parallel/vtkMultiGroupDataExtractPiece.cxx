a94a90115c33b2843d97c9696e34268ae723985b
Amy Squillacote <amy.squillacote@kitware.com>
2005-11-17 21:55:39 +0000

    COMP: removing unused variables

--- VTK/Parallel/vtkMultiGroupDataExtractPiece.cxx
+++ VTK/Parallel/vtkMultiGroupDataExtractPiece.cxx
@@ -112,8 +112,6 @@ void vtkMultiGroupDataExtractPiece::ExtractImageData(
   vtkImageData *imageData, vtkMultiGroupDataSet *output,
   int piece, int numberOfPieces, int ghostLevel, unsigned int group)
 {
-  vtkStreamingDemandDrivenPipeline *extractExecutive;
-  vtkInformation *extractInfo;
   int ext[6];
 
   // not using vtkImageClip because it doesn't work properly if you set
