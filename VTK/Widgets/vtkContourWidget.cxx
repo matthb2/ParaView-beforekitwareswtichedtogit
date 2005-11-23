97c7271eb8888bdcf3fedf6723b2bc57a4d1e930
Lisa Avila <lisa.avila@kitware.com>
2005-11-23 20:01:27 +0000

    BUG: fixed some widget events / rerendering

--- VTK/Widgets/vtkContourWidget.cxx
+++ VTK/Widgets/vtkContourWidget.cxx
@@ -224,6 +224,7 @@ void vtkContourWidget::AddNode()
       // manipulate mode
       this->WidgetState = vtkContourWidget::Manipulate;
       reinterpret_cast<vtkContourRepresentation*>(this->WidgetRep)->ClosedLoopOn();    
+      this->Render();
       this->EventCallbackCommand->SetAbortFlag(1);
       this->InvokeEvent(vtkCommand::EndInteractionEvent,NULL);
       return;
