a1e157a91d5ac06a2403c457d4e2af8f49a330e3
Berk Geveci <berk.geveci@kitware.com>
2005-12-20 20:48:39 +0000

    COMP: Needed support for vtkIdType

--- Servers/Common/vtkPVXMLElement.cxx
+++ Servers/Common/vtkPVXMLElement.cxx
@@ -347,3 +347,12 @@ int vtkPVXMLElement::GetVectorAttribute(const char* name, int length,
 {
   return vtkPVXMLVectorAttributeParse(this->GetAttribute(name), length, data);
 }
+
+#if defined(VTK_USE_64BIT_IDS)
+//----------------------------------------------------------------------------
+int vtkPVXMLElement::GetVectorAttribute(const char* name, int length,
+                                        vtkIdType* data)
+{
+  return vtkPVXMLVectorAttributeParse(this->GetAttribute(name), length, data);
+}
+#endif
