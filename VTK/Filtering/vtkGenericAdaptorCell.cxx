fd69f40b8f66b29bf5f9302abeb04d0f939b8392
Francois Bertel <francois.bertel@kitware.com>
2006-02-17 13:46:33 +0000

    COMP:Fixed some warning on mac/ppc gcc 4.0.1 about uninitialized variable

--- VTK/Filtering/vtkGenericAdaptorCell.cxx
+++ VTK/Filtering/vtkGenericAdaptorCell.cxx
@@ -253,6 +253,9 @@ void vtkGenericAdaptorCell::Contour(vtkContourValues *contourValues,
   vtkCell *linearCell;
   vtkIdType ptsCount;
  
+  range[0]=0; // to fix warning
+  range[1]=0; // on some compilers
+  
   this->Reset();
   
   // for each cell-centered attribute: copy the value in the secondary
