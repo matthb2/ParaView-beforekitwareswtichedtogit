499ea5641568bde4e8ea491575443e582ddf3053
Will Schroeder <will.schroeder@kitware.com>
2006-03-28 13:42:29 +0000

    ENH:Tweaks as timer changes are introduced and tested

--- VTK/Rendering/vtkRenderWindowInteractor.cxx
+++ VTK/Rendering/vtkRenderWindowInteractor.cxx
@@ -468,12 +468,12 @@ int vtkRenderWindowInteractor::CreateOneShotTimer(unsigned long duration)
 } 
 
 //new-style group #2 returns type (non-zero unless bad timerId)
-int vtkRenderWindowInteractor::IsSingleShotTimer(int timerId)
+int vtkRenderWindowInteractor::IsOneShotTimer(int timerId)
 {
   vtkTimerIdMapIterator iter = this->TimerMap->find(timerId);
   if ( iter != this->TimerMap->end() )
     {
-    return (*iter).second.Type;
+    return ((*iter).second.Type == OneShotTimer);
     }
   return 0;
 } 
