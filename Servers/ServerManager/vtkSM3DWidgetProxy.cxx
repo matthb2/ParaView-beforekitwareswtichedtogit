d8fee091bf7c3834d2c0a072ef4f418d0c639648
Berk Geveci <berk.geveci@kitware.com>
2006-04-26 15:25:06 +0000

    BUG: 3D widgets should not be placed at creation. Since there is an observer on PlaceWidget that sets the values of some properties, PlaceWidget, if called before UpdateVTKObjects, might end up throwing values stored in the properties

--- Servers/ServerManager/vtkSM3DWidgetProxy.cxx
+++ Servers/ServerManager/vtkSM3DWidgetProxy.cxx
@@ -195,21 +195,6 @@ void vtkSM3DWidgetProxy::CreateVTKObjects(int numObjects)
       pm->GetObjectFromID(this->GetID(cc)));
     this->InitializeObservers(widget);
     }
-
-  vtkClientServerStream stream;
-  for (cc=0; cc <this->GetNumberOfIDs(); cc++)
-    {
-    vtkClientServerID id = this->GetID(cc);
-    stream << vtkClientServerStream::Invoke << id
-           << "SetPlaceFactor" << 1.0
-           << vtkClientServerStream::End;
-    stream << vtkClientServerStream::Invoke << id 
-           << "PlaceWidget"
-           << 0 << 1 << 0 << 1 << 0 << 1 
-           << vtkClientServerStream::End;
-    // this->Bounds have already been initialized to 0,1,0,1,0,1
-    pm->SendStream(this->ConnectionID, this->GetServers(), stream);
-    }
 }
 
 //----------------------------------------------------------------------------
