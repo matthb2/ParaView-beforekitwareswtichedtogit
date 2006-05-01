4c3fe64a2b12668f192e96018d9a3978f320cc28
Andy Cedilnik <andy.cedilnik@kitware.com>
2006-05-01 17:20:35 +0000

    COMP: Fix the build issue

--- Servers/ServerManager/vtkSMProxyManager.cxx
+++ Servers/ServerManager/vtkSMProxyManager.cxx
@@ -623,7 +623,7 @@ void vtkSMProxyManager::ExecuteEvent(vtkObject* obj, unsigned long event,
       this->MarkProxyAsModified(proxy);
       ModifiedPropertyInformation info;
       info.Proxy = proxy;
-      info.PropertyName = static_cast<const char*>(data);
+      info.PropertyName = reinterpret_cast<const char*>(data);
       if (info.PropertyName)
         {
         this->InvokeEvent(vtkCommand::PropertyModifiedEvent,
