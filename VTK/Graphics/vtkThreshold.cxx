c3c6c989477ffe5f17b755b0cdb48e7970a9ec90
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-05-02 18:39:05 +0000

    ENH: Propagate the new cell type into more filters

--- VTK/Graphics/vtkThreshold.cxx
+++ VTK/Graphics/vtkThreshold.cxx
@@ -58,7 +58,7 @@ void vtkThreshold::ThresholdByLower(double lower)
     this->Modified();
     }
 }
-                           
+
 // Criterion is cells whose scalars are greater or equal to upper threshold.
 void vtkThreshold::ThresholdByUpper(double upper)
 {
@@ -70,7 +70,7 @@ void vtkThreshold::ThresholdByUpper(double upper)
     this->Modified();
     }
 }
-                           
+
 // Criterion is cells whose scalars are between lower and upper thresholds.
 void vtkThreshold::ThresholdBetween(double lower, double upper)
 {
