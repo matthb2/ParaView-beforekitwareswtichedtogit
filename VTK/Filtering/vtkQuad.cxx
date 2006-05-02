c1c20eb13c9197ac48cb7caaeafbf29592e81ad2
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-05-02 19:42:22 +0000

    BUG: Fix GetEdge for the linear quad. remove duplicate include

--- VTK/Filtering/vtkQuad.cxx
+++ VTK/Filtering/vtkQuad.cxx
@@ -14,17 +14,16 @@
 =========================================================================*/
 #include "vtkQuad.h"
 
+#include "vtkObjectFactory.h"
 #include "vtkCellArray.h"
 #include "vtkCellData.h"
 #include "vtkLine.h"
 #include "vtkTriangle.h"
 #include "vtkMath.h"
-#include "vtkObjectFactory.h"
 #include "vtkPlane.h"
 #include "vtkPointData.h"
 #include "vtkPointLocator.h"
 #include "vtkPoints.h"
-#include "vtkTriangle.h"
 
 vtkCxxRevisionMacro(vtkQuad, "$Revision$");
 vtkStandardNewMacro(vtkQuad);
