ba4b07811a0b94a5d67c693397264b122218dc03
Ken Moreland <kmorel@sandia.gov>
2006-06-26 18:50:03 +0000

    ENH: Pass global ids since they will be consistent.

--- VTK/Graphics/vtkGeometryFilter.cxx
+++ VTK/Graphics/vtkGeometryFilter.cxx
@@ -246,7 +246,9 @@ int vtkGeometryFilter::RequestData(
   newPts = vtkPoints::New();
   newPts->Allocate(numPts,numPts/2);
   output->Allocate(4*numCells,numCells/2);
+  outputPD->CopyGlobalIdsOn();
   outputPD->CopyAllocate(pd,numPts,numPts/2);
+  outputPD->CopyGlobalIdsOn();
   outputCD->CopyAllocate(cd,numCells,numCells/2);
 
   if ( this->Merging )
@@ -654,6 +656,7 @@ void vtkGeometryFilter::UnstructuredGridExecute(vtkDataSet *dataSetInput,
   output->SetPoints(input->GetPoints());
   outputPD->PassData(pd);
 
+  outputCD->CopyGlobalIdsOn();
   outputCD->CopyAllocate(cd,numCells,numCells/2);
 
   verts = vtkCellArray::New();
@@ -1150,6 +1153,7 @@ void vtkGeometryFilter::StructuredGridExecute(vtkDataSet *dataSetInput,
   //
   output->SetPoints(input->GetPoints());
   outputPD->PassData(pd);
+  outputCD->CopyGlobalIdsOn();
   outputCD->CopyAllocate(cd,numCells,numCells/2);
 
   cells = vtkCellArray::New();
