371f3f80a9bf12322a1428050cc9954554f523c8
Will Schroeder <will.schroeder@kitware.com>
2006-07-17 13:37:36 +0000

    ENH:Now using SelectRegion() method properly

--- VTK/Widgets/vtkCameraWidget.cxx
+++ VTK/Widgets/vtkCameraWidget.cxx
@@ -62,6 +62,8 @@ void vtkCameraWidget::SelectRegion(double eventPos[2])
     {
     reinterpret_cast<vtkCameraRepresentation*>(this->WidgetRep)->InitializePath();
     }
+  
+  this->Superclass::SelectRegion(eventPos);
 }
 
 //-------------------------------------------------------------------------
