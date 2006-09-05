79e11b75b352c753688b5e259c98996a55f8c677
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-09-05 23:22:51 +0000

    BUG: Fix Bug #3690 - An error in function: vtkFieldData::AllocateArrays(int nSize)

--- VTK/Filtering/vtkFieldData.cxx
+++ VTK/Filtering/vtkFieldData.cxx
@@ -299,7 +299,10 @@ void vtkFieldData::AllocateArrays(int num)
     {
     for ( i=num; i < this->NumberOfArrays; i++)
       {
-      this->Data[i]->UnRegister(this);
+      if( this->Data[i] )
+        {
+        this->Data[i]->UnRegister(this);
+        }
       }
     this->NumberOfArrays = num;
     }
