52ae1ae2e372f38d12d8e636305b2a9bc58aca45
Clinton Stimpson <clinton@elemtech.com>
2006-09-19 19:18:50 +0000

    BUG:  Use delete [].

--- Servers/Filters/vtkPVLookupTable.cxx
+++ Servers/Filters/vtkPVLookupTable.cxx
@@ -74,7 +74,7 @@ void vtkPVLookupTable::Build()
       lut_ptr[4*cc+2] = (unsigned char)(255.0*table[3*cc+2] + 0.5);
       lut_ptr[4*cc+3] = 255;
       }
-    delete table;
+    delete [] table;
 
     this->BuildTime.Modified();
     }
