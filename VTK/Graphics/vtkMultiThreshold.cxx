1fc9e725fc81f8cb7f32f10b7d96789a3f27adc5
Ken Moreland <kmorel@sandia.gov>
2006-10-11 13:21:43 +0000

    COMP: Get rid of using isnan, since that is not supported with MSVC.
    COMP: Get rid of warning about unreachable code.

--- VTK/Graphics/vtkMultiThreshold.cxx
+++ VTK/Graphics/vtkMultiThreshold.cxx
@@ -160,7 +160,6 @@ int vtkMultiThreshold::Interval::Match( double cellNorm[2] )
     {
     return VTK_MULTITHRESH_IS_IN_INTERVAL( cellNorm[0] );
     }
-  return 0;
 }
 
 void vtkMultiThreshold::Interval::PrintNode( ostream& os )
@@ -352,7 +351,9 @@ int vtkMultiThreshold::AddIntervalSet( NormKey& nk, double xmin, double xmax, in
     return -1;
     }
 
-  if ( isnan( xmin ) || isnan( xmax ) )
+  // The following condition will only hold if xmin or xmax is a NAN (or maybe
+  // if they are both infinite, which is an empty range anyway).
+  if (!(xmin <= xmax) && !(xmin >= xmax))
     {
     vtkWarningMacro( "One of the interval endpoints is not a number." );
     return -1;
