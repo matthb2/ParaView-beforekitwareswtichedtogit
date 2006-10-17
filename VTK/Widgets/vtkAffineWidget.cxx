cc65b6f74e807d130f6f06661201cf173268d75c
David Cole <David.Cole@kitware.com>
2006-10-17 13:43:38 +0000

    COMP: Revert obvious typo...

--- VTK/Widgets/vtkAffineWidget.cxx
+++ VTK/Widgets/vtkAffineWidget.cxx
@@ -14,7 +14,7 @@
 =========================================================================*/
 #include "vtkAffineWidget.h"
 #include "vtkAffineRepresentation2D.h"
-#include "vtkCommand.h"d
+#include "vtkCommand.h"
 #include "vtkCallbackCommand.h"
 #include "vtkRenderWindowInteractor.h"
 #include "vtkObjectFactory.h"
