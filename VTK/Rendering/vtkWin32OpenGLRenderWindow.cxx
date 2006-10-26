81b38b387436a069fc79095f7ac667c8fb8f02e2
David Cole <David.Cole@kitware.com>
2006-10-26 21:36:56 +0000

    BUG: Fix failing offscreen test - if interactor has 0 size, pass render window's size along when SetInteractor is called. Also, keep them in sync when render window SetSize is called by somebody other than the interactor. ENH: Make offscreen tests pass on Windows by ensuring that SetSize has been called even when a test script doesn't explicitly call it. Call it at first Render time if the size is still 0 by then.

--- VTK/Rendering/vtkWin32OpenGLRenderWindow.cxx
+++ VTK/Rendering/vtkWin32OpenGLRenderWindow.cxx
@@ -253,6 +253,12 @@ void vtkWin32OpenGLRenderWindow::SetSize(int x, int y)
     this->Modified();
     this->Size[0] = x;
     this->Size[1] = y;
+
+    if (this->Interactor)
+      {
+      this->Interactor->SetSize(x, y);
+      }
+
     if (this->OffScreenRendering)
       {
       if (!resizing)
