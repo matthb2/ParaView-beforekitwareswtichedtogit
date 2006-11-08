eedc7edbc8a99d6912ebb667b813236cace88138
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-11-08 16:02:42 +0000

    BUG: Fixes BUG# 2838. vtkOpenGLActor::Render() has the responsibility of pushing the actor transformations on the Model-View stack. However, in case of CG, the model view matrices are passed as unformarms in the vtkShader::PassShaderVariables() which happens before vtkOpenGLActor::Render(). Hence in CgShader, we override PassShaderVariables() to explicitly push actor transformations.

--- VTK/Rendering/vtkShader.cxx
+++ VTK/Rendering/vtkShader.cxx
@@ -368,11 +368,8 @@ void vtkShader::PassShaderVariables(vtkActor* actor, vtkRenderer* renderer)
     return;
     }
 
-  if( this->XMLShader->GetRootElement() )
-    {
-    this->SetShaderParameters(actor, renderer, this->XMLShader->GetRootElement());
-    this->PassShaderVariablesTime.Modified();
-    }
+  this->SetShaderParameters(actor, renderer, this->XMLShader->GetRootElement());
+  this->PassShaderVariablesTime.Modified();
 }
 
 //-----------------------------------------------------------------------------
