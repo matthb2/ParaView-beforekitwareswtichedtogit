a72a0d27cd9b3c90dbad20ff9015f57cfd0f2a11
Amy Squillacote <amy.squillacote@kitware.com>
2007-01-25 20:06:33 +0000

    BUG: in order for turning the polygonal plane visibility on/off to work through properties (called from vtkPVImplicitPlaneWidget), we must consistently set this value using properties, not setting it's value directly.

--- Servers/ServerManager/vtkSMImplicitPlaneWidgetProxy.cxx
+++ Servers/ServerManager/vtkSMImplicitPlaneWidgetProxy.cxx
@@ -133,7 +133,12 @@ void vtkSMImplicitPlaneWidgetProxy::ExecuteEvent(vtkObject *wdg, unsigned long e
     }
   if (!widget->GetDrawPlane() && event == vtkCommand::InteractionEvent)
     { 
-    this->SetDrawPlane(1);
+    vtkSMIntVectorProperty *dp = vtkSMIntVectorProperty::SafeDownCast(
+      this->GetProperty("DrawPlane"));
+    if (dp)
+      {
+      dp->SetElements1(1);
+      }
     }
   this->Superclass::ExecuteEvent(wdg,event,p);
 }
