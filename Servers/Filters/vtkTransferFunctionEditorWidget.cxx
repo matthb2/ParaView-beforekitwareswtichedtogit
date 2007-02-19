a9756bc48bf18f9b0bdd776c504da3fbabaea630
Amy Squillacote <amy.squillacote@kitware.com>
2007-02-19 20:39:18 +0000

    ENH: Add functionality to modify a color transfer function. Add methods to set the opacity and color of a transfer function element from the transfer function viewer

--- Servers/Filters/vtkTransferFunctionEditorWidget.cxx
+++ Servers/Filters/vtkTransferFunctionEditorWidget.cxx
@@ -15,6 +15,7 @@
 #include "vtkTransferFunctionEditorWidget.h"
 
 #include "vtkCellData.h"
+#include "vtkColorTransferFunction.h"
 #include "vtkDataArray.h"
 #include "vtkDataSet.h"
 #include "vtkPiecewiseFunction.h"
@@ -32,6 +33,7 @@ vtkTransferFunctionEditorWidget::vtkTransferFunctionEditorWidget()
   this->WholeScalarRange[1] = this->VisibleScalarRange[1] = 0;
   this->ModificationType = OPACITY;
   this->OpacityFunction = vtkPiecewiseFunction::New();
+  this->ColorFunction = vtkColorTransferFunction::New();
   this->Histogram = NULL;
 }
 
@@ -39,6 +41,7 @@ vtkTransferFunctionEditorWidget::vtkTransferFunctionEditorWidget()
 vtkTransferFunctionEditorWidget::~vtkTransferFunctionEditorWidget()
 {
   this->OpacityFunction->Delete();
+  this->ColorFunction->Delete();
   this->SetHistogram(NULL);
 }
 
