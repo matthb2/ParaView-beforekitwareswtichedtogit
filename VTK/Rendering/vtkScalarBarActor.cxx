ffcdbce054dff12ab189af5c856c024cba19182d
Karthik Krishnan <karthik.krishnan@kitware.com>
2007-03-14 23:01:48 +0000

    BUG: Printself defect

--- VTK/Rendering/vtkScalarBarActor.cxx
+++ VTK/Rendering/vtkScalarBarActor.cxx
@@ -606,6 +606,8 @@ void vtkScalarBarActor::PrintSelf(ostream& os, vtkIndent indent)
   if (this->UseOpacity)
     {
     os << indent << "TextureGridWidth: " << this->TextureGridWidth << "\n";
+    os << indent << "TextureActor:\n";
+    this->TextureActor->PrintSelf(os, indent.GetNextIndent());
     }
 }
 
