958c700ee0cde42916c2ea0b1d543327fadba3cb
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-03-24 19:00:48 +0000

    ENH: Fixed BUG #4543. Now, when two views are linked, their undo stacks are linked as well. That way when one undoes on one view, the other view's undo stack also updates to show that 1 step has been undone and the user can redo the action from the undo buttons on the other view.

--- Servers/ServerManager/vtkSMCameraLink.cxx
+++ Servers/ServerManager/vtkSMCameraLink.cxx
@@ -137,6 +137,11 @@ void vtkSMCameraLink::AddLinkedProxy(vtkSMProxy* proxy, int updateDir)
     this->Superclass::AddLinkedProxy(proxy, updateDir);
     if(updateDir == vtkSMLink::INPUT)
       {
+      proxy->CreateVTKObjects(1); 
+        // ensure that the proxy is created.
+        // This is necessary since when loading state the proxy may not yet be
+        // created, however we want to observer events on the
+        // interactor. 
       this->Internals->LinkedProxies.push_back(
         new vtkSMCameraLinkInternals::LinkedCamera(proxy, this));
       }
