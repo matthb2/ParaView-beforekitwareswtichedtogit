430b7562fb5abbccb9ed1b8f67ea263a360a6b2e
Dave Demarle <dave.demarle@kitware.com>
2007-03-28 23:05:36 +0000

    ENH: Make python filter able to take multiple inputs.

--- Servers/Filters/vtkPythonProgrammableFilter.cxx
+++ Servers/Filters/vtkPythonProgrammableFilter.cxx
@@ -163,6 +163,22 @@ void vtkPythonProgrammableFilter::Exec()
 
 
 //----------------------------------------------------------------------------
+int vtkPythonProgrammableFilter::FillInputPortInformation(
+  int port, vtkInformation *info)
+{
+  if(!this->Superclass::FillInputPortInformation(port, info))
+    {
+    return 0;
+    }
+  if(port==0)
+    {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
+    info->Set(vtkAlgorithm::INPUT_IS_REPEATABLE(),1);
+    }
+  return 1;
+}
+
+//----------------------------------------------------------------------------
 void vtkPythonProgrammableFilter::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
