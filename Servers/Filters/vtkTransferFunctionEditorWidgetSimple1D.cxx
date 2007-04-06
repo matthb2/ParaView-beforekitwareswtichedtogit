a1f40a0d241563cc371a9f4616f29efebaf3baaa
Amy Squillacote <amy.squillacote@kitware.com>
2007-04-06 12:24:55 +0000

    COMP: fixing compile warnings on the dashboard

--- Servers/Filters/vtkTransferFunctionEditorWidgetSimple1D.cxx
+++ Servers/Filters/vtkTransferFunctionEditorWidgetSimple1D.cxx
@@ -402,8 +402,8 @@ void vtkTransferFunctionEditorWidgetSimple1D::AddNewNode(double scalar)
     newHandleWidget->SetEnabled(1);
     }
   rep->SetActiveHandle(currentHandleNumber);
-  this->LeftClickEventPosition[0] = displayPos[0];
-  this->LeftClickEventPosition[1] = displayPos[1];
+  this->LeftClickEventPosition[0] = vtkMath::Round(displayPos[0]);
+  this->LeftClickEventPosition[1] = vtkMath::Round(displayPos[1]);
   this->InvokeEvent(vtkCommand::PlacePointEvent,(void*)&(currentHandleNumber));
   this->InvokeEvent(vtkCommand::InteractionEvent,NULL);
 }
