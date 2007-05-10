6a94603f5c58effb043f6c33347276c5bc883040
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-05-10 20:46:02 +0000

    COMP: warnings

--- Servers/ServerManager/vtkSMRenderViewProxy.cxx
+++ Servers/ServerManager/vtkSMRenderViewProxy.cxx
@@ -702,6 +702,8 @@ vtkSMProxy *vtkSMRenderViewProxy::GetProxyFromPropID(
   vtkClientServerID *id,
   int proxyType)
 {
+  (void)id;
+  (void)proxyType;
   vtkSMProxy *ret = NULL;
 // FIXME
 #if 0 
@@ -751,6 +753,8 @@ vtkSMProxy *vtkSMRenderViewProxy::GetProxyForDisplay(
   int number,
   int proxyType)
 {
+  (void)number;
+  (void)proxyType;
   vtkSMProxy *ret = NULL;
 
   /* FIXME
