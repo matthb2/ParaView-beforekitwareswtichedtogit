f29220a5ed8b7540558627cdf3f571666e766f63
Jeffrey Baumes <jeff.baumes@kitware.com>
2007-06-13 14:06:55 +0000

    BUG: Variant ToNumeric() should only validate the parsing if it reads the full string into a numeric. ENH: Allow user to get the prop from the prop id in vtkVisibleCellSelector.

--- VTK/Rendering/vtkVisibleCellSelector.cxx
+++ VTK/Rendering/vtkVisibleCellSelector.cxx
@@ -653,15 +653,13 @@ void vtkVisibleCellSelector::GetSelectedIds(vtkSelection *dest)
 }
 
 //----------------------------------------------------------------------------
-vtkProp* vtkVisibleCellSelector::GetActorFromId(vtkIdType cellid)
+vtkProp* vtkVisibleCellSelector::GetActorFromId(vtkIdType id)
 {
-  if ( (this->Renderer == NULL) || 
-       (cellid >= this->Renderer->PropsSelectedFromCount) )
+  if (!this->Renderer || !this->Renderer->IdentPainter)
     {
-    return NULL;
+    return 0;
     }
-
-  return this->Renderer->PropsSelectedFrom[cellid];
+  return this->Renderer->IdentPainter->GetActorFromId(id);
 }
 
 //----------------------------------------------------------------------------
