c02f59e88af73d2b1c976029fcb0c629d949b2cc
Berk Geveci <berk.geveci@kitware.com>
2007-06-20 13:55:09 +0000

    BUG: Fixed uninitialized variable.

--- Servers/Filters/vtkPPhastaReader.cxx
+++ Servers/Filters/vtkPPhastaReader.cxx
@@ -434,7 +434,7 @@ int vtkPPhastaReader::RequestInformation(vtkInformation*,
       }
     }
 
-  int numberOfFields, numberOfFields2;
+  int numberOfFields=0, numberOfFields2=0;
   for (unsigned int i=0; i<numElements; i++)
     {
     vtkPVXMLElement* nested = rootElement->GetNestedElement(i);
