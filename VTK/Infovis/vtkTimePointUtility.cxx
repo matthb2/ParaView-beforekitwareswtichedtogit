b918354a6c1bac899a7f64eed046f9501276a9e3
Jeffrey Baumes <jeff.baumes@kitware.com>
2007-07-31 16:16:36 +0000

    COMP: Fixing initializing variables and iomanip issues.

--- VTK/Infovis/vtkTimePointUtility.cxx
+++ VTK/Infovis/vtkTimePointUtility.cxx
@@ -19,6 +19,18 @@
 #include "vtkStdString.h"
 
 #include "vtksys/ios/sstream"
+#include <iomanip>
+
+const int vtkTimePointUtility::MILLIS_PER_SECOND  =     1000;
+const int vtkTimePointUtility::MILLIS_PER_MINUTE  =    60000;
+const int vtkTimePointUtility::MILLIS_PER_HOUR    =  3600000;
+const int vtkTimePointUtility::MILLIS_PER_DAY     = 86400000;
+const int vtkTimePointUtility::SECONDS_PER_MINUTE =       60;
+const int vtkTimePointUtility::SECONDS_PER_HOUR   =     3600;
+const int vtkTimePointUtility::SECONDS_PER_DAY    =    86400;
+const int vtkTimePointUtility::MINUTES_PER_HOUR   =       60;
+const int vtkTimePointUtility::MINUTES_PER_DAY    =     1440;
+const int vtkTimePointUtility::HOURS_PER_DAY      =       24;
 
 vtkCxxRevisionMacro(vtkTimePointUtility, "$Revision$");
 vtkStandardNewMacro(vtkTimePointUtility);
