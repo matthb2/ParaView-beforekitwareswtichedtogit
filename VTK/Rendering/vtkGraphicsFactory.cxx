ae38000e11c93e39f88d62f5a75e226c5edfd4e6
Alexander Neundorf <neundorf@kde.org>
2007-08-10 20:35:42 +0000

    COMP: also return OpenGL (for now) when using OSMesa
    Alex

--- VTK/Rendering/vtkGraphicsFactory.cxx
+++ VTK/Rendering/vtkGraphicsFactory.cxx
@@ -139,7 +139,7 @@ const char *vtkGraphicsFactory::GetRenderLibrary()
   // if nothing is set then work down the list of possible renderers
   if ( !temp )
     {
-#ifdef VTK_DISPLAY_X11_OGL
+#if defined(VTK_DISPLAY_X11_OGL) || defined(VTK_USE_OSMESA)
     temp = "OpenGL";
 #endif
 #ifdef VTK_DISPLAY_WIN32_OGL
