5ba13173fafa3ce419324f9480953b3a3a150c3f
Yumin Yuan <yumin.yuan@kitware.com>
2007-08-14 17:56:14 +0000

    BUG: If the vector is empty, the Copy() will fail. The checking of vector size is added.

--- Servers/ServerManager/vtkSMIntVectorProperty.cxx
+++ Servers/ServerManager/vtkSMIntVectorProperty.cxx
@@ -471,10 +471,16 @@ void vtkSMIntVectorProperty::Copy(vtkSMProperty* src)
     this->ImmediateUpdate = 0;
     this->SetNumberOfElements(dsrc->GetNumberOfElements());
     this->SetNumberOfUncheckedElements(dsrc->GetNumberOfUncheckedElements());
-    this->SetElements(&dsrc->Internals->Values[0]);
-    memcpy(&this->Internals->UncheckedValues[0], 
-           &dsrc->Internals->UncheckedValues[0], 
-           this->GetNumberOfUncheckedElements()*sizeof(int));
+    if (this->GetNumberOfElements() > 0)
+      {
+      this->SetElements(&dsrc->Internals->Values[0]);
+      }
+    if (this->GetNumberOfUncheckedElements() > 0)
+      {
+      memcpy(&this->Internals->UncheckedValues[0], 
+             &dsrc->Internals->UncheckedValues[0], 
+             this->GetNumberOfUncheckedElements()*sizeof(int));
+      }
     this->ImmediateUpdate = imUpdate;
     this->Modified();
     }
