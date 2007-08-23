035e0f08a8338badedfc815e2314b58854a89cb0
Amy Squillacote <amy.squillacote@kitware.com>
2007-08-23 15:09:32 +0000

    BUG: cleaning up memory issues

--- VTK/Infovis/vtkSimple2DLayoutStrategy.cxx
+++ VTK/Infovis/vtkSimple2DLayoutStrategy.cxx
@@ -68,6 +68,7 @@ vtkSimple2DLayoutStrategy::vtkSimple2DLayoutStrategy()
   this->RestDistance = 0;
   this->Jitter = true;
   this->MaxNumberOfIterations = 100;
+  this->EdgeArray = NULL;
 }
 
 // ----------------------------------------------------------------------
@@ -133,6 +134,11 @@ void vtkSimple2DLayoutStrategy::Initialize()
     }
     
   // Put the edge data into compact, fast access edge data structure
+  if (this->EdgeArray)
+    {
+    delete [] this->EdgeArray;
+    this->EdgeArray = NULL;
+    }
   this->EdgeArray =  new vtkLayoutEdge[numEdges];
   
   // If jitter then do it now at initialization
