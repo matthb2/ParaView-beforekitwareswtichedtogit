231dfbb7f4a4e1b25ec51d327016d4cf9a55a76c
Berk Geveci <berk.geveci@kitware.com>
2007-09-06 17:00:40 +0000

    ENH: Added check for the proxy. If proxy is the same, do not modify self.

--- Servers/ServerManager/vtkSMProxyProperty.cxx
+++ Servers/ServerManager/vtkSMProxyProperty.cxx
@@ -385,6 +385,11 @@ unsigned int vtkSMProxyProperty::RemoveProxy(vtkSMProxy* proxy, int modify)
 //---------------------------------------------------------------------------
 int vtkSMProxyProperty::SetProxy(unsigned int idx, vtkSMProxy* proxy)
 {
+  if (proxy == this->PPInternals->Proxies[idx])
+    {
+    return 1;
+    }
+
   if ( vtkSMProperty::GetCheckDomains() )
     {
     this->SetUncheckedProxy(idx, proxy);
