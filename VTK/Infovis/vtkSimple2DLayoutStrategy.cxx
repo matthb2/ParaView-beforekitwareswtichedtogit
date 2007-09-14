1a145f756a4b9a1f37535ffc5ab8f2906d1ce1da
Brian Wylie <bnwylie@sandia.gov>
2007-09-14 15:37:11 +0000

    STYLE: Small spelling correction in a comment

--- VTK/Infovis/vtkSimple2DLayoutStrategy.cxx
+++ VTK/Infovis/vtkSimple2DLayoutStrategy.cxx
@@ -249,7 +249,7 @@ void vtkSimple2DLayoutStrategy::Layout()
         
       for(vtkIdType k=0; k<numVertices; ++k)
         {
-        // Don't repluse against yourself :)
+        // Don't repulse against yourself :)
         if (k == j) continue;
           
         pointIndex2 = k * 3;
