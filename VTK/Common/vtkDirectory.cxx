ac69e4bc5c68a0f3cb335a495c95ce7077cb5971
Alexander Neundorf <neundorf@kde.org>
2007-09-20 13:41:29 +0000

    COMP: make it build with the PGI compiler on Cray Xt3/Catamount
    Alex

--- VTK/Common/vtkDirectory.cxx
+++ VTK/Common/vtkDirectory.cxx
@@ -152,6 +152,15 @@ const char* vtkDirectory::GetCurrentWorkingDirectory(char* buf,
 #include <dirent.h>
 #include <unistd.h>
 
+/* There is a problem with the Portland compiler, large file
+support and glibc/Linux system headers: 
+             http://www.pgroup.com/userforum/viewtopic.php?
+             p=1992&sid=f16167f51964f1a68fe5041b8eb213b6
+*/
+#if defined(__PGI) && defined(__USE_FILE_OFFSET64)
+# define dirent dirent64
+#endif
+
 int vtkDirectory::Open(const char* name)
 {
   // clean up from any previous open
