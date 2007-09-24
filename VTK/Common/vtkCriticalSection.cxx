fe3fc5b8ce7764640d41c7b89d69dfdee57048d3
Brad King <brad.king@kitware.com>
2007-09-24 13:52:56 +0000

    BUG: Reverting previous change.  The proper fix is to not define VTK_HP_PTHREADS when CMA threads are not available.

--- VTK/Common/vtkCriticalSection.cxx
+++ VTK/Common/vtkCriticalSection.cxx
@@ -36,7 +36,7 @@ void vtkSimpleCriticalSection::Init()
 #endif
 
 #ifdef VTK_USE_PTHREADS
-#if defined(VTK_HP_PTHREADS) && 0 /* no pthread_mutexattr_default anymore */
+#ifdef VTK_HP_PTHREADS
   pthread_mutex_init(&(this->CritSec), pthread_mutexattr_default);
 #else
   pthread_mutex_init(&(this->CritSec), NULL);
