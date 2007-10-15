a1bf27ca565028efb58fb0d0d494849aaf0ca095
Dave Demarle <dave.demarle@kitware.com>
2007-10-15 18:49:16 +0000

    BUG: Continuosly variable temporal sources now show the range in the Information tab. The UI for the tab has not yet been updated distinguish an analytic range from from a two point discrete source. Also, fix bug #5863, TimeSource does not show time info.

--- VTK/Graphics/vtkTimeSourceExample.cxx
+++ VTK/Graphics/vtkTimeSourceExample.cxx
@@ -197,10 +197,10 @@ int vtkTimeSourceExample::RequestInformation(
   double reqTime = 0.0;
   //int reqNTS = 0; since we are only answering the first request omit this
   double *reqTS = NULL;
-  if (info->Has(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS()))
+  if (reqInfo->Has(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS()))
     {
     //reqNTS = info->Length(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS());    
-    reqTS = info->Get(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS());
+    reqTS = reqInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEPS());
     }
   if (reqTS != NULL)
     {
