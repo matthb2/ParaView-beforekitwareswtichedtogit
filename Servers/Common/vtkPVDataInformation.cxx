756998545cc0b30d3b37495d50bdabe52384c92b
Dave Demarle <dave.demarle@kitware.com>
2007-10-17 01:57:07 +0000

    BUG: reenable my last change, but avoid the seg fault, to keep the feature and fix the tests

--- Servers/Common/vtkPVDataInformation.cxx
+++ Servers/Common/vtkPVDataInformation.cxx
@@ -370,15 +370,13 @@ void vtkPVDataInformation::CopyFromDataSet(vtkDataSet* data)
     }
 
   //Copy time span from the dataset's producing algorithm
-/*
   vtkInformation *pinfo = data->GetPipelineInformation();
-  if (pinfo->Has(vtkStreamingDemandDrivenPipeline::TIME_RANGE()))
+  if (pinfo && pinfo->Has(vtkStreamingDemandDrivenPipeline::TIME_RANGE()))
     {    
     double *times = pinfo->Get(vtkStreamingDemandDrivenPipeline::TIME_RANGE());
     this->TimeSpan[0] = times[0];
     this->TimeSpan[1] = times[1];
     }
-*/
 }
 
 //----------------------------------------------------------------------------
