1e01c50e59c512e9be7772673dd02687b968828c
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-10-31 15:43:26 +0000

    ENH: Added StackDepth to undo-redo stack. Until we start supporting multiple clients, disabling building of the undo stack on the server side. This simplifies life with stack depth.

--- Servers/ServerManager/vtkSMUndoStack.cxx
+++ Servers/ServerManager/vtkSMUndoStack.cxx
@@ -203,7 +203,12 @@ void vtkSMUndoStack::Push(vtkIdType cid, const char* label, vtkUndoSet* set)
   vtkPVXMLElement* state = set->SaveState(NULL);
 //    if (!this->ClientOnly)
 //      state->PrintXML();
-  if (this->ClientOnly)
+
+  /*
+   * FIXME: Until we start supporting multiple client, it really unnecessary to push
+   * the elements on the server side. 
+   */
+  if (true || this->ClientOnly)
     {
     vtkSMUndoStackUndoSet* elem = vtkSMUndoStackUndoSet::New();
     elem->SetConnectionID(cid);
@@ -221,6 +226,7 @@ void vtkSMUndoStack::Push(vtkIdType cid, const char* label, vtkUndoSet* set)
     // when the undo stack on any connection is updated.
     this->PushUndoConnection(label, cid); 
     }
+
   state->Delete();
 }
 
