d8abbbc5f36401710298aaeacc9173c42267cf01
Francois Bertel <francois.bertel@kitware.com>
2007-11-07 18:46:12 +0000

    COMP:Removed warning about deprecated conversion from string constant to char*.

--- VTK/IO/vtkDataWriter.cxx
+++ VTK/IO/vtkDataWriter.cxx
@@ -908,7 +908,7 @@ int vtkDataWriter::WriteCoordinates(ostream *fp, vtkDataArray *coords,
 int vtkDataWriter::WriteScalarData(ostream *fp, vtkDataArray *scalars, int num)
 {
   int i, j, size=0;
-  char *name;
+  const char *name;
   vtkLookupTable *lut;
   int dataType = scalars->GetDataType();
   int numComp = scalars->GetNumberOfComponents();
@@ -916,7 +916,7 @@ int vtkDataWriter::WriteScalarData(ostream *fp, vtkDataArray *scalars, int num)
   if ( (lut=scalars->GetLookupTable()) == NULL || 
        (size = lut->GetNumberOfColors()) <= 0 )
     {
-    name = static_cast<char *>("default");
+    name = "default";
     }
   else 
     {
