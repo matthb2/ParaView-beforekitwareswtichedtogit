54d2aea29577659cae294b9cea8887d3f624c565
Amy Squillacote <amy.squillacote@kitware.com>
2007-11-15 20:22:51 +0000

    COMP: Fix from Alan to get ParaView to compile with some older versions of Python.

--- Utilities/VTKPythonWrapping/Executable/vtkPVPythonInterpretor.cxx
+++ Utilities/VTKPythonWrapping/Executable/vtkPVPythonInterpretor.cxx
@@ -524,7 +524,8 @@ void vtkPVPythonInterpretor::RunSimpleFile(const char* const filename)
     return;
     }
  
-  PyRun_SimpleFile(fp, filename);
+  // The cast is necessary because PyRun_SimpleFile() hasn't always been const-correct
+  PyRun_SimpleFile(fp, const_cast<char*>(filename));
 
   fclose(fp);
   this->ReleaseControl();
