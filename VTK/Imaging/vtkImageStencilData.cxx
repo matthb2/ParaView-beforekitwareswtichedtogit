fc9fc398e5a570d2931badea355abb05f57b6c47
Karthik Krishnan <karthik.krishnan@kitware.com>
2007-12-05 20:50:05 +0000

    BUG: The stencil isnt confined to the clipped region defined by rmax and rmin, when we arent getting the first extent for the current y,z

--- VTK/Imaging/vtkImageStencilData.cxx
+++ VTK/Imaging/vtkImageStencilData.cxx
@@ -414,6 +414,10 @@ int vtkImageStencilData::GetNextExtent(int &r1, int &r2,
       return 0;
       }
     r1 = clist[iter++];
+    if (r1 < rmin)
+      {
+      r1 = rmin;
+      }
     }
 
   if (r1 > rmax)
