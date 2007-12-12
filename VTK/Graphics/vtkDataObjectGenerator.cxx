713b22332fd5951db99775f19d0dbc93d7f6c728
Dave Demarle <dave.demarle@kitware.com>
2007-12-12 16:50:52 +0000

    COMP: Fix warnings

--- VTK/Graphics/vtkDataObjectGenerator.cxx
+++ VTK/Graphics/vtkDataObjectGenerator.cxx
@@ -135,7 +135,7 @@ public:
       {
       (*it)->print(level+1);
       }
-    if (type = GS || type == HDS || type == HBS || type == MBS)
+    if (type == GS || type == HDS || type == HBS || type == MBS)
       {
       for (int i = 0; i < level; i++)
         {
