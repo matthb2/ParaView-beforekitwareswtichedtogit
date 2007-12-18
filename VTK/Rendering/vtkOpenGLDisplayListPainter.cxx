2e6527d93e4e2fbf0c90086c0358ff5e2ffc6db7
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-12-18 18:06:15 +0000

    BUG: To compute time to draw correctly, when using display lists, we need to call glFinish.

--- VTK/Rendering/vtkOpenGLDisplayListPainter.cxx
+++ VTK/Rendering/vtkOpenGLDisplayListPainter.cxx
@@ -143,6 +143,8 @@ void vtkOpenGLDisplayListPainter::RenderInternal(vtkRenderer* renderer, vtkActor
     // if nothing has changed we use an old display list else
     // we use the newly generated list.
     glCallList(this->ListIds[i]);
+    glFinish(); // To compute time correctly, we need to wait 
+                // till OpenGL finishes.
     
     this->Timer->StopTimer();
     this->TimeToDraw += this->Timer->GetElapsedTime();
