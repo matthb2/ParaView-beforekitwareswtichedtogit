918235ce785e1722110f2fafb405586f6763eddb
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2007-12-27 18:30:30 +0000

    BUG: Fix segfault with arguments are null.

--- Servers/ServerManager/vtkSMAnimationSceneImageWriter.cxx
+++ Servers/ServerManager/vtkSMAnimationSceneImageWriter.cxx
@@ -153,6 +153,11 @@ vtkImageData* vtkSMAnimationSceneImageWriter::CaptureViewImage(
 //-----------------------------------------------------------------------------
 void vtkSMAnimationSceneImageWriter::Merge(vtkImageData* dest, vtkImageData* src)
 {
+  if (!src || !dest)
+    {
+    return;
+    }
+
   vtkImageIterator<unsigned char> inIt(src, src->GetExtent());
   int outextent[6];
   src->GetExtent(outextent);
