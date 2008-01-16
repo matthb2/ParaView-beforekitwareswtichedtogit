a2b0b475197fb3dc3c2d08ea641fd763e4c4261b
Jeffrey Baumes <jeff.baumes@kitware.com>
2008-01-16 20:17:52 +0000

    BUG: Moving ivar initialization earlier to avoid accessing uninitialized variables.

--- VTK/Infovis/vtkGraphMapper.cxx
+++ VTK/Infovis/vtkGraphMapper.cxx
@@ -59,6 +59,8 @@ vtkGraphMapper::vtkGraphMapper()
   this->IconTexture       = vtkSmartPointer<vtkTexture>::New();
   this->VertexColorArrayNameInternal = 0;
   this->EdgeColorArrayNameInternal = 0;
+  this->VertexPointSize = 5;
+  this->EdgeLineWidth = 1;
   
   this->VertexMapper->SetScalarModeToUsePointData();
   this->VertexMapper->SetLookupTable(this->VertexLookupTable);
@@ -88,8 +90,6 @@ vtkGraphMapper::vtkGraphMapper()
   this->ColorVerticesOff();
   this->SetEdgeColorArrayName("weight");
   this->ColorEdgesOff();
-  this->VertexPointSize = 5;
-  this->EdgeLineWidth = 1;
   
   // FIXME: Total hack for testing textures
 #if 0
