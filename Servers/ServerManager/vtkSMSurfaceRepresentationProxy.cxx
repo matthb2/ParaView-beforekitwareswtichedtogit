22da056d1056e72190c30cbc4e5da327fce4eb04
Berk Geveci <berk.geveci@kitware.com>
2008-01-25 15:44:57 +0000

    ENH: Added support for disabling LOD of individual representations.

--- Servers/ServerManager/vtkSMSurfaceRepresentationProxy.cxx
+++ Servers/ServerManager/vtkSMSurfaceRepresentationProxy.cxx
@@ -58,6 +58,8 @@ vtkSMSurfaceRepresentationProxy::vtkSMSurfaceRepresentationProxy()
   command->SetCallback(*this,
     &vtkSMSurfaceRepresentationProxy::ProcessViewInformation);
   this->ViewInformationObserver = command;
+
+  this->SuppressLOD = 0;
 }
 
 //----------------------------------------------------------------------------
@@ -193,7 +195,8 @@ void vtkSMSurfaceRepresentationProxy::ProcessViewInformation()
 
   bool use_lod = false;
   if (this->ViewInformation && 
-    this->ViewInformation->Has(vtkSMRenderViewProxy::USE_LOD()))
+      this->ViewInformation->Has(vtkSMRenderViewProxy::USE_LOD()) &&
+      !this->SuppressLOD)
     {
     use_lod = this->ViewInformation->Get(vtkSMRenderViewProxy::USE_LOD()) > 0;
     }
