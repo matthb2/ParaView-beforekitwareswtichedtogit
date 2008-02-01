dd703936e2987434c2d7f6d382c500e24b10eb79
Francois Bertel <francois.bertel@kitware.com>
2008-02-01 16:15:23 +0000

    COMP:Fixed old-style cast.

--- VTK/Filtering/vtkPolyLine.cxx
+++ VTK/Filtering/vtkPolyLine.cxx
@@ -215,7 +215,8 @@ int vtkPolyLine::GenerateSlidingNormals(vtkPoints *pts, vtkCellArray *lines,
 
           //compute rotation of line segment
           vtkMath::Cross (sNext, sPrev, q);
-          if ( (theta=asin((double)vtkMath::Normalize(q))) == 0.0 ) 
+          theta=asin(static_cast<double>(vtkMath::Normalize(q)));
+          if (theta==0.0) 
             { //no rotation, use previous normal
             normals->InsertTuple(linePts[j],normal);
             continue;
