a2a8e712967b68367065f86a9cddd294635a6d6b
Will Schroeder <will.schroeder@kitware.com>
2008-02-15 12:57:25 +0000

    BUG:Fixed PrintSelf() warning

--- VTK/Graphics/vtkTransformCoordinateSystems.cxx
+++ VTK/Graphics/vtkTransformCoordinateSystems.cxx
@@ -166,4 +166,42 @@ unsigned long vtkTransformCoordinateSystems::GetMTime()
 void vtkTransformCoordinateSystems::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
+
+  os << indent << "Input Coordinate System: ";
+  if ( this->InputCoordinateSystem == VTK_DISPLAY )
+    {
+    os << " DISPLAY\n";
+    }
+  else if ( this->InputCoordinateSystem == VTK_WORLD )
+    {
+    os << " WORLD\n";
+    }
+  else //if ( this->InputCoordinateSystem == VTK_VIEWPORT )
+    {
+    os << " VIEWPORT\n";
+    }
+  
+  os << indent << "Output Coordinate System: ";
+  if ( this->OutputCoordinateSystem == VTK_DISPLAY )
+    {
+    os << " DISPLAY\n";
+    }
+  else if ( this->OutputCoordinateSystem == VTK_WORLD )
+    {
+    os << " WORLD\n";
+    }
+  else //if ( this->OutputCoordinateSystem == VTK_VIEWPORT )
+    {
+    os << " VIEWPORT\n";
+    }
+  
+  os << indent << "Viewport: ";
+  if (this->Viewport)
+    {
+    os << this->Viewport << "\n";
+    }
+  else
+    {
+    os << "(none)\n";
+    }
 }
