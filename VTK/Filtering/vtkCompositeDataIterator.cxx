03e08cbf76497e1ce1800854d9b99f6685a01210
David Cole <David.Cole@kitware.com>
2008-02-17 21:56:42 +0000

    COMP: Eliminate dashboard warning about int to bool performance warning.

--- VTK/Filtering/vtkCompositeDataIterator.cxx
+++ VTK/Filtering/vtkCompositeDataIterator.cxx
@@ -197,7 +197,7 @@ void vtkCompositeDataIterator::GoToFirstItem()
     }
 
   vtkInternals::vtkLocation loc(this->DataSet->Internals->Children,
-    this->Reverse);
+    this->Reverse != 0);
   this->Internals->LocationStack.push_back(loc);
 
   this->Internals->EnsureStackValidity();
@@ -271,7 +271,7 @@ void vtkCompositeDataIterator::NextInternal()
       if (this->TraverseSubTree)
         {
         vtkCompositeDataSet* cds = vtkCompositeDataSet::SafeDownCast(loc.Data());
-        vtkInternals::vtkLocation subLoc(cds->Internals->Children, this->Reverse);
+        vtkInternals::vtkLocation subLoc(cds->Internals->Children, this->Reverse != 0);
         this->Internals->LocationStack.push_back(subLoc);
         }
       else
