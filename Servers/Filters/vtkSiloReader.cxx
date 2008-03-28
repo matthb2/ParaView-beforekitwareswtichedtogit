3af8686b9c09743991e31f13e815acc1e170392f
Pat Marion <pat.marion@kitware.com>
2008-03-28 14:50:01 +0000

    COMP: Change win32 ifdef to _WIN32

--- Servers/Filters/vtkSiloReader.cxx
+++ Servers/Filters/vtkSiloReader.cxx
@@ -677,7 +677,7 @@ void vtkSiloReader::SetFileName(char * filename)
 
   vtkstd::string fullName(filename);
   
-  #ifdef WIN32
+  #ifdef _WIN32
   char slashChar[] = "\\";
   #else
   char slashChar[] = "/";
