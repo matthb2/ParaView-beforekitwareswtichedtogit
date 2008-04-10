a895f3b1d2f649eab223ae854f7ee0fc5f738a3a
John Biddiscombe <biddisco@cscs.ch>
2008-04-10 12:35:38 +0000

    BUG: Removing the origin setting was making the individual blocks appear in the wrong place and causing the image tests using multi-block to fail. Might have a problem with parallel load, but this will fix tests for now.

--- Utilities/Xdmf2/vtk/vtkXdmfReader.cxx
+++ Utilities/Xdmf2/vtk/vtkXdmfReader.cxx
@@ -401,8 +401,8 @@ vtkXdmfReader::vtkXdmfReader()
   this->OutputTemporal = 0;
   this->TimeStep       = 0;
   this->ActualTimeStep = 0;
-  this->TimeStepRange[0] = 0.0;
-  this->TimeStepRange[1] = 0.0;
+  this->TimeStepRange[0] = 0;
+  this->TimeStepRange[1] = 0;
 }
 
 //----------------------------------------------------------------------------
@@ -2505,7 +2505,7 @@ int vtkXdmfReaderInternal::RequestGridData(
 //                   origin[2], origin[1], origin[0]);
 
     }
-//    vGrid->SetOrigin(origin[2], origin[1], origin[0]);
+    vGrid->SetOrigin(origin[2], origin[1], origin[0]);
 //    vGrid->SetDimensions(Dimensions[2], Dimensions[1], Dimensions[0]);
     stride[2] = readerStride[0];
     stride[1] = readerStride[1];
