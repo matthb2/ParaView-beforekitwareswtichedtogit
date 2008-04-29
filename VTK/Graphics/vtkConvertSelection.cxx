9183c71073b158be3fc6521255e1f1e5cb858055
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-04-29 18:50:37 +0000

    BUG: When converting to indices from global IDs, the composite index was not getting added.

--- VTK/Graphics/vtkConvertSelection.cxx
+++ VTK/Graphics/vtkConvertSelection.cxx
@@ -382,12 +382,10 @@ int vtkConvertSelection::ConvertCompositeDataSet(
       this->OutputType == vtkSelection::FRUSTUM) &&
       this->OutputType != vtkSelection::GLOBALIDS) 
       {
-      if (has_composite_key)
-        {
-        outputChild->GetProperties()->Set(vtkSelection::COMPOSITE_INDEX(), 
-          iter->GetCurrentFlatIndex());
-        }
-      if (has_hieararchical_key)
+      outputChild->GetProperties()->Set(vtkSelection::COMPOSITE_INDEX(),
+        iter->GetCurrentFlatIndex());
+
+      if (has_hieararchical_key && hbIter)
         {
         outputChild->GetProperties()->Set(vtkSelection::HIERARCHICAL_LEVEL(),
           hierarchical_level);
