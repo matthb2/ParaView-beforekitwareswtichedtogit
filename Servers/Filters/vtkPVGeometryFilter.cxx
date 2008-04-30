de48fc9f622af108a60b291b0ae8d81133af0951
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-04-30 13:39:15 +0000

    BUG: Remove ghost cells only after assigning original ids, otherwise original ids are not really original ids. This fixes BUG #5384.

--- Servers/Filters/vtkPVGeometryFilter.cxx
+++ Servers/Filters/vtkPVGeometryFilter.cxx
@@ -880,7 +880,6 @@ void vtkPVGeometryFilter::PolyDataExecute(
     else
       {
       out->ShallowCopy(input);
-      out->RemoveGhostCells(1);
       if (this->PassThroughCellIds)
         {
         vtkIdTypeArray *originalCellIds = vtkIdTypeArray::New();
@@ -913,6 +912,7 @@ void vtkPVGeometryFilter::PolyDataExecute(
         originalPointIds->Delete();
         originalPointIds = NULL;
         }
+      out->RemoveGhostCells(1);
       }
     return;
     }
