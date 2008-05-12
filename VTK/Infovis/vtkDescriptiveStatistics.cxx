3ba7425a334354d34eed58cacc9357ad35c80e94
Philippe Pebay <pppebay@sandia.gov>
2008-05-12 22:26:28 +0000

    ENH: better protection against inconsistent input STYLE: updated documentation

--- VTK/Infovis/vtkDescriptiveStatistics.cxx
+++ VTK/Infovis/vtkDescriptiveStatistics.cxx
@@ -333,6 +333,13 @@ void vtkDescriptiveStatistics::ExecuteEvince( vtkTable* dataset,
   
   for ( vtkstd::set<vtkIdType>::iterator it = this->Internals->Columns.begin(); it != this->Internals->Columns.end(); ++ it )
     {
+    if ( *it < 0 || *it >= nCol )
+      {
+      vtkWarningMacro( "Dataset table does not have a column with index "<<*it<<". Ignoring it." );
+      this->SampleSize = 0;
+      return;
+    }
+
     double nomVal = params->GetValue( 0, *it ).ToDouble();
     double accDev = params->GetValue( 1, *it ).ToDouble();
     double minVal = nomVal - accDev;
