6ff3c972769a3c0609f10e5d856861b1fe201a31
Timothy M. Shead <tshead@sandia.gov>
2008-05-23 22:06:04 +0000

    ENH: Eliminate a harmless warning when working with vtkArrayData.

--- Servers/Common/vtkPVDataInformation.cxx
+++ Servers/Common/vtkPVDataInformation.cxx
@@ -536,9 +536,10 @@ void vtkPVDataInformation::CopyFromObject(vtkObject* object)
     return;
     }
 
-  vtkErrorMacro("Could not cast object to a known data set: "
-                << (dobj?dobj->GetClassName():"(null"));
-
+  // Because custom applications may implement their own data
+  // object types, this isn't an error condition - just
+  // display the name of the data object and return quietly.
+  this->SetDataClassName(dobj->GetClassName());
 }
 
 //----------------------------------------------------------------------------
