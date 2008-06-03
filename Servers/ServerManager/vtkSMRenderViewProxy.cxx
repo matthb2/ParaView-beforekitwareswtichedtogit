8ef152d3a4fb49971ea5762f4acd13631e33a501
Yumin Yuan <yumin.yuan@kitware.com>
2008-06-03 20:21:53 +0000

    ENH: Made SelectFrustum() to use multiple_selections flag, instead of ignoring it.

--- Servers/ServerManager/vtkSMRenderViewProxy.cxx
+++ Servers/ServerManager/vtkSMRenderViewProxy.cxx
@@ -1377,7 +1377,7 @@ bool vtkSMRenderViewProxy::SelectFrustum(unsigned int x0,
                                          vtkCollection* selectedRepresentations,
                                          vtkCollection* selectionSources,
                                          vtkCollection* frustumSelections,
-                                         bool vtkNotUsed(multiple_selections),
+                                         bool multiple_selections,
                                          bool ofPoints/*=false*/)
 {
   int displayRectangle[4] = {x0, y0, x1, y1};
@@ -1506,7 +1506,10 @@ bool vtkSMRenderViewProxy::SelectFrustum(unsigned int x0,
       selectedRepresentations->AddItem(repr);
 
       selectionSource->Delete();
-      break;
+      if(!multiple_selections)
+        {
+        break;
+        }
       }
     }
 
