f64d0558840f303457c763464e985755f2ad30b5
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-06-05 18:55:25 +0000

    ENH: When state is loaded, proxies are created before they are registered i.e. UpdateVTKOBjects() is called before the proxy is registered.

--- Servers/ServerManager/vtkSMStateLoaderBase.cxx
+++ Servers/ServerManager/vtkSMStateLoaderBase.cxx
@@ -157,6 +157,8 @@ vtkSMProxy* vtkSMStateLoaderBase::NewProxyFromElement(vtkPVXMLElement* proxyElem
   vtksys_ios::ostringstream stream;
   stream << "Created New Proxy: " << proxy->GetXMLGroup() << " , " << proxy->GetXMLName();
   vtkProcessModule::DebugLog(stream.str().c_str());
+  proxy->UpdateVTKObjects();
+
   // Give subclasses a chance to process the newly created proxy.
   this->CreatedNewProxy(id, proxy);
   return proxy;
