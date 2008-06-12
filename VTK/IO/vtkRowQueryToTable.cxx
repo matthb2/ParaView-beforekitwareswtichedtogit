16737d027ab1442d58e137deb01dd3d87289cbab
Jeffrey Baumes <jeff.baumes@kitware.com>
2008-06-12 16:17:58 +0000

    ENH: Ensure that all column names are distinct.

--- VTK/IO/vtkRowQueryToTable.cxx
+++ VTK/IO/vtkRowQueryToTable.cxx
@@ -29,6 +29,8 @@
 #include "vtkVariant.h"
 #include "vtkVariantArray.h"
 
+#include <vtksys/ios/sstream>
+
 vtkCxxRevisionMacro(vtkRowQueryToTable, "$Revision$");
 vtkStandardNewMacro(vtkRowQueryToTable);
 
@@ -109,7 +111,28 @@ int vtkRowQueryToTable::RequestData(
       arr = vtkAbstractArray::CreateArray(type);
       }
 
-    arr->SetName(this->Query->GetFieldName(c));
+    // Make sure name doesn't clash with existing name.
+    const char* name = this->Query->GetFieldName(c);
+    if (output->GetColumnByName(name))
+      {
+      int i = 1;
+      vtksys_ios::ostringstream oss;
+      vtkStdString newName;
+      do
+        {
+        oss.str("");
+        oss << name << "_" << i;
+        newName = oss.str();
+        ++i;
+        } while (output->GetColumnByName(newName));
+      arr->SetName(newName);
+      }
+    else
+      {
+      arr->SetName(name);
+      }
+
+    // Add the column to the table.
     output->AddColumn(arr);
     arr->Delete();
     }
