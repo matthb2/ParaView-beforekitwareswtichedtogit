bb01e0f60d7a5aff156ba0ee634e317fb47b7afb
Will Schroeder <will.schroeder@kitware.com>
2008-06-14 11:39:54 +0000

    BUG:Cleaning up test failures (otherPrint, TestEmptyInput, SetGet

--- VTK/Geovis/vtkGeoSampleArcs.cxx
+++ VTK/Geovis/vtkGeoSampleArcs.cxx
@@ -71,6 +71,10 @@ int vtkGeoSampleArcs::RequestData(
   vtkCellArray* lines = input->GetLines();
   vtkCellArray* newLines = vtkCellArray::New();
   vtkPoints* points = input->GetPoints();
+  if ( !points )
+    {
+    return 0;
+    }
   float* pointsPtr = static_cast<float*>(points->GetVoidPointer(0));
   vtkPoints* newPoints = vtkPoints::New();
   lines->InitTraversal();
