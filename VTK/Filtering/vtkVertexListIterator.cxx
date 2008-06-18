416f17f1a755011c9395a1f49df756bb86ab8b47
Douglas Gregor <dgregor@osl.iu.edu>
2008-06-18 14:32:57 +0000

    ENH: Merge VTK-DistribGraph branch, providing support for parallel computation on distributed vtkGraphs.

--- VTK/Filtering/vtkVertexListIterator.cxx
+++ VTK/Filtering/vtkVertexListIterator.cxx
@@ -20,6 +20,9 @@
 
 #include "vtkVertexListIterator.h"
 
+#include "vtkDataObject.h"
+#include "vtkDistributedGraphHelper.h"
+#include "vtkInformation.h"
 #include "vtkObjectFactory.h"
 #include "vtkGraph.h"
 
@@ -50,6 +53,18 @@ void vtkVertexListIterator::SetGraph(vtkGraph *graph)
     {
     this->Current = 0;
     this->End = this->Graph->GetNumberOfVertices();
+
+    // For a distributed graph, shift the iteration space to cover
+    // local vertices
+    vtkDistributedGraphHelper *helper 
+      = this->Graph->GetDistributedGraphHelper();
+    if (helper)
+      {
+      int myRank 
+        = graph->GetInformation()->Get(vtkDataObject::DATA_PIECE_NUMBER());
+      this->Current = helper->MakeDistributedId(myRank, this->Current);
+      this->End = helper->MakeDistributedId(myRank, this->End);
+      }
     }
 }
 
