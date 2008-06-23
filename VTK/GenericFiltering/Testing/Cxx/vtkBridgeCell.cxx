bfd0495270da90141c2d03d27baf3fadbc097e00
Francois Bertel <francois.bertel@kitware.com>
2008-06-23 19:32:25 +0000

    BUG:Fixed missing parentheses.

--- VTK/GenericFiltering/Testing/Cxx/vtkBridgeCell.cxx
+++ VTK/GenericFiltering/Testing/Cxx/vtkBridgeCell.cxx
@@ -446,7 +446,7 @@ int vtkBridgeCell::EvaluatePosition(double x[3],
       }
     }
 
-  assert("post: valid_result" && result==-1 || result==0 || result==1);
+  assert("post: valid_result" && (result==-1 || result==0 || result==1));
   assert("post: positive_distance" && (!(result!=-1) || (!(closestPoint!=0)||dist2>=0))); // A=>B: !A || B
   return result;
 }
