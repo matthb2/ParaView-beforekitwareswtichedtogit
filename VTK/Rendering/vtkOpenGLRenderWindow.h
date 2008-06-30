b1af71d5ef500bebbf6981b71775d61f7a7c0b15
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2008-06-30 12:13:10 +0000

    STYLE: minor style

--- VTK/Rendering/vtkOpenGLRenderWindow.h
+++ VTK/Rendering/vtkOpenGLRenderWindow.h
@@ -1,15 +1,15 @@
 /*=========================================================================
 
-Program:   Visualization Toolkit
-Module:    $RCSfile$
+  Program:   Visualization Toolkit
+  Module:    $RCSfile$
 
-Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-All rights reserved.
-See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
 
-This software is distributed WITHOUT ANY WARRANTY; without even
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the above copyright notice for more information.
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
 // .NAME vtkOpenGLRenderWindow - OpenGL rendering window
