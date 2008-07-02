6c3688b6c4d9f86e6b10d94be416db08a150a475
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-07-02 12:55:43 +0000

    COMP: fixed more warnings.

--- VTK/Hybrid/vtkX3DExporterXMLWriter.cxx
+++ VTK/Hybrid/vtkX3DExporterXMLWriter.cxx
@@ -264,12 +264,10 @@ void vtkX3DExporterXMLWriter::SetField(int attributeID, float value)
 }
 
 //-----------------------------------------------------------------------------
-void vtkX3DExporterXMLWriter::SetField(int attributeID, double value)
+void vtkX3DExporterXMLWriter::SetField(int attributeID, double vtkNotUsed(value))
 {
   this->OutputStream << " " << x3dAttributeString[attributeID] << "=\"" << "WHY DOUBLE?" << "\"";
   assert(false);
-  // prevent ununsed parameter warning
-  value = 0.0;
 }
 
 //-----------------------------------------------------------------------------
