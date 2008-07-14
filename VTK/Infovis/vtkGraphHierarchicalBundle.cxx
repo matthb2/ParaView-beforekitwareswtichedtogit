d0692a98ec557f899a815cc2536ac0c6e74d7d36
Brian Wylie <bnwylie@sandia.gov>
2008-07-14 21:30:35 +0000

    BUG:  vtkVariantCompare had a serious bug in it. Deleted all instances of it and replacing with vtkVariantLessThan which now properly supports heterogeneous types.

--- VTK/Infovis/vtkGraphHierarchicalBundle.cxx
+++ VTK/Infovis/vtkGraphHierarchicalBundle.cxx
@@ -38,31 +38,6 @@
 #include <vtksys/stl/map>
 using vtksys_stl::map;
 
-
-//---------------------------------------------------------------------------
-class vtkVariantCompare
-{
-public:
-  bool operator()(
-    const vtkVariant& a,
-    const vtkVariant& b) const
-  {
-    if (a.GetType() == VTK_STRING)
-      {
-      return a.ToString() < b.ToString();
-      }
-    else if (a.IsNumeric())
-      {
-      return a.ToDouble() < b.ToDouble();
-      }
-    else
-      {
-      // Punt, just do pointer difference.
-      return &a < &b;
-      }
-  }
-};
-
 //---------------------------------------------------------------------------
 template <typename T>
 vtkVariant vtkGetValue(T* arr, vtkIdType index)
@@ -182,7 +157,7 @@ int vtkGraphHierarchicalBundle::RequestData(
         }
       }
 
-    map<vtkVariant,vtkIdType,vtkVariantCompare> graphIdMap;
+    map<vtkVariant,vtkIdType,vtkVariantLessThan> graphIdMap;
     
     // Create a map from graph id to graph index
     for (int i=0; i<graph->GetNumberOfVertices(); ++i)
@@ -194,7 +169,7 @@ int vtkGraphHierarchicalBundle::RequestData(
     for (int i=0; i<tree->GetNumberOfVertices(); ++i)
       {
       vtkVariant id = vtkGetVariantValue(treeIdArray,i);
-      if (graphIdMap.count(id) > 0)
+      if (graphIdMap.count(id))
         {
         graphIndexToTreeIndex[graphIdMap[id]] = i;
         }
