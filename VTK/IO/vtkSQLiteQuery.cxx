70f7bac703129dd689a141de851a8c295b4e52cc
David Cole <David.Cole@kitware.com>
2008-07-25 20:16:32 +0000

    BUG: Do not use cout from a PrintSelf method. Do not try to stream a null const char*.

--- VTK/IO/vtkSQLiteQuery.cxx
+++ VTK/IO/vtkSQLiteQuery.cxx
@@ -70,22 +70,21 @@ vtkSQLiteQuery::~vtkSQLiteQuery()
 void vtkSQLiteQuery::PrintSelf(ostream  &os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
-  
+
   os << indent << "Statement: ";
   if (this->Statement)
     {
-    cout << this->Statement << "\n";
+    os << this->Statement << "\n";
     }
   else
     {
-    cout << "(null)" << "\n";
+    os << "(null)" << "\n";
     }
-  os << indent << "InitialFetch: " << InitialFetch; 
-  os << indent << "InitialFetchResult: " << InitialFetchResult; 
-  os << indent << "TransactionInProgress: " << TransactionInProgress;  
-  os << indent << "LastErrorText: " 
-    << (this->LastErrorText ? this->LastErrorText : "NULL") << endl;
-
+  os << indent << "InitialFetch: " << InitialFetch;
+  os << indent << "InitialFetchResult: " << InitialFetchResult;
+  os << indent << "TransactionInProgress: " << TransactionInProgress;
+  os << indent << "LastErrorText: "
+    << (this->LastErrorText ? this->LastErrorText : "(null)") << endl;
 }
 
 // ----------------------------------------------------------------------
