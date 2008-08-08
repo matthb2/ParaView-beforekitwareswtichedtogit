5c17519eec8f8bf99f3caf9d9c35dc63537f2b5a
John Biddiscombe <biddisco@cscs.ch>
2008-08-08 15:29:22 +0000

    BUG: vtkIdType should have been int

--- VTK/Filtering/vtkAbstractCellLocator.cxx
+++ VTK/Filtering/vtkAbstractCellLocator.cxx
@@ -99,7 +99,7 @@ int vtkAbstractCellLocator::IntersectWithLine(
   return 0;
 }
 //----------------------------------------------------------------------------
-vtkIdType vtkAbstractCellLocator::IntersectWithLine(
+int vtkAbstractCellLocator::IntersectWithLine(
   const double [3]vtkNotUsed(a0), const double [3]vtkNotUsed(a1),
   vtkPoints *vtkNotUsed(points), vtkIdList *vtkNotUsed(cellIds))
 {
