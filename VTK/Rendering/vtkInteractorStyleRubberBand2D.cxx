5f91e8d5ce26d50eca8c067537e836c6377f5eef
Dave Demarle <dave.demarle@kitware.com>
2008-08-15 20:41:30 +0000

    BUG: #3713. Dragging rubber band off window could crash. Thanks Gerrick Bivins for the patch.

--- VTK/Rendering/vtkInteractorStyleRubberBand2D.cxx
+++ VTK/Rendering/vtkInteractorStyleRubberBand2D.cxx
@@ -189,6 +189,23 @@ void vtkInteractorStyleRubberBand2D::OnMouseMove()
     {
     this->EndPosition[0] = this->Interactor->GetEventPosition()[0];
     this->EndPosition[1] = this->Interactor->GetEventPosition()[1];  
+    int *size = this->Interactor->GetRenderWindow()->GetSize();  
+    if (this->EndPosition[0] > (size[0]-1))
+      {
+      this->EndPosition[0] = size[0]-1;
+      }
+    if (this->EndPosition[0] < 0)
+      {
+      this->EndPosition[0] = 0;
+      }
+    if (this->EndPosition[1] > (size[1]-1))
+      {
+      this->EndPosition[1] = size[1]-1;
+      }
+    if (this->EndPosition[1] < 0)
+      {
+      this->EndPosition[1] = 0;
+      }    
     this->InvokeEvent(vtkCommand::InteractionEvent);
     this->RedrawRubberBand();
     }
