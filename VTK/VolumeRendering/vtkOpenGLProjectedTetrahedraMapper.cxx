694e5f814b8b2ec6bdb4ae68753f957c15c3db6f
Ken Moreland <kmorel@sandia.gov>
2008-08-26 13:57:26 +0000

    BUG: Make sure that the polygon fill mode and culling are reset to where we expect.

--- VTK/VolumeRendering/vtkOpenGLProjectedTetrahedraMapper.cxx
+++ VTK/VolumeRendering/vtkOpenGLProjectedTetrahedraMapper.cxx
@@ -426,6 +426,8 @@ void vtkOpenGLProjectedTetrahedraMapper::ProjectTetrahedra(vtkRenderer *renderer
   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
   glShadeModel(GL_SMOOTH);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  glDisable(GL_CULL_FACE);
   
   // save the default blend function.
   glPushAttrib(GL_COLOR_BUFFER_BIT);
