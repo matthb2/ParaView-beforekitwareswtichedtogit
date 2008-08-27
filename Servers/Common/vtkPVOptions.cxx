db73d8a61335ee85de4a715a8fed032ca8bbcbfe
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-08-27 20:15:13 +0000

    ENH: Fixed BUG #5988. It's now possible to specify file-series to load from command-line. To specify file series replace the numeral with a '.' eg. my0.vtk, my1.vtk...myN.vtk becomes my..vtk

--- Servers/Common/vtkPVOptions.cxx
+++ Servers/Common/vtkPVOptions.cxx
@@ -102,7 +102,9 @@ vtkPVOptions::~vtkPVOptions()
 void vtkPVOptions::Initialize()
 {
   this->AddArgument("--data", 0, &this->ParaViewDataName,
-                    "Load the specified data.", 
+                    "Load the specified data. "
+                    "To specify file series replace the numeral with a '.' eg. "
+                    "my0.vtk, my1.vtk...myN.vtk becomes my..vtk",
                     vtkPVOptions::PVCLIENT|vtkPVOptions::PARAVIEW);
   /*
   this->AddBooleanArgument("--client-render-server", "-crs", &this->ClientRenderServer,
