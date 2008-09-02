1208b23eb9c892ef96b77983ede7ad11ed03905c
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-09-02 13:18:29 +0000

    COMP: reverting patch since the warning is indeed legitimate.

--- VTK/Parallel/vtkPKdTree.cxx
+++ VTK/Parallel/vtkPKdTree.cxx
@@ -290,9 +290,7 @@ double *vtkPKdTree::VolumeBounds()
   // Get the spatial bounds of the whole volume
   
   double *volBounds = new double [6];
-  double localMin[3], localMax[3],
-         globalMin[3] = {0, 0, 0},
-         globalMax[3] = {0, 0, 0};
+  double localMin[3], localMax[3], globalMin[3], globalMax[3];
 
   int number_of_datasets = this->GetNumberOfDataSets();
   if (number_of_datasets == 0)
