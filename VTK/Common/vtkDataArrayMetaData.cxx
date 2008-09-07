5b9c7f6634c96bd06b8f062c8ce1bcd8cf304a88
Burlen Loring <burlen.loring@kitware.com>
2008-09-07 16:04:45 +0000

    COMP: Added all the trappings of VTK object to DataArrayMetaData so that it will pass header testing etc.

---
+++ VTK/Common/vtkDataArrayMetaData.cxx
@@ -0,0 +1,11 @@
+
+#include "vtkDataArrayMetaData.h"
+#include "vtkObjectFactory.h"
+
+vtkStandardNewMacro(vtkDataArrayMetaData);
+vtkCxxRevisionMacro(vtkDataArrayMetaData, "$Revision$");
+
+void vtkDataArrayMetaData::PrintSelf(ostream &os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
