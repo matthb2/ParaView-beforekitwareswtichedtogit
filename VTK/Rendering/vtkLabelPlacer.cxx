522d4bf87bb6161f69ee714c1d7a6b684da0b8d4
Dave Partyka <dave.partyka@kitware.com>
2008-09-15 23:35:35 +0000

    COMP: reduce compile errors of octree for VS6.

--- VTK/Rendering/vtkLabelPlacer.cxx
+++ VTK/Rendering/vtkLabelPlacer.cxx
@@ -140,7 +140,7 @@ public:
       {
       for ( int ty = ty0; ty <= ty1; ++ ty )
         {
-        ScreenTile& tile( this->Tiles[tx][ty] );
+        ScreenTile tile( this->Tiles[tx][ty] );
         // Do this check here for speed, even though we repeat w/ small mod below.
         if ( ! tile.IsSpotOpen( opacity, r ) ) 
           return false;
