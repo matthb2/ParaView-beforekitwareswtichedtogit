c62a53199e55fa7e3f2af65831ac203318cf71cf
Burlen Loring <burlen.loring@kitware.com>
2008-09-15 21:29:33 +0000

    BUG: When there are no fragments in PrepareToCollectGeometricAttributes was taking the address of the first element of an empty vector.

--- Servers/Filters/vtkCTHFragmentConnect.cxx
+++ Servers/Filters/vtkCTHFragmentConnect.cxx
@@ -6586,7 +6586,15 @@ int vtkCTHFragmentConnect::PrepareToCollectGeometricAttributes(
     }
   // ids
   ids.resize(nProcs,static_cast<int *>(0));
-  ids[myProcId]=&this->ResolvedFragmentIds[this->MaterialId][0];
+  if (this->ResolvedFragmentIds[this->MaterialId].size()!=0)
+    {
+    ids[myProcId]=&(this->ResolvedFragmentIds[this->MaterialId][0]);
+    }
+  else
+    {
+    ids[myProcId]=0;
+    }
+  
   // note, this could be a problem if we need to update ResolvedFragmentIds
   // but we don't need to since after the gather we have everything which
   // is just sequential 0-nFragmentsResolvedFragments. Also we do need to 
