ddea739329270ea6cbcf346fff70f45ff0490425
Brian Wylie <bnwylie@sandia.gov>
2008-10-06 18:10:03 +0000

    ENH: EdgeWeightField should not be printed out in PrintSelf, that variable belongs to the super class.

--- VTK/Infovis/vtkSimple2DLayoutStrategy.cxx
+++ VTK/Infovis/vtkSimple2DLayoutStrategy.cxx
@@ -356,7 +356,6 @@ void vtkSimple2DLayoutStrategy::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "MaxNumberOfIterations: " << this->MaxNumberOfIterations << endl;
   os << indent << "IterationsPerLayout: " << this->IterationsPerLayout << endl;
   os << indent << "CoolDownRate: " << this->CoolDownRate << endl;
-  os << indent << "EdgeWeightField: " << (this->EdgeWeightField ? this->EdgeWeightField : "(none)") << endl;
   os << indent << "Jitter: " << (this->Jitter ? "True" : "False") << endl;
   os << indent << "RestDistance: " << this->RestDistance << endl;
 }
