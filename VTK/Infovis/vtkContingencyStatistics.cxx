89591bdd3e941c4bf12bd7ca75d47e61e86dd3b7
Philippe Pebay <pppebay@sandia.gov>
2008-10-09 21:32:58 +0000

    BUG: Standard Deviation was not stored.

--- VTK/Infovis/vtkContingencyStatistics.cxx
+++ VTK/Infovis/vtkContingencyStatistics.cxx
@@ -397,7 +397,7 @@ void vtkContingencyStatistics::ExecuteAssess( vtkTable* inData,
         pxy = yit->second;
         pdfYcondX[x][y] = pxy / pdfX[x];
         pdfXcondY[x][y] = pxy / pdfY[y];
-        
+
         HXY -= pxy * log( pxy );
         HYcondX -= pxy * log( pdfYcondX[x][y] );
         HXcondY -= pxy * log( pdfXcondY[x][y] );
