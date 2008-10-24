f7bbdf23795fc7c5a27a20131f01643d95ca4ffb
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-10-24 16:38:31 +0000

    COMP: Fixed warnings

--- Servers/Common/vtkPMPISelfConnection.cxx
+++ Servers/Common/vtkPMPISelfConnection.cxx
@@ -38,7 +38,8 @@ void vtkPMPISelfConnection::Finalize()
 }
 
 //----------------------------------------------------------------------------
-int vtkPMPISelfConnection::InitializeSatellite(int argc, char** argv)
+int vtkPMPISelfConnection::InitializeSatellite(
+  int vtkNotUsed(argc), char** vtkNotUsed(argv))
 {
   this->RegisterSatelliteRMIs();
 
