62cda534cf09b9502201c69ce6c5ea43e78ce378
Philippe Pebay <pppebay@sandia.gov>
2008-11-04 18:48:44 +0000

    STYLE: document the fact that the Validate output port (3rd -- #2) is not        used for now.

--- VTK/Infovis/vtkStatisticsAlgorithm.cxx
+++ VTK/Infovis/vtkStatisticsAlgorithm.cxx
@@ -111,7 +111,7 @@ int vtkStatisticsAlgorithm::RequestData( vtkInformation*,
   // Extract output tables
   vtkTable* outData = vtkTable::GetData( outputVector, 0 );
   vtkTable* outMeta1 = vtkTable::GetData( outputVector, 1 );
-  vtkTable* outMeta2 = vtkTable::GetData( outputVector, 2 );
+  vtkTable* outMeta2; // Unused for now
 
   outData->ShallowCopy( inData );
 
