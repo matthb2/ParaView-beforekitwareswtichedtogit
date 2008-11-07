2b1c3fbc1647908f4f80dab8251120d967041765
Dave Partyka <dave.partyka@kitware.com>
2008-11-07 20:10:34 +0000

    BUG: Fix origin issues on terrain.

--- VTK/Geovis/vtkGeoTerrain.cxx
+++ VTK/Geovis/vtkGeoTerrain.cxx
@@ -264,9 +264,7 @@ void vtkGeoTerrain::AddActors(
       mapper->SetInput(cur->GetModel());
       mapper->ScalarVisibilityOff();
       actor->SetMapper(mapper);
-      //actor->SetPosition(0.0, 0.0, -0.1);
       actor->SetPosition(-this->Origin[0], -this->Origin[1], -this->Origin[2] - 0.1);
-      //actor->SetOrigin(this->Origin);
       visibleActors++;
 
       if (textureNode1)
