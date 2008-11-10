3cdf39598fa72a25d9c0b6196d279d35dcf6d4e1
Pat Marion <pat.marion@kitware.com>
2008-11-10 18:19:22 +0000

    ENH: Streaming app/plugin now uses a helper proxy to store streaming settings.  The get/set methods for streaming settings have been removed from pqServer and vtkSMProxyManager.

--- Servers/ServerManager/vtkSMProxyManager.cxx
+++ Servers/ServerManager/vtkSMProxyManager.cxx
@@ -107,12 +107,6 @@ vtkSMProxyManager::vtkSMProxyManager()
   this->AddObserver(vtkCommand::RegisterEvent, obs);
   this->AddObserver(vtkCommand::UnRegisterEvent, obs);
 #endif
-  this->StreamedPasses = 16;
-  this->EnableStreamMessages = 0;
-  this->UseCulling = 1;
-  this->UseViewOrdering = 1;
-  this->PieceCacheLimit = 16;
-  this->PieceRenderCutoff = -1;
 }
 
 //---------------------------------------------------------------------------
@@ -1617,16 +1611,6 @@ vtkPVXMLElement* vtkSMProxyManager::GetPropertyHints(
       }
     }
   return 0;
-}
-
-//---------------------------------------------------------------------------
-void vtkSMProxyManager::SetStreamedPasses(int n)
-{
-  if (n > 0 && n != this->StreamedPasses)
-    {
-    this->StreamedPasses = n;
-    this->Modified();
-    }
 }
 
 //---------------------------------------------------------------------------
