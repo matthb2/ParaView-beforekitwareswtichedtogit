1de690ac7e0c243ebd97bbe18b1f5de4d967a8f0
Dave Demarle <dave.demarle@kitware.com>
2008-11-11 20:03:20 +0000

    ENH: Working to enable streaming in parallel again

--- Plugins/Streaming/Server/vtkSMSUnstructuredDataParallelStrategy.cxx
+++ Plugins/Streaming/Server/vtkSMSUnstructuredDataParallelStrategy.cxx
@@ -86,6 +86,13 @@ void vtkSMSUnstructuredDataParallelStrategy::BeginCreateVTKObjects()
 //----------------------------------------------------------------------------
 void vtkSMSUnstructuredDataParallelStrategy::CreatePipeline(vtkSMSourceProxy* input, int outputport)
 {
+  //turn off caching for animation it will interfere with streaming
+  vtkSMSourceProxy *cacher =
+    vtkSMSourceProxy::SafeDownCast(this->GetSubProxy("CacheKeeper"));
+  vtkSMIntVectorProperty *ivp = vtkSMIntVectorProperty::SafeDownCast(
+    cacher->GetProperty("CachingEnabled"));
+  ivp->SetElement(0, 0);
+
   this->Connect(input, this->ViewSorter);
   this->Connect(this->ViewSorter, this->PieceCache);
   this->Superclass::CreatePipeline(this->PieceCache, outputport);
