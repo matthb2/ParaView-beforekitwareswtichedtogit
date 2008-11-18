370fd0e6610787d42d06bec53a6f88fadfb99fac
Dave Partyka <dave.partyka@kitware.com>
2008-11-18 15:41:13 +0000

    BUG: Fix runtime corruption in SetInput.

--- VTK/Graphics/vtkDijkstraImageGeodesicPath.cxx
+++ VTK/Graphics/vtkDijkstraImageGeodesicPath.cxx
@@ -81,7 +81,7 @@ void vtkDijkstraImageGeodesicPath::SetInput( vtkDataObject *input )
 
   image->UpdateInformation();
   int* dimensions = image->GetDimensions();
-  int u[2];
+  int u[3];
   int n = 0;
   for ( int i = 0; i < 3; ++i )
     {
