2178d0451bd31206aa33ed5f257016892614491e
Andy Bauer <andy.bauer@kitware.com>
2008-11-25 19:36:12 +0000

    BUG: Fixed bug using distance squared instead of radius (square root of distance squared).

--- VTK/Filtering/vtkKdTree.cxx
+++ VTK/Filtering/vtkKdTree.cxx
@@ -2263,7 +2263,7 @@ vtkIdType vtkKdTree::FindClosestPoint(double x, double y, double z, double &dist
 
     newCloseId = 
       this->FindClosestPointInSphere(x, y, z,
-                                     minDistance2,    // radius
+                                     sqrt(minDistance2),    // radius
                                      regionId,        // skip this region
                                      newDistance2);   // distance to closest point
     
@@ -2283,9 +2283,9 @@ vtkIdType vtkKdTree::FindClosestPoint(double x, double y, double z, double &dist
         // The closest point may be in a neighboring region
 
         newCloseId = this->FindClosestPointInSphere(x, y, z,
-                                     minDistance2,   // radius
-                                     regionId,       // skip this region
-                                     newDistance2);
+                                                    sqrt(minDistance2),   // radius
+                                                    regionId,       // skip this region
+                                                    newDistance2);
         }
       }
     }
