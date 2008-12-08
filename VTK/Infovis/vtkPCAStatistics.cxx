09f557ded58beb779d6e16f70a3ffb5b87776a1f
Philippe Pebay <pppebay@sandia.gov>
2008-12-08 16:27:47 +0000

    ENH: modified SelectAssessFunctor(), to make it responsible for the creation      of assessment columns as well. This allows the developer to specify      the desired data type at the concrete class level. STYLE: removed unnecessary parameter ENH: modified tests accordingly

--- VTK/Infovis/vtkPCAStatistics.cxx
+++ VTK/Infovis/vtkPCAStatistics.cxx
@@ -592,7 +592,11 @@ void vtkPCAStatistics::ExecuteAssess(
       continue;
       }
 
-    this->SelectAssessFunctor( inData, reqModel, 0, this->AssessParameters, dfunc );
+    this->SelectAssessFunctor( inData, 
+                               reqModel, 
+                               0, 
+                               dfunc );
+
     vtkPCAAssessFunctor* pcafunc = static_cast<vtkPCAAssessFunctor*>( dfunc );
     if ( ! pcafunc )
       {
@@ -644,10 +648,10 @@ void vtkPCAStatistics::ExecuteAssess(
     }
 }
 
-void vtkPCAStatistics::SelectAssessFunctor(
-  vtkTable* inData, vtkDataObject* inMetaDO,
-  vtkStringArray* vtkNotUsed(rowNames), vtkStringArray* vtkNotUsed(columnNames),
-  AssessFunctor*& dfunc )
+void vtkPCAStatistics::SelectAssessFunctor( vtkTable* inData, 
+                                            vtkDataObject* inMetaDO,
+                                            vtkStringArray* vtkNotUsed(rowNames), 
+                                            AssessFunctor*& dfunc )
 {
   dfunc = 0;
   vtkTable* reqModel = vtkTable::SafeDownCast( inMetaDO );
