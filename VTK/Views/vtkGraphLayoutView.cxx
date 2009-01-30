8ffff2227f1ca95d2efbab6615da2cb86dc2e3fe
Dave Partyka <dave.partyka@kitware.com>
2009-01-30 00:01:55 +0000

    BUG: revert changes to vtkGraphLayoutView until I can figure out what is causing test that use GraphLayoutView to fail.

--- VTK/Views/vtkGraphLayoutView.cxx
+++ VTK/Views/vtkGraphLayoutView.cxx
@@ -178,8 +178,9 @@ vtkGraphLayoutView::vtkGraphLayoutView()
   this->EdgeLayout->SetInputConnection(this->GraphLayout->GetOutputPort());
   this->VertexDegree->SetInputConnection(this->EdgeLayout->GetOutputPort());
   
-  this->PerturbCoincidentVertices->SetInputConnection(this->VertexDegree->GetOutputPort());
-  this->GraphMapper->SetInputConnection(this->PerturbCoincidentVertices->GetOutputPort());
+  //this->PerturbCoincidentVertices->SetInputConnection(this->VertexDegree->GetOutputPort());
+  //this->GraphMapper->SetInputConnection(this->PerturbCoincidentVertices->GetOutputPort());
+  this->GraphMapper->SetInputConnection(this->VertexDegree->GetOutputPort());
   this->GraphActor->SetMapper(this->GraphMapper);
   this->VertexLabelMapper->SetInputConnection(this->VertexDegree->GetOutputPort());
   this->VertexLabelActor->SetMapper(this->VertexLabelMapper);
