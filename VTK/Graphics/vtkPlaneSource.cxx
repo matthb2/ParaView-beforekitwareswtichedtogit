48ea0d0b7940fa7b0e2b3acdb7562df4000e47af
Francois Bertel <francois.bertel@kitware.com>
2009-02-03 16:12:29 +0000

    COMP:Removed old-style cast.

--- VTK/Graphics/vtkPlaneSource.cxx
+++ VTK/Graphics/vtkPlaneSource.cxx
@@ -123,10 +123,10 @@ int vtkPlaneSource::RequestData(
   //
   for (numPts=0, i=0; i<(this->YResolution+1); i++)
     {
-    tc[1] = (double) i / this->YResolution;
+    tc[1] = static_cast<double>(i)/ this->YResolution;
     for (j=0; j<(this->XResolution+1); j++)
       {
-      tc[0] = (double) j / this->XResolution;
+      tc[0] = static_cast<double>(j) / this->XResolution;
 
       for ( ii=0; ii < 3; ii++)
         {
