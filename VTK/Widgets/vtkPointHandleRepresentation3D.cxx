c1553caa2c4b1104bafa4a9b92520ed150de6612
Karthik Krishnan <karthik.krishnan@kitware.com>
2009-02-03 21:56:53 +0000

    ENH: Sanity check to see if the handle poistion is valid.

--- VTK/Widgets/vtkPointHandleRepresentation3D.cxx
+++ VTK/Widgets/vtkPointHandleRepresentation3D.cxx
@@ -665,6 +665,15 @@ void vtkPointHandleRepresentation3D::ReleaseGraphicsResources(vtkWindow *win)
 int vtkPointHandleRepresentation3D::RenderOpaqueGeometry(vtkViewport *viewport)
 {
   this->BuildRepresentation();
+
+  // Sanity check
+  double worldPos[3];
+  this->GetWorldPosition( worldPos );
+  if (worldPos[0] == VTK_DOUBLE_MAX)
+    {
+    return 0;
+    }
+
   return this->Actor->RenderOpaqueGeometry(viewport);
 }
 
@@ -673,6 +682,15 @@ int vtkPointHandleRepresentation3D::RenderTranslucentPolygonalGeometry(
   vtkViewport *viewport)
 {
   this->BuildRepresentation();
+
+  // Sanity check
+  double worldPos[3];
+  this->GetWorldPosition( worldPos );
+  if (worldPos[0] == VTK_DOUBLE_MAX)
+    {
+    return 0;
+    }
+  
   return this->Actor->RenderTranslucentPolygonalGeometry(viewport);
 }
 //-----------------------------------------------------------------------------
