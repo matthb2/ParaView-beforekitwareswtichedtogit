72c0d6dfd2c451d6e15ce90ae90dc3e8dd74aaf5
Dave Partyka <dave.partyka@kitware.com>
2009-02-05 12:09:10 +0000

    COMP: Fix warning.

--- VTK/Infovis/vtkPerturbCoincidentVertices.cxx
+++ VTK/Infovis/vtkPerturbCoincidentVertices.cxx
@@ -169,7 +169,7 @@ int vtkPerturbCoincidentVertices::RequestData(
     // Iterate over all coincident point ids and perturb them
     numCoincidentPoints = coincidentPoints->GetNumberOfIds();
     vtkMath::SpiralPoints( numCoincidentPoints + 1, offsets );
-    for(int i = 0; i < numCoincidentPoints; ++i)
+    for(i = 0; i < numCoincidentPoints; ++i)
       {
       Id = coincidentPoints->GetId(i);
       points->GetPoint(Id, point);
