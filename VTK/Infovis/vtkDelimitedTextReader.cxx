f7ea59684655144bb41a529b7cb885f9caedbe20
Jeffrey Baumes <jeff.baumes@kitware.com>
2009-02-24 14:24:46 +0000

    COMP: Need to use vtkStdString's size_type to avoid errors on 64 bit machines.

--- VTK/Infovis/vtkDelimitedTextReader.cxx
+++ VTK/Infovis/vtkDelimitedTextReader.cxx
@@ -66,7 +66,8 @@ static int my_getline(istream& stream, vtkStdString &output, int& line_count);
 // Returns true if the line is entirely whitespace, false otherwise.
 static bool isspace(vtkStdString s)
 {
-  for (unsigned int i = 0; i < s.length(); ++i)
+  vtkStdString::size_type i;
+  for (i = 0; i < s.length(); ++i)
     {
     if (!isspace(s[i]))
       {
