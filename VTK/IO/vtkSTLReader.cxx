bc494e0583c017fe7eeae48d00cf1a24a634aa4d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2009-04-01 09:50:49 +0000

    ENH: reported on the vtkusers mailing list.

--- VTK/IO/vtkSTLReader.cxx
+++ VTK/IO/vtkSTLReader.cxx
@@ -23,6 +23,7 @@
 #include "vtkInformation.h"
 #include "vtkInformationVector.h"
 #include "vtkPolyData.h"
+#include "vtkErrorCode.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
 
 #include <ctype.h>
@@ -95,6 +96,7 @@ int vtkSTLReader::RequestData(
   if (!this->FileName || (this->FileName && (0==strlen(this->FileName))))
     {
     vtkErrorMacro(<<"A FileName must be specified.");
+    this->SetErrorCode( vtkErrorCode::NoFileNameError );
     return 0;
     }
 
@@ -103,6 +105,7 @@ int vtkSTLReader::RequestData(
   if ((fp = fopen(this->FileName, "r")) == NULL)
     {
     vtkErrorMacro(<< "File " << this->FileName << " not found");
+    this->SetErrorCode( vtkErrorCode::CannotOpenFileError );
     return 0;
     }
 
