74ebf5bbb6db426bba81b17908faa49ba63f22a8
Eric Stanton <etstant@sandia.gov>
2009-04-06 19:31:44 +0000

    COMP: Forgot to commit these files which caused a compiler error in vtkQtRecordView. Adds an optional inputFieldType argument to vtkConvertSelection::ToSelectionType() method.

--- VTK/Graphics/vtkConvertSelection.cxx
+++ VTK/Graphics/vtkConvertSelection.cxx
@@ -502,18 +502,13 @@ int vtkConvertSelection::Convert(
     else if (vtkTable::SafeDownCast(data))
       {
       if (!inputNode->GetProperties()->Has(vtkSelectionNode::FIELD_TYPE()) ||
-          inputNode->GetFieldType() == vtkSelectionNode::ROW)
+          inputNode->GetFieldType() != vtkSelectionNode::FIELD)
         {
         dsa = vtkTable::SafeDownCast(data)->GetRowData();
         }
-      else if (inputNode->GetFieldType() == vtkSelectionNode::FIELD)
-        {
-        fd = data->GetFieldData();
-        }
       else
         {
-        vtkErrorMacro("Inappropriate selection type for a vtkTable");
-        return 0;
+        fd = data->GetFieldData();
         }
       }
     else
@@ -1025,7 +1020,8 @@ vtkSelection* vtkConvertSelection::ToSelectionType(
   vtkSelection* input, 
   vtkDataObject* data, 
   int type, 
-  vtkStringArray* arrayNames)
+  vtkStringArray* arrayNames,
+  int inputFieldType)
 {
   VTK_CREATE(vtkConvertSelection, convert);
   vtkDataObject* dataCopy = data->NewInstance();
@@ -1036,6 +1032,7 @@ vtkSelection* vtkConvertSelection::ToSelectionType(
   convert->SetInput(1, dataCopy);
   convert->SetOutputType(type);
   convert->SetArrayNames(arrayNames);
+  convert->SetInputFieldType(inputFieldType);
   convert->Update();
   vtkSelection* output = convert->GetOutput();
   output->Register(0);
