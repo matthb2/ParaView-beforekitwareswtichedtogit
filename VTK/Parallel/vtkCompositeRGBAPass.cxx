c887d1dc7a84cc9992a67652d5e320caafb2aa4a
Francois Bertel <francois.bertel@kitware.com>
2009-06-04 20:28:38 +0000

    ENH:Fixed gcc sign conversion warnings.

--- VTK/Parallel/vtkCompositeRGBAPass.cxx
+++ VTK/Parallel/vtkCompositeRGBAPass.cxx
@@ -379,7 +379,7 @@ void vtkCompositeRGBAPass::Render(const vtkRenderState *s)
         this->RootTexture=vtkTextureObject::New();
         this->RootTexture->SetContext(context);
         }
-      this->RootTexture->Allocate2D(w,h,4,VTK_UNSIGNED_CHAR);
+      this->RootTexture->Allocate2D(dims[0],dims[1],4,VTK_UNSIGNED_CHAR);
       this->RootTexture->CopyFromFrameBuffer(0,0,0,0,w,h);
       }
     
