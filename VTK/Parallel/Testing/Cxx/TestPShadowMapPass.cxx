4292682795dc414752c03c1364671aade5bea887
Francois Bertel <francois.bertel@kitware.com>
2009-06-16 15:57:43 +0000

    BUG:Fixed bakground artifact by using a compositer that supports alpha (default vtkCompressCompositer does not, use vtkTreeCompositer instead).

--- VTK/Parallel/Testing/Cxx/TestPShadowMapPass.cxx
+++ VTK/Parallel/Testing/Cxx/TestPShadowMapPass.cxx
@@ -75,6 +75,7 @@
 
 #include "vtkLightActor.h"
 #include "vtkProcess.h"
+#include "vtkTreeCompositer.h"
 
 // Defined in TestLightActor.cxx
 // For each spotlight, add a light frustum wireframe representation and a cone
@@ -119,6 +120,10 @@ void MyProcess::Execute()
   int me=this->Controller->GetLocalProcessId();
  
   vtkCompositeRenderManager *prm = vtkCompositeRenderManager::New();
+  
+  vtkTreeCompositer *compositer=vtkTreeCompositer::New();
+  prm->SetCompositer(compositer);
+  compositer->Delete();
   
   vtkRenderWindowInteractor *iren=0;
   
