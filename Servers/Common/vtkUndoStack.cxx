94da13cc724cd2e8d1e6827d8acb27edeef1f45b
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-07-16 19:17:59 +0000

    COMP: Fixed warnings

--- Servers/Common/vtkUndoStack.cxx
+++ Servers/Common/vtkUndoStack.cxx
@@ -90,7 +90,8 @@ const char* vtkUndoStack::GetRedoSetLabel(unsigned int position)
     {
     return NULL;
     }
-  position = (this->Internal->RedoStack.size() - position) -1;
+  position = (static_cast<unsigned int>(
+      this->Internal->RedoStack.size()) - position) -1;
   return this->Internal->RedoStack[position].Label.c_str();
 }
 
