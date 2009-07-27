f88b5280ac8331624202f101b300e3c6da897758
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-07-27 13:04:13 +0000

    ENH: Adding example plugins for * Simple representation * Hidden Line Removal Representation that uses hardware shaders for hidden lines   removal.

---
+++ Examples/Plugins/Representation/vtkMySpecialPolyDataMapper.cxx
@@ -0,0 +1,37 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    $RCSfile$
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkMySpecialPolyDataMapper.h"
+
+#include "vtkObjectFactory.h"
+
+vtkStandardNewMacro(vtkMySpecialPolyDataMapper);
+vtkCxxRevisionMacro(vtkMySpecialPolyDataMapper, "$Revision$");
+//----------------------------------------------------------------------------
+vtkMySpecialPolyDataMapper::vtkMySpecialPolyDataMapper()
+{
+}
+
+//----------------------------------------------------------------------------
+vtkMySpecialPolyDataMapper::~vtkMySpecialPolyDataMapper()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkMySpecialPolyDataMapper::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+
