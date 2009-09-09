9a02099117fa46dd608f1000f229ebbd0f25aa25
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2009-09-09 20:03:02 +0000

    BUG: Fixed segfault when converting global-id selection to block-based selection (invalid memory access).

--- VTK/Graphics/vtkConvertSelection.cxx
+++ VTK/Graphics/vtkConvertSelection.cxx
@@ -231,7 +231,7 @@ int vtkConvertSelection::ConvertToBlockSelection(
   vtkstd::set<unsigned int> indices;
   for (unsigned int n = 0; n < input->GetNumberOfNodes(); ++n)
     {
-    vtkSelectionNode* inputNode = input->GetNode(n);
+    vtkSmartPointer<vtkSelectionNode> inputNode = input->GetNode(n);
     if (inputNode->GetContentType() == vtkSelectionNode::GLOBALIDS)
       {
       // global id selection does not have COMPOSITE_INDEX() key, so we convert
