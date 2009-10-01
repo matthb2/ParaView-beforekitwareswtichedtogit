4a840bd435d4da18c8dc8363f3b37e563e4bb01d
Pat Marion <pat.marion@kitware.com>
2009-10-01 09:44:55 +0000

    ENH: Add methods ProcessQtEvents and ProcessQtEventsNoUserInput to vtkQtView, these can be called to update the view when running without a Qt event loop.  Add a SaveImage method that calls QPixmap::save to save the view's widget contents.

--- VTK/GUISupport/Qt/vtkQtView.cxx
+++ VTK/GUISupport/Qt/vtkQtView.cxx
@@ -12,7 +12,8 @@
 
 #include "vtkQtView.h"
 
-#include <QWidget>
+#include <QApplication>
+#include <QPixmap>
 #include "vtkObjectFactory.h"
 
 vtkCxxRevisionMacro(vtkQtView, "$Revision$");
@@ -35,3 +36,21 @@ void vtkQtView::PrintSelf(ostream& os, vtkIndent indent)
   this->Superclass::PrintSelf(os,indent);
 }
 
+//----------------------------------------------------------------------------
+void vtkQtView::ProcessQtEvents()
+{
+  QApplication::processEvents();
+}
+
+//----------------------------------------------------------------------------
+void vtkQtView::ProcessQtEventsNoUserInput()
+{
+  QApplication::processEvents(QEventLoop::ExcludeUserInputEvents);
+}
+
+//----------------------------------------------------------------------------
+bool vtkQtView::SaveImage(const char* filename)
+{
+  // This is ok even if this->GetWidget() returns null.
+  return QPixmap::grabWidget(this->GetWidget()).save(filename);
+}
