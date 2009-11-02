70448481012a9978190a894a1fabc3e0ed1441c5
Zhanping Liu, Kitware, Inc. <zhanping.liu@kitware.com>
2009-11-02 20:20:01 +0000

    BUG: fixed the test failure with PrintSelf() and cleaned up the code.

--- VTK/IO/vtkTecplotReader.cxx
+++ VTK/IO/vtkTecplotReader.cxx
@@ -575,20 +575,11 @@ void vtkTecplotReader::PrintSelf( ostream & os, vtkIndent indent )
 {
   this->Superclass::PrintSelf( os, indent );
   
-  os << indent << "NumberOfVariables: "  << this->NumberOfVariables  << endl;
-  /*//
-  os << indent << "DataTitle: "          << this->DataTitle          << endl;
-  os << indent << "Size of CellBased: "  << this->CellBased.size()   << endl;
-  os << indent << "Size of ZoneNames: "  << this->ZoneNames.size()   << endl;
-  os << indent << "Size of Variables: "  << this->Variables.size()   << endl;
-  //*/
-  
-  ///
-  os << indent << "Internal: "           << this->Internal           << endl;
-  os << indent << "FileName: "           << this->FileName           << endl;
-  os << indent << "SelectionObserver: "  << this->SelectionObserver  << endl;
-  os << indent << "DataArraySelection: " << this->DataArraySelection << endl;
-  //*/
+  os << indent << "DataTitle: "          << this->DataTitle         << endl;
+  os << indent << "Size of CellBased: "  << this->CellBased.size()  << endl;
+  os << indent << "Size of ZoneNames: "  << this->ZoneNames.size()  << endl;
+  os << indent << "Size of Variables: "  << this->Variables.size()  << endl;
+  os << indent << "NumberOfVariables: "  << this->NumberOfVariables << endl;
 }
 
 // ----------------------------------------------------------------------------
