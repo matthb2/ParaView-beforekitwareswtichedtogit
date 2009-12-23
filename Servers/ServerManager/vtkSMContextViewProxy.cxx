e619bf5f18d2964467215c7fc98928a813341a39
Marcus Hanwell <marcus.hanwell@kitware.com>
2009-12-23 21:44:56 +0000

    COMP: Fixed a Windows VS9 compiler error.

--- Servers/ServerManager/vtkSMContextViewProxy.cxx
+++ Servers/ServerManager/vtkSMContextViewProxy.cxx
@@ -96,10 +96,11 @@ vtkContextView* vtkSMContextViewProxy::GetChartView()
 }
 
 //----------------------------------------------------------------------------
-bool vtkSMContextViewProxy::WriteImage(const char* filename)
+bool vtkSMContextViewProxy::WriteImage(const char*)
 {
 //  this->PerformRender();
 //  return this->ChartView->SaveImage(filename);
+  return false;
 }
 
 //----------------------------------------------------------------------------
