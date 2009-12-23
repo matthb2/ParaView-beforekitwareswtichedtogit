005d72a66d0495d739367e6fc08e982fb26ba778
Marcus Hanwell <marcus.hanwell@kitware.com>
2009-12-23 20:08:58 +0000

    COMP: Exclude the mouse events from being wrapped.

--- VTK/Charts/vtkChartXY.cxx
+++ VTK/Charts/vtkChartXY.cxx
@@ -335,7 +335,7 @@ bool vtkChartXY::MouseMoveEvent(const vtkContextMouseEvent &mouse)
 //-----------------------------------------------------------------------------
 bool vtkChartXY::MouseLeaveEvent(const vtkContextMouseEvent &mouse)
 {
-  return true;
+  return false;
 }
 
 //-----------------------------------------------------------------------------
@@ -354,7 +354,7 @@ bool vtkChartXY::MouseButtonPressEvent(const vtkContextMouseEvent &mouse)
 //-----------------------------------------------------------------------------
 bool vtkChartXY::MouseButtonReleaseEvent(const vtkContextMouseEvent &mouse)
 {
-  return true;
+  return false;
 }
 
 //-----------------------------------------------------------------------------
