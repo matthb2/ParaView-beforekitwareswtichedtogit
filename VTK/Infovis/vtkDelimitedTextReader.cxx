5d4772f10e28b2ad0d6f402f7e6c82d6e76f8511
Thomas Otahal <tjotaha@sandia.gov>
2010-01-05 21:06:56 +0000

    COMP:  Fixed compiler warnings on Linux and Mac.

--- VTK/Infovis/vtkDelimitedTextReader.cxx
+++ VTK/Infovis/vtkDelimitedTextReader.cxx
@@ -84,15 +84,15 @@ public:
     EscapeDelimiter(escape.begin(), escape.end()),
     HaveHeaders(have_headers),
     UnicodeArrayOutput(unicode_array_output),
+    WhiteSpaceOnlyString(true),
     OutputTable(output_table),
     CurrentRecordIndex(0),
     CurrentFieldIndex(0),
     RecordAdjacent(true),
-    WithinString(0),
     MergeConsDelims(merg_cons_delimiters),
-    WhiteSpaceOnlyString(true),
     ProcessEscapeSequence(false),
-    UseStringDelimiter(use_string_delimeter)
+    UseStringDelimiter(use_string_delimeter),
+    WithinString(0)
   {
   }
 
