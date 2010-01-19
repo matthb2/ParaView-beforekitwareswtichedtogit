242314955bc9213a1fc28fa1bbaa6a585bfd7203
Bill Lorensen <lorensen@crd.ge.com>
2010-01-19 20:27:13 +0000

    BUG: memory leak. Destrcutor was not deleting QApplication.

--- VTK/GUISupport/Qt/vtkQtInitialization.cxx
+++ VTK/GUISupport/Qt/vtkQtInitialization.cxx
@@ -28,15 +28,20 @@ vtkStandardNewMacro(vtkQtInitialization);
 
 vtkQtInitialization::vtkQtInitialization()
 {
+  this->Application = NULL;
   if(!QApplication::instance())
     {
     int argc = 0;
-    new QApplication(argc, 0);
+    this->Application = new QApplication(argc, 0);
     }
 }
 
 vtkQtInitialization::~vtkQtInitialization()
 {
+  if (this->Application)
+    {
+    delete this->Application;
+    }
 }
 
 void vtkQtInitialization::PrintSelf(ostream &os, vtkIndent indent)
