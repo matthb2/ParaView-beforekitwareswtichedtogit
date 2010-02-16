4ade7b1c4a2fa7f87f19d88d87afd2496f8ed4e1
Zack Galbreath <zack.galbreath@kitware.com>
2010-02-16 17:16:51 +0000

    BUG: change GetTable() behavior so that it is consistent with other database classes: don't require the user to delete the returned vtkStringArray

--- VTK/IO/vtkSQLiteDatabase.cxx
+++ VTK/IO/vtkSQLiteDatabase.cxx
@@ -39,6 +39,10 @@ vtkSQLiteDatabase::vtkSQLiteDatabase()
 {
   this->SQLiteInstance = NULL;
 
+  this->Tables = vtkStringArray::New();
+  this->Tables->Register(this);
+  this->Tables->Delete();
+
   // Initialize instance variables
   this->DatabaseType = 0;
   this->SetDatabaseType("sqlite");
@@ -60,6 +64,7 @@ vtkSQLiteDatabase::~vtkSQLiteDatabase()
     {
     this->SetDatabaseFileName(0);
     }
+  this->Tables->UnRegister(this);
 }
 
 // ----------------------------------------------------------------------
@@ -356,10 +361,11 @@ vtkSQLQuery * vtkSQLiteDatabase::GetQueryInstance()
 // ----------------------------------------------------------------------
 vtkStringArray * vtkSQLiteDatabase::GetTables()
 {
+  this->Tables->Resize(0);
   if (this->SQLiteInstance == NULL)
     {
     vtkErrorMacro(<<"GetTables(): Database is not open!");
-    return NULL;
+    return this->Tables;
     }
 
   vtkSQLQuery *query = this->GetQueryInstance();
@@ -371,18 +377,17 @@ vtkStringArray * vtkSQLiteDatabase::GetTables()
     vtkErrorMacro(<< "GetTables(): Database returned error: "
                   << vtk_sqlite3_errmsg(this->SQLiteInstance) );
     query->Delete();
-    return NULL;
+    return this->Tables;
     }
   else
     {
     vtkDebugMacro(<<"GetTables(): SQL query succeeded.");
-    vtkStringArray *results = vtkStringArray::New();
     while (query->NextRow() )
       {
-      results->InsertNextValue(query->DataValue(0).ToString() );
+      this->Tables->InsertNextValue(query->DataValue(0).ToString() );
       }
     query->Delete();
-    return results;
+    return this->Tables;
     }
 }
 
