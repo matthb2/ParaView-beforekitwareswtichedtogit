654973cbbdd83d8b213c134321ca354db075a585
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2010-03-08 18:50:57 +0000

    BUG: Fixed BUG #10379.Initialized MakeOutlineOfInput variable.

--- Servers/Filters/vtkPVGeometryFilter.cxx
+++ Servers/Filters/vtkPVGeometryFilter.cxx
@@ -152,6 +152,7 @@ vtkPVGeometryFilter::vtkPVGeometryFilter ()
   this->PassThroughPointIds = 1;
   this->ForceUseStrips = 0;
   this->StripModFirstPass = 1;
+  this->MakeOutlineOfInput = 0;
 
   this->GetInformation()->Set(vtkAlgorithm::PRESERVES_RANGES(), 1);
   this->GetInformation()->Set(vtkAlgorithm::PRESERVES_BOUNDS(), 1);  
